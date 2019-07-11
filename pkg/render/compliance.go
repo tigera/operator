c2d40f3dee4aa5e3bf02de415e0237b530d465d0
Rafael Vanoni <rafael@tigera.io>
2019-07-10 17:05:24 -0700

    OS-3744 Install compliance via operator (ServiceAccounts and ClusterRoles)

--- pkg/render/compliance.go
+++ pkg/render/compliance.go
@@ -22,6 +22,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
@@ -60,11 +61,29 @@ func Compliance(cr *operator.Installation) Component {
 
 // compliance-controller
 func complianceControllerServiceAccount(cr *operator.Installation) *v1.ServiceAccount {
-	return &v1.ServiceAccount{}
+	return &v1.ServiceAccount{
+		TypeMeta:   metav1.TypeMeta{Kind: "ServiceAccount", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-controller", Namespace: "calico-monitoring"},
+	}
 }
 
 func complianceControllerClusterRole(cr *operator.Installation) *rbacv1.ClusterRole {
-	return &rbacv1.ClusterRole{}
+	return &rbacv1.ClusterRole{
+		TypeMeta:   metav1.TypeMeta{Kind: "ClusterRole", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-controller"},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"batch"},
+				Resources: []string{"jobs"},
+				Verbs:     []string{"create", "list", "get", "delete"},
+			},
+			{
+				APIGroups: []string{""},
+				Resources: []string{"podTemplates"},
+				Verbs:     []string{"get"},
+			},
+		},
+	}
 }
 
 func complianceControllerRoleBinding(cr *operator.Installation) *rbacv1.RoleBinding {
@@ -81,11 +100,24 @@ func complianceControllerDeployment(cr *operator.Installation) *appsv1.Deploymen
 
 // compliance-reporter
 func complianceReporterServiceAccount(cr *operator.Installation) *v1.ServiceAccount {
-	return &v1.ServiceAccount{}
+	return &v1.ServiceAccount{
+		TypeMeta:   metav1.TypeMeta{Kind: "ServiceAccount", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-reporter", Namespace: "calico-monitoring"},
+	}
 }
 
 func complianceReporterClusterRole(cr *operator.Installation) *rbacv1.ClusterRole {
-	return &rbacv1.ClusterRole{}
+	return &rbacv1.ClusterRole{
+		TypeMeta:   metav1.TypeMeta{Kind: "ClusterRole", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-reporter"},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"projectcalico.org"},
+				Resources: []string{"globalreporttypes", "globalreports"},
+				Verbs:     []string{"get"},
+			},
+		},
+	}
 }
 
 func complianceReporterClusterRoleBinding(cr *operator.Installation) *rbacv1.ClusterRoleBinding {
@@ -98,11 +130,34 @@ func complianceReporterDaemonSet(cr *operator.Installation) *apps.DaemonSet {
 
 // compliance-server
 func complianceServerServiceAccount(cr *operator.Installation) *v1.ServiceAccount {
-	return &v1.ServiceAccount{}
+	return &v1.ServiceAccount{
+		TypeMeta:   metav1.TypeMeta{Kind: "ServiceAccount", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-server", Namespace: "calico-monitoring"},
+	}
 }
 
 func complianceServerClusterRole(cr *operator.Installation) *rbacv1.ClusterRole {
-	return &rbacv1.ClusterRole{}
+	return &rbacv1.ClusterRole{
+		TypeMeta:   metav1.TypeMeta{Kind: "ClusterRole", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-server"},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"batch"},
+				Resources: []string{"globalreporttypes", "globalreports"},
+				Verbs:     []string{"get"},
+			},
+			{
+				APIGroups: []string{"authentication.k8s.io"},
+				Resources: []string{"tokenreviews"},
+				Verbs:     []string{"create"},
+			},
+			{
+				APIGroups: []string{"authentication.k8s.io"},
+				Resources: []string{"subjectaccessreviews"},
+				Verbs:     []string{"create"},
+			},
+		},
+	}
 }
 
 func complianceServerClusterRoleBinding(cr *operator.Installation) *rbacv1.ClusterRoleBinding {
@@ -119,11 +174,31 @@ func complianceServerDeployment(cr *operator.Installation) *appsv1.Deployment {
 
 // compliance-snapshotter
 func complianceSnapshotterServiceAccount(cr *operator.Installation) *v1.ServiceAccount {
-	return &v1.ServiceAccount{}
+	return &v1.ServiceAccount{
+		TypeMeta:   metav1.TypeMeta{Kind: "ServiceAccount", APIVersion: "v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-snapshotter", Namespace: "calico-monitoring"},
+	}
 }
 
 func complianceSnapshotterClusterRole(cr *operator.Installation) *rbacv1.ClusterRole {
-	return &rbacv1.ClusterRole{}
+	return &rbacv1.ClusterRole{
+		TypeMeta:   metav1.TypeMeta{Kind: "ClusterRole", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{Name: "tigera-compliance-snapshotter"},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{"extensions", "authentication.k8s.io", ""},
+				Resources: []string{"networkpolicies", "nodes", "namespaces", "pods", "serviceaccounts",
+					"endpoints", "services"},
+				Verbs: []string{"get", "list"},
+			},
+			{
+				APIGroups: []string{"projectcalico.org"},
+				Resources: []string{"globalnetworkpolicies", "networkpolicies", "tier.globalnetworkpolicies",
+					"tier.networkpolicies", "tiers", "hostendpoints", "globalnetworksets"},
+				Verbs: []string{"get", "list"},
+			},
+		},
+	}
 }
 
 func complianceSnapshotterClusterRoleBinding(cr *operator.Installation) *rbacv1.ClusterRoleBinding {
@@ -138,9 +213,11 @@ func complianceSnapshotterDeployment(cr *operator.Installation) *appsv1.Deployme
 func complianceGlobalReportInventory(cr *operator.Installation) *v3.GlobalReportType {
 	return &v3.GlobalReportType{}
 }
+
 func complianceGlobalReportNetworkAccess(cr *operator.Installation) *v3.GlobalReportType {
 	return &v3.GlobalReportType{}
 }
+
 func complianceGlobalReportPolicyAudit(cr *operator.Installation) *v3.GlobalReportType {
 	return &v3.GlobalReportType{}
 }

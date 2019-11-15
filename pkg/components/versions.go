88a6acac129bbefae7f3860b3c9180b4a511b446
Alina Militaru <alina@tigera.io>
2019-11-15 15:38:41 -0800

    Switch Voltron to master

--- pkg/components/versions.go
+++ pkg/components/versions.go
@@ -36,7 +36,7 @@ const (
 
 	// Manager images.
 	VersionManager        = "v2.5.0-347-gb01f72d0"
-	VersionManagerProxy   = "v2.5.0-mcm0.1-32-gdd59369"
+	VersionManagerProxy   = "master"
 	VersionManagerEsProxy = "v2.6.0-0.dev-96-g38d646f"
 
 	// ECK Elasticsearch images

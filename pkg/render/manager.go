c16dd0a7f1f891c2e1709a05af79634a069b76eb
Rene Dekker <rene@tigera.io>
2020-01-16 16:50:48 -0800

    Change RBAC resources to match es-proxy

--- pkg/render/manager.go
+++ pkg/render/manager.go
@@ -653,7 +653,7 @@ func (c *managerComponent) tigeraUserClusterRole() *rbacv1.ClusterRole {
 			// Access to flow logs, audit logs, and statistics
 			{
 				APIGroups: []string{"lma.tigera.io"},
-				Resources: []string{"index"},
+				Resources: []string{"*"},
 				ResourceNames: []string{
 					"flows", "audit*", "events", "dns",
 				},
@@ -739,7 +739,7 @@ func (c *managerComponent) tigeraNetworkAdminClusterRole() *rbacv1.ClusterRole {
 			// Access to flow logs, audit logs, and statistics
 			{
 				APIGroups: []string{"lma.tigera.io"},
-				Resources: []string{"index"},
+				Resources: []string{"*"},
 				ResourceNames: []string{
 					"flows", "audit*", "events", "dns",
 				},

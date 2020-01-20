3a9c582ecd8cb784ecee3b4755798e0ad9439141
Brendan Creane <brendan@tigera.io>
2020-01-20 17:21:06 +0000

    pull master tags of enterprise images

--- pkg/components/versions.go
+++ pkg/components/versions.go
@@ -12,41 +12,41 @@ const (
 // This section contains images used when installing Tigera Secure.
 const (
 	// Overrides for Calico.
-	VersionTigeraNode            = "v2.6.0-0.dev-175-g3f547b6"
-	VersionTigeraTypha           = "v2.6.0-0.dev-104-g6c51073"
-	VersionTigeraKubeControllers = "v2.6.0-0.dev-86-g506e244-dirty"
+	VersionTigeraNode            = "master"
+	VersionTigeraTypha           = "master"
+	VersionTigeraKubeControllers = "master"
 
 	// API server images.
-	VersionAPIServer   = "v2.7.0-0.dev-16-g64e84f77"
-	VersionQueryServer = "v2.6.0-0.dev-12-gca85666"
+	VersionAPIServer   = "master"
+	VersionQueryServer = "master"
 
 	// Logging
-	VersionFluentd = "v2.6.0-0.dev-33-g382faf2"
+	VersionFluentd = "master"
 
 	// Compliance images.
-	VersionComplianceController  = "v2.7.0-0.dev-22-gf5eb877"
-	VersionComplianceReporter    = "v2.7.0-0.dev-22-gf5eb877"
-	VersionComplianceServer      = "v2.7.0-0.dev-22-gf5eb877"
-	VersionComplianceSnapshotter = "v2.7.0-0.dev-22-gf5eb877"
-	VersionComplianceBenchmarker = "v2.7.0-0.dev-22-gf5eb877"
+	VersionComplianceController  = "master"
+	VersionComplianceReporter    = "master"
+	VersionComplianceServer      = "master"
+	VersionComplianceSnapshotter = "master"
+	VersionComplianceBenchmarker = "master"
 
 	// Intrusion detection images.
-	VersionIntrusionDetectionController   = "v2.7.0-0.dev-15-g44db458"
-	VersionIntrusionDetectionJobInstaller = "v2.7.0-0.dev-17-gbf06bde"
+	VersionIntrusionDetectionController   = "master"
+	VersionIntrusionDetectionJobInstaller = "master"
 
 	// Manager images.
-	VersionManager        = "v2.5.0-347-gb01f72d0"
-	VersionManagerProxy   = "v2.7.0-0.dev-21-g9e5b660"
-	VersionManagerEsProxy = "v2.7.0-0.dev-22-g2e2f167"
+	VersionManager        = "master"
+	VersionManagerProxy   = "master"
+	VersionManagerEsProxy = "master"
 
 	// ECK Elasticsearch images
 	VersionECKOperator      = "0.9.0"
 	VersionECKElasticsearch = "7.3.2"
 	VersionECKKibana        = "7.3.2"
-	VersionEsCurator        = "v2.6.0-0.dev-25-gb04da05"
+	VersionEsCurator        = "master"
 
 	VersionKibana = "7.3"
 
 	// Multicluster tunnel image.
-	VersionGuardian = "v2.7.0-0.dev-21-g9e5b660"
+	VersionGuardian = "master"
 )

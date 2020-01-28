e37ca1fe1e5fa2274db88c0cb07244ee084a0061
ozdanborne <dan@projectcalico.org>
2020-01-27 17:14:24 -0800

    enhance gen-versions to include digests

---
+++ pkg/components/images.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package components
+
+// Default registries for Calico and Tigera.
+const (
+	CalicoRegistry = "docker.io/"
+	TigeraRegistry = "gcr.io/unique-caldron-775/cnx/"
+	K8sGcrRegistry = "gcr.io/"
+	ECKRegistry    = "docker.elastic.co/"
+)
+
+const (
+	OperatorInitImageName = "tigera/operator-init:"
+)

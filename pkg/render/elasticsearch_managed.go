e269b0dde49152f87f1a2dcd418d5f06ff0a4e5c
Casey Davenport <casey@tigera.io>
2020-02-05 11:28:00 -0800

    Add copyright headers to go files

--- pkg/render/elasticsearch_managed.go
+++ pkg/render/elasticsearch_managed.go
@@ -1,9 +1,24 @@
+// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // The code in this file renders the necessary components for a managed cluster to be able to communicate with the elasticsearch
 // in it's management cluster
 package render
 
 import (
 	"fmt"
+
 	operatorv1 "github.com/tigera/operator/pkg/apis/operator/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"

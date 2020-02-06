f5ff728ce034705890c26641bdcde338550a8224
matt <mleung975@gmail.com>
2020-02-05 18:15:06 -0800

    Update versions for release 1.2

--- pkg/components/versions.go
+++ pkg/components/versions.go
@@ -16,51 +16,51 @@ package components
 
 // This section contains images used when installing open-source Calico.
 const (
-	VersionCalicoNode            = "v3.11.1-with-auto-backend"
-	VersionCalicoCNI             = "v3.11.1"
-	VersionCalicoTypha           = "v3.11.1"
-	VersionCalicoKubeControllers = "v3.11.1"
-	VersionFlexVolume            = "v3.11.1"
+	VersionCalicoNode            = "v3.12.0"
+	VersionCalicoCNI             = "v3.12.0"
+	VersionCalicoTypha           = "v3.12.0"
+	VersionCalicoKubeControllers = "v3.12.0"
+	VersionFlexVolume            = "v3.12.0"
 )
 
 // This section contains images used when installing Tigera Secure.
 const (
 	// Overrides for Calico.
-	VersionTigeraNode            = "v2.7.0-0.dev-188-gc5255d3"
-	VersionTigeraTypha           = "v2.7.0-0.dev-74-g67bc0b1"
-	VersionTigeraKubeControllers = "v2.7.0-0.dev-109-gfa8b79c"
+	VersionTigeraNode            = "release-v2.7"
+	VersionTigeraTypha           = "release-v2.7"
+	VersionTigeraKubeControllers = "release-v2.7"
 
 	// API server images.
-	VersionAPIServer   = "v2.7.0-0.dev-32-gb094281f"
-	VersionQueryServer = "v2.7.0-0.dev-26-g232a725"
+	VersionAPIServer   = "release-v2.7"
+	VersionQueryServer = "release-v2.7"
 
 	// Logging
-	VersionFluentd = "v2.7.0-0.dev-4-gb1486b3"
+	VersionFluentd = "release-v2.7"
 
 	// Compliance images.
-	VersionComplianceController  = "v2.7.0-0.dev-38-g23b93c3"
-	VersionComplianceReporter    = "v2.7.0-0.dev-38-g23b93c3"
-	VersionComplianceServer      = "v2.7.0-0.dev-38-g23b93c3"
-	VersionComplianceSnapshotter = "v2.7.0-0.dev-38-g23b93c3"
-	VersionComplianceBenchmarker = "v2.7.0-0.dev-38-g23b93c3"
+	VersionComplianceController  = "release-v2.7"
+	VersionComplianceReporter    = "release-v2.7"
+	VersionComplianceServer      = "release-v2.7"
+	VersionComplianceSnapshotter = "release-v2.7"
+	VersionComplianceBenchmarker = "release-v2.7"
 
 	// Intrusion detection images.
-	VersionIntrusionDetectionController   = "v2.7.0-0.dev-27-gd7ce71b"
-	VersionIntrusionDetectionJobInstaller = "v2.7.0-0.dev-27-gd7ce71b"
+	VersionIntrusionDetectionController   = "release-v2.7"
+	VersionIntrusionDetectionJobInstaller = "release-v2.7"
 
 	// Manager images.
-	VersionManager        = "v2.7.0-0.dev-234-g952d0a82"
-	VersionManagerProxy   = "v2.7.0-0.dev-34-g4eac4a3"
-	VersionManagerEsProxy = "v2.7.0-0.dev-43-g422fefb"
+	VersionManager        = "release-v2.7"
+	VersionManagerProxy   = "release-v2.7"
+	VersionManagerEsProxy = "release-v2.7"
 
 	// ECK Elasticsearch images
 	VersionECKOperator      = "0.9.0"
 	VersionECKElasticsearch = "7.3.2"
 	VersionECKKibana        = "7.3.2"
-	VersionEsCurator        = "v2.6.0-0.dev-25-gb04da05"
+	VersionEsCurator        = "release-v2.7"
 
 	VersionKibana = "7.3"
 
 	// Multicluster tunnel image.
-	VersionGuardian = "v2.7.0-0.dev-34-g4eac4a3"
+	VersionGuardian = "release-v2.7"
 )

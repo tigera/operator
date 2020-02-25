30d71e1ebd6531e942664446240ea41744862581
Erik Stidham <erik@tigera.io>
2020-02-25 10:09:22 -0600

    Fix test and image name processing order

--- pkg/render/images_test.go
+++ pkg/render/images_test.go
@@ -25,7 +25,7 @@ var _ = Describe("No registry override", func() {
 		Expect(constructImage(NodeImageNameCalico, "", "")).To(Equal("docker.io/calico/node:" + components.VersionCalicoNode))
 	})
 	It("should render a tigera image correctly", func() {
-		Expect(constructImage(NodeImageNameTigera, "")).To(Equal("gcr.io/unique-caldron-775/cnx/tigera/cnx-node:" + components.VersionTigeraNode))
+		Expect(constructImage(NodeImageNameTigera, "", "")).To(Equal("gcr.io/unique-caldron-775/cnx/tigera/cnx-node:" + components.VersionTigeraNode))
 	})
 	It("should render an ECK image correctly", func() {
 		Expect(constructImage(ECKOperatorImageName, "", "")).To(Equal("docker.elastic.co/eck/eck-operator:" + components.VersionECKOperator))
@@ -49,7 +49,7 @@ var _ = Describe("imagepath override", func() {
 		Expect(constructImage(NodeImageNameCalico, "", "userpath")).To(Equal("docker.io/userpath/node:" + components.VersionCalicoNode))
 	})
 	It("should render a tigera image correctly", func() {
-		Expect(constructImage(NodeImageNameTigera, "", "userpath")).To(Equal("quay.io/userpath/cnx-node:" + components.VersionTigeraNode))
+		Expect(constructImage(NodeImageNameTigera, "", "userpath")).To(Equal("gcr.io/unique-caldron-775/cnx/userpath/cnx-node:" + components.VersionTigeraNode))
 	})
 	It("should render an ECK image correctly", func() {
 		Expect(constructImage(ECKOperatorImageName, "", "userpath")).To(Equal("docker.elastic.co/userpath/eck-operator:" + components.VersionECKOperator))
@@ -58,12 +58,12 @@ var _ = Describe("imagepath override", func() {
 
 var _ = Describe("registry and imagepath override", func() {
 	It("should render a calico image correctly", func() {
-		Expect(constructImage(NodeImageNameCalico, "quay.io/extra", "userpath")).To(Equal("quay.io/extra/userpath/node:" + components.VersionCalicoNode))
+		Expect(constructImage(NodeImageNameCalico, "quay.io/extra/", "userpath")).To(Equal("quay.io/extra/userpath/node:" + components.VersionCalicoNode))
 	})
 	It("should render a tigera image correctly", func() {
-		Expect(constructImage(NodeImageNameTigera, "quay.io/extra", "userpath")).To(Equal("quay.io/extra/userpath/cnx-node:" + components.VersionTigeraNode))
+		Expect(constructImage(NodeImageNameTigera, "quay.io/extra/", "userpath")).To(Equal("quay.io/extra/userpath/cnx-node:" + components.VersionTigeraNode))
 	})
 	It("should render an ECK image correctly", func() {
-		Expect(constructImage(ECKOperatorImageName, "quay.io/extra", "userpath")).To(Equal("quay.io/extra/userpath/eck-operator:" + components.VersionECKOperator))
+		Expect(constructImage(ECKOperatorImageName, "quay.io/extra/", "userpath")).To(Equal("quay.io/extra/userpath/eck-operator:" + components.VersionECKOperator))
 	})
 })

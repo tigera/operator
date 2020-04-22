3c1ac792c6643dbfdf43462cbe47141cf7ae130b
ozdanborne <dan@projectcalico.org>
2020-04-22 13:46:20 -0700

    proactively check for missing namespace

--- pkg/controller/manager/manager_controller.go
+++ pkg/controller/manager/manager_controller.go
@@ -20,6 +20,7 @@ import (
 	"time"
 
 	operatorv1 "github.com/tigera/operator/pkg/apis/operator/v1"
+	"github.com/tigera/operator/pkg/common"
 	"github.com/tigera/operator/pkg/controller/compliance"
 	"github.com/tigera/operator/pkg/controller/installation"
 	"github.com/tigera/operator/pkg/controller/status"
@@ -213,6 +214,17 @@ func (r *ReconcileManager) Reconcile(request reconcile.Request) (reconcile.Resul
 	if compliance.Status.State != operatorv1.ComplianceStatusReady {
 		r.status.SetDegraded("Compliance is not ready", fmt.Sprintf("compliance status: %s", compliance.Status.State))
 		return reconcile.Result{}, nil
+	}
+
+	// check that prometheus is running
+	ns := &corev1.Namespace{}
+	if err = r.client.Get(ctx, client.ObjectKey{Name: common.TigeraPrometheusNamespace}, ns); err != nil {
+		if errors.IsNotFound(err) {
+			r.status.SetDegraded("prometheus namespace does not exist", "Dependency on tigera-prometheus not satisfied")
+		} else {
+			r.status.SetDegraded("Error querying prometheus", err.Error())
+		}
+		return reconcile.Result{}, err
 	}
 
 	// Check that if the manager certpair secret exists that it is valid (has key and cert fields)

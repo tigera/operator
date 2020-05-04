52f728c7fbf3869348eda906b8173c4fe3025cef
Dan (Turk) Osborne <turk@tigera.io>
2020-05-04 09:46:25 -0700

    remove digest machinery (#561)

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -20,35 +20,30 @@ var (
 	
 	ComponentCalicoCNI = component{
 		Version: "v3.14.0-0.dev-24-g0cab7f2",
-		Digest:  "",
 		Image:   "calico/cni",
 	}
 	
 	
 	ComponentCalicoKubeControllers = component{
 		Version: "v3.14.0-0.dev-30-g947e802",
-		Digest:  "",
 		Image:   "calico/kube-controllers",
 	}
 	
 	
 	ComponentCalicoNode = component{
 		Version: "v3.14.0-0.dev-41-g28bcc6a",
-		Digest:  "",
 		Image:   "calico/node",
 	}
 	
 	
 	ComponentCalicoTypha = component{
 		Version: "v3.14.0-0.dev-22-g303330a",
-		Digest:  "",
 		Image:   "calico/typha",
 	}
 	
 	
 	ComponentFlexVolume = component{
 		Version: "v3.14.0-0.dev-0-g07caa12",
-		Digest:  "",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 	

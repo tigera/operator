b7a692f655dac3ae34280a185a37302945e799d1
Doug Collier <doug@tigera.io>
2020-06-16 18:17:31 -0700

    add tolerations for dockeree

--- pkg/render/node.go
+++ pkg/render/node.go
@@ -491,11 +491,17 @@ func (c *nodeComponent) nodeDaemonset(cniCfgMap *v1.ConfigMap) *apps.DaemonSet {
 
 // nodeTolerations creates the node's tolerations.
 func (c *nodeComponent) nodeTolerations() []v1.Toleration {
-	return []v1.Toleration{
+	t := []v1.Toleration{
 		{Operator: v1.TolerationOpExists, Effect: v1.TaintEffectNoSchedule},
 		{Operator: v1.TolerationOpExists, Effect: v1.TaintEffectNoExecute},
 		{Operator: v1.TolerationOpExists, Key: "CriticalAddonsOnly"},
 	}
+	if c.provider == operator.ProviderDockerEE {
+		t = append(t, v1.Toleration{
+			Operator: v1.TolerationOpExists, Key: "node.kubernetes.io/not-ready",
+		})
+	}
+	return t
 }
 
 // cniDirectories returns the binary and network config directories for the configured platform.

ba196c09e7d3a65bcf9a3797f80c86d5979c59c6
Erik Stidham <erik@tigera.io>
2020-06-25 20:53:36 -0500

    Make init registry be a separate define
    This allows us to set a TigeraRegistry for pulling dev images from
    gcr.io but keep pulling the Init image from quay.io.

--- pkg/components/images.go
+++ pkg/components/images.go
@@ -17,6 +17,12 @@ package components
 const (
 	CalicoRegistry = "docker.io/"
 	TigeraRegistry = "gcr.io/unique-caldron-775/cnx/"
+	// For production InitRegistry should match TigeraRegistry.
+	// For the master branch and other testing scenarios we switch TigeraRegistry to
+	// point to a testing repo but the init image will be pushed to quay, so having
+	// these separate allows pulling the proper test images for the Tigera components
+	// and Init image when testing.
+	InitRegistry   = "quay.io/"
 	K8sGcrRegistry = "gcr.io/"
 	ECKRegistry    = "docker.elastic.co/"
 )

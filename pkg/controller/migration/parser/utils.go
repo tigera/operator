a0359dd3b6af385a743b8da546ad67333852ef14
ozdanborne <dan@projectcalico.org>
2020-07-12 15:02:12 -0400

    migrate existing config

---
+++ pkg/controller/migration/parser/utils.go
@@ -0,0 +1,26 @@
+package parser
+
+import corev1 "k8s.io/api/core/v1"
+
+func getContainer(containers []corev1.Container, name string) *corev1.Container {
+	for _, container := range containers {
+		if container.Name == name {
+			return &container
+		}
+	}
+	return nil
+}
+
+func getContainers(spec corev1.PodSpec, name string) *corev1.Container {
+	for _, container := range spec.Containers {
+		if container.Name == name {
+			return &container
+		}
+	}
+	for _, container := range spec.InitContainers {
+		if container.Name == name {
+			return &container
+		}
+	}
+	return nil
+}

130e451903f088ec58060c49cd2d4e39ffcea8d9
ozdanborne <dan@projectcalico.org>
2020-07-16 10:25:26 -0400

    migrate flexvol path

--- pkg/controller/migration/convert/core_test.go
+++ pkg/controller/migration/convert/core_test.go
@@ -103,4 +103,30 @@ var _ = Describe("core handler", func() {
 			Expect(i.Spec.NodeUpdateStrategy).To(Equal(updateStrategy))
 		})
 	})
+
+	Context("flexvol", func() {
+		It("should not be set by default", func() {
+			Expect(handleCore(&comps, i)).ToNot(HaveOccurred())
+			Expect(i.Spec.FlexVolumePath).To(Equal("None"))
+		})
+		It("should carry forward flexvolumepath", func() {
+			hostPathDirectoryOrCreate := v1.HostPathDirectoryOrCreate
+			path := "/foo/bar/"
+			comps.node.Spec.Template.Spec.Volumes = []v1.Volume{{
+				Name: "flexvol-driver-host",
+				VolumeSource: v1.VolumeSource{
+					HostPath: &v1.HostPathVolumeSource{
+						Path: path,
+						Type: &hostPathDirectoryOrCreate,
+					},
+				},
+			}}
+			comps.node.Spec.Template.Spec.InitContainers = append(comps.node.Spec.Template.Spec.InitContainers, v1.Container{
+				Name: "flexvol-driver",
+			})
+
+			Expect(handleCore(&comps, i)).ToNot(HaveOccurred())
+			Expect(i.Spec.FlexVolumePath).To(Equal(path))
+		})
+	})
 })

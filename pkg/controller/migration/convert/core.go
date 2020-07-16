d3ea464e0136d8e2725d2a3bbc634df173f3c9e4
ozdanborne <dan@projectcalico.org>
2020-07-16 11:55:28 -0400

    ignore remaining common vars

--- pkg/controller/migration/convert/core.go
+++ pkg/controller/migration/convert/core.go
@@ -14,6 +14,20 @@ func handleCore(c *components, install *Installation) error {
 	c.node.ignoreEnv("calico-node", "CLUSTER_TYPE")
 	c.node.ignoreEnv("calico-node", "NODENAME")
 	c.node.ignoreEnv("calico-node", "CALICO_DISABLE_FILE_LOGGING")
+	c.node.ignoreEnv("calico-node", "CALICO_IPV4POOL_IPIP")
+	c.node.ignoreEnv("calico-node", "CALICO_IPV4POOL_VXLAN")
+	c.node.ignoreEnv("calico-node", "FELIX_IPINIPMTU")
+	c.node.ignoreEnv("calico-node", "FELIX_VXLANMTU")
+	c.node.ignoreEnv("calico-node", "FELIX_WIREGUARDMTU")
+	c.node.ignoreEnv("calico-node", "FELIX_IPV6SUPPORT")
+	c.node.ignoreEnv("calico-node", "FELIX_LOGSEVERITYSCREEN")
+	c.node.ignoreEnv("calico-node", "FELIX_HEALTHENABLED")
+	c.node.ignoreEnv("calico-node", "FELIX_USAGEREPORTINGENABLED")
+	c.node.ignoreEnv("upgrade-ipam", "KUBERNETES_NODE_NAME")
+	c.node.ignoreEnv("upgrade-ipam", "CALICO_NETWORKING_BACKEND")
+	c.node.ignoreEnv("install-cni", "CNI_CONF_NAME")
+	c.node.ignoreEnv("install-cni", "KUBERNETES_NODE_NAME")
+	c.node.ignoreEnv("install-cni", "SLEEP")
 
 	return nil
 }

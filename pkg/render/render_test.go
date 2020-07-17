9ca4bcb6cd78a4e5d124db4b4a051e480330f796
Tomas Hruby <tomas@tigera.io>
2020-07-17 13:48:32 -0700

    Add Typha

--- pkg/render/render_test.go
+++ pkg/render/render_test.go
@@ -39,6 +39,7 @@ var _ = Describe("Rendering tests", func() {
 	var typhaNodeTLS *render.TyphaNodeTLS
 	one := intstr.FromInt(1)
 	miMode := operator.MultiInterfaceModeNone
+	k8sServiceEp := render.K8sServiceEndpoint{}
 
 	BeforeEach(func() {
 		// Initialize a default instance to use. Each test can override this to its
@@ -82,7 +83,7 @@ var _ = Describe("Rendering tests", func() {
 		// - 5 kube-controllers resources (ServiceAccount, ClusterRole, Binding, Deployment, PodSecurityPolicy)
 		// - 1 namespace
 		// - 1 PriorityClass
-		c, err := render.Calico(instance, nil, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
+		c, err := render.Calico(k8sServiceEp, instance, nil, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
 		Expect(err).To(BeNil(), "Expected Calico to create successfully %s", err)
 		Expect(componentCount(c.Render())).To(Equal(6 + 4 + 2 + 7 + 5 + 1 + 1))
 	})
@@ -95,7 +96,7 @@ var _ = Describe("Rendering tests", func() {
 		var nodeMetricsPort int32 = 9081
 		instance.Spec.Variant = operator.TigeraSecureEnterprise
 		instance.Spec.NodeMetricsPort = &nodeMetricsPort
-		c, err := render.Calico(instance, nil, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
+		c, err := render.Calico(k8sServiceEp, instance, nil, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
 		Expect(err).To(BeNil(), "Expected Calico to create successfully %s", err)
 		Expect(componentCount(c.Render())).To(Equal(((6 + 4 + 2 + 7 + 5 + 1) + 1 + 1)))
 	})
@@ -110,7 +111,7 @@ var _ = Describe("Rendering tests", func() {
 		instance.Spec.Variant = operator.TigeraSecureEnterprise
 		instance.Spec.NodeMetricsPort = &nodeMetricsPort
 
-		c, err := render.Calico(instance, &operator.ManagementCluster{}, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
+		c, err := render.Calico(k8sServiceEp, instance, &operator.ManagementCluster{}, nil, nil, typhaNodeTLS, nil, nil, operator.ProviderNone, nil, false)
 		Expect(err).To(BeNil(), "Expected Calico to create successfully %s", err)
 
 		expectedResources := []struct {

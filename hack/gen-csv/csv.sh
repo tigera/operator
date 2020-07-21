27a53bc7149e5cb7f27f5518713fff4dc67455ee
Laurence Man <laurence@tigera.io>
2020-07-21 10:00:05 -0700

    Fix manifest downloading

--- hack/gen-csv/csv.sh
+++ hack/gen-csv/csv.sh
@@ -65,49 +65,8 @@ mkdir -p ${CSV_DIR}
 DEPLOY_DIR=${OUTPUT_DIR}/deploy
 mkdir -p ${DEPLOY_DIR}
 
-# Get the base path for the Calico docs site. This will be used to download manifests.
-CALICO_BASE_URL=https://docs.projectcalico.org
-CALICO_VERSION=$(yq read config/calico_versions.yml components.typha.version)
-
-# If Calico version is something other than master, then strip the patch version
-# numbers for both Calico and Enterprise and update the base urls.
-if [ "${CALICO_VERSION}" == "master" ]; then
-	CALICO_BASE_URL=${CALICO_BASE_URL}/master
-else
-	CALICO_VERSION=$(sed -e 's/\.[0-9]\+$//' <<< $CALICO_VERSION)
-	CALICO_BASE_URL=${CALICO_BASE_URL}/archive/${CALICO_VERSION}
-fi
-
-# Download operator manifests.
-curl ${CALICO_BASE_URL}/manifests/ocp/tigera-operator/02-tigera-operator.yaml --output ${DEPLOY_DIR}/operator.yaml
-curl ${CALICO_BASE_URL}/manifests/ocp/tigera-operator/02-role-tigera-operator.yaml --output ${DEPLOY_DIR}/role.yaml
-
-# Download the installation and tigerastatus CRDs.
-curl ${CALICO_BASE_URL}/manifests/ocp/crds/01-crd-installation.yaml --output ${CSV_DIR}/operator.tigera.io_installations_crd.yaml
-curl ${CALICO_BASE_URL}/manifests/ocp/crds/01-crd-tigerastatus.yaml --output ${CSV_DIR}/operator.tigera.io_tigerastatuses_crd.yaml
-
-CALICO_RESOURCES="
-bgpconfigurations
-bgppeers
-blockaffinities
-clusterinformations
-felixconfigurations
-globalnetworkpolicies
-globalnetworksets
-hostendpoints
-ipamblocks
-ipamconfigs
-ipamhandles
-ippools
-kubecontrollersconfigurations
-networkpolicies
-networksets
-"
-
-# Download the Calico CRDs into CSV dir.
-for resource in $CALICO_RESOURCES; do
-	curl ${CALICO_BASE_URL}/manifests/ocp/crds/calico/kdd/crd.projectcalico.org_${resource}.yaml --output ${CSV_DIR}/crd.projectcalico.org_${resource}.yaml
-done
+# Download operator and Calico manifests.
+source hack/gen-csv/get-manifests.sh
 
 CSV=${CSV_DIR}/tigera-operator.v${VERSION}.clusterserviceversion.yaml
 

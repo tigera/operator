77622602d46f5771518965778eb93045e34f8408
Erik Stidham <erik@tigera.io>
2020-07-22 10:42:53 -0500

    WIP

--- pkg/controller/migration/convert/network.go
+++ pkg/controller/migration/convert/network.go
@@ -16,24 +16,6 @@ const (
 )
 
 func handleNetwork(c *components, install *Installation) error {
-	// TODO: be more elegant about creation of this spec field if it already exists
-	if install.Spec.CalicoNetwork == nil {
-		install.Spec.CalicoNetwork = &operatorv1.CalicoNetworkSpec{}
-	}
-
-	// CALICO_NETWORKING_BACKEND
-	netBackend, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "CALICO_NETWORKING_BACKEND")
-	if err != nil {
-		return err
-	}
-	if netBackend != nil && *netBackend != "" && *netBackend != "bird" {
-		return ErrIncompatibleCluster{"only CALICO_NETWORKING_BACKEND=bird is supported at this time"}
-	}
-
-	// Calico CNI
-	if c.calicoCNIConfig == nil {
-		return fmt.Errorf("no 'calico' cni conf found in CNI_NETWORK_CONFIG on install-cni")
-	}
 
 	// FELIX_DEFAULTENDPOINTTOHOSTACTION
 	defaultWepAction, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "FELIX_DEFAULTENDPOINTTOHOSTACTION")
@@ -44,6 +26,36 @@ func handleNetwork(c *components, install *Installation) error {
 		return ErrIncompatibleCluster{
 			fmt.Sprintf("unexpected FELIX_DEFAULTENDPOINTTOHOSTACTION: '%s'. Only 'accept' is supported.", *defaultWepAction),
 		}
+	}
+
+	if c.calicoCNIConfig != nil {
+		return handleCalicoCNI(c, install)
+	} else {
+		return handleNoCNIConfiguration(c, install)
+	}
+
+}
+
+func handleCalicoCNI(c *components, install *Installation) error {
+
+	// CALICO_NETWORKING_BACKEND
+	netBackend, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "CALICO_NETWORKING_BACKEND")
+	if err != nil {
+		return err
+	}
+
+	if netBackend != nil && *netBackend != "" && *netBackend != "bird" {
+		return ErrIncompatibleCluster{"only CALICO_NETWORKING_BACKEND=bird is supported at this time"}
+	}
+
+	// Calico CNI
+	if c.calicoCNIConfig == nil {
+		return ErrIncompatibleCluster{"no 'calico' cni conf found in CNI_NETWORK_CONFIG on install-cni"}
+	}
+
+	// TODO: be more elegant about creation of this spec field if it already exists
+	if install.Spec.CalicoNetwork == nil {
+		install.Spec.CalicoNetwork = &operatorv1.CalicoNetworkSpec{}
 	}
 
 	// IP
@@ -79,11 +91,6 @@ func handleNetwork(c *components, install *Installation) error {
 		install.Spec.CalicoNetwork.HostPorts = &hp
 	}
 
-	if c.calicoCNIConfig == nil {
-		// TODO: don't return an error once we support this, instead just returning nil.
-		return ErrIncompatibleCluster{"operator does not yet support running without calico CNI"}
-	}
-
 	if c.calicoCNIConfig.MTU == -1 {
 		// if MTU is -1, we assume it was us who replaced it when doing initial CNI
 		// config loading. We need to pull it from the correct source
@@ -116,6 +123,115 @@ func handleNetwork(c *components, install *Installation) error {
 		return ErrIncompatibleCluster{"IpAddrsNoIpam not supported"}
 	}
 
+	return nil
+}
+
+func handleNoCNIConfiguration(c *components, install *Installation) error {
+	c := getContainer(c.node.Spec.Template.Spec, containerInstallCNI)
+	if c != nil {
+		return ErrIncompatibleCluster{
+			fmt.Sprintf("%s container on %s is not supported when there is no CNI configuration",
+				containerInstallCNI,
+				c.node.Name),
+		}
+	}
+
+	// CALICO_NETWORKING_BACKEND
+	netBackend, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "CALICO_NETWORKING_BACKEND")
+	if err != nil {
+		return err
+	}
+	if netBackend == nil || *netBackend != "none" {
+		return ErrIncompatibleCluster{"with no CNI config, only CALICO_NETWORKING_BACKEND=none is supported"}
+	}
+
+	// FELIX_IPTABLESMANGLEALLOWACTION
+	mangleAllow, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "FELIX_IPTABLESMANGLEALLOWACTION")
+	if err != nil {
+		return err
+	}
+
+	// FELIX_IPTABLESFILTERALLOWACTION
+	filterAllow, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "FELIX_IPTABLESFILTERALLOWACTION")
+	if err != nil {
+		return err
+	}
+
+	// FELIX_INTERFACEPREFIX
+	prefix, err := c.node.getEnv(ctx, c.client, containerCalicoNode, "FELIX_INTERFACEPREFIX")
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case prefix == nil:
+		return ErrIncompatibleCluster{
+			fmt.Sprintf("with no CNI config, expected FELIX_INTERFACEPREFIX to be set", prefix),
+		}
+	case *prefix == "eni":
+		install.Spec.CNI.Type = PluginAmazonVPC
+		// For AmazonVPC, check that IPTABLESMANGLE is set Return
+		switch {
+		case mangleAllow == nil:
+			return ErrIncompatibleCluster{"detected AmazonVPC CNI plugin, FELIX_IPTABLESMANGLEALLOWACTION was unset, expected it to be 'Return'."}
+		case *mangleAllow == "Return":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf("detected AmazonVPC CNI plugin, FELIX_IPTABLESMANGLEALLOWACTION was %s, expected it to be 'Return'.", *mangleAllow)}
+		}
+		// For AmazonVPC, check that IPTABLESFILTER is not set or the default Accept
+		switch {
+		case filterAllow == nil:
+		case *filterAllow == "Accept":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf(
+				"detected AmazonVPC CNI plugin, FELIX_IPTABLESFILTERALLOWACTION was set to %s, expected it to be unset or 'Accept'.",
+				*filterAllow)}
+		}
+	case *prefix == "avz":
+		install.Spec.CNI.Type = PluginAzureVNET
+		// For AzureVNET, check that IPTABLESMANGLE is not set or the default Accept
+		switch {
+		case mangleAllow == nil:
+		case *mangleAllow == "Accept":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf(
+				"detected AzureVNET CNI plugin, FELIX_IPTABLESMANGLEALLOWACTION was set to %s, expected it to be unset or 'Accept'.",
+				*mangleAllow)}
+		}
+		// For AzureVNET, check that IPTABLESFILTER is not set or the default Accept
+		switch {
+		case filterAllow == nil:
+		case *filterAllow == "Accept":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf(
+				"detected AzureVNET CNI plugin, FELIX_IPTABLESFILTERALLOWACTION was set to %s, expected it to be unset or 'Accept'.",
+				*filterAllow)}
+		}
+	case *prefix == "gke":
+		install.Spec.CNI.Type = PluginGKE
+		// For GKE, check that IPTABLESMANGLE is set Return
+		switch {
+		case mangleAllow == nil:
+			return ErrIncompatibleCluster{"detected GKE CNI plugin, FELIX_IPTABLESMANGLEALLOWACTION was unset, expected it to be 'Return'."}
+		case *mangleAllow == "Return":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf("detected GKE CNI plugin, FELIX_IPTABLESMANGLEALLOWACTION was %s, expected it to be 'Return'.", *mangleAllow)}
+		}
+		// For GKE, check that IPTABLESFILTER is set Return
+		switch {
+		case filterAllow == nil:
+			return ErrIncompatibleCluster{"detected GKE CNI plugin, FELIX_IPTABLESFILTERALLOWACTION was unset, expected it to be 'Return'."}
+		case *filterAllow == "Return":
+		default:
+			return ErrIncompatibleCluster{fmt.Sprintf(
+				"detected GKE CNI plugin, FELIX_IPTABLESFILTERALLOWACTION was set to %s, expected it to be 'Return'.",
+				*filterAllow)}
+		}
+	default:
+		return ErrIncompatibleCluster{
+			fmt.Sprintf("unexpected FELIX_INTERFACEPREFIX value: '%s'. Only 'eni, avz, gke' are supported.", prefix),
+		}
+	}
 	return nil
 }
 

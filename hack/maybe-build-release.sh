10067496da8691f8012ec169b03a2d812f1d37f2
Laurence Man <laurence@tigera.io>
2020-07-24 13:04:28 -0700

    Tag and push release images to RedHat Connect for validation (#688)

--- hack/maybe-build-release.sh
+++ hack/maybe-build-release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 tag=$(git describe --exact-match --tags HEAD)
 if [[ -z "${tag}" ]]; then
@@ -11,3 +12,33 @@ make release VERSION=${tag}
 
 echo "Publish release ${tag}"
 make release-publish-images VERSION=${tag}
+
+echo "Tagging and pushing operator images to RedHat Connect for certification..."
+
+docker login -u unused scan.connect.redhat.com --password-stdin <<< ${OPERATOR_RH_REGISTRY_KEY}
+redhatImage=scan.connect.redhat.com/$OPERATOR_RH_PROJECTID/operator:$VERSION
+
+# Pushes to scan.connect.redhat.com fail if the image exists already.
+# If it already exists, skip tagging and pushing.
+if ! docker pull $redhatImage 2>/dev/null; then
+	echo "Tagging and pushing operator image..."
+	quayImage=quay.io/tigera/operator:$VERSION
+	docker pull $quayImage
+	docker tag $quayImage $redhatImage
+	docker push $redhatImage
+else
+	echo "operator image exists on scan.connect.redhat.com, skipping tagging/pushing"
+fi
+
+docker login -u unused scan.connect.redhat.com --password-stdin <<< ${OPERATOR_INIT_RH_REGISTRY_KEY}
+redhatImage=scan.connect.redhat.com/$OPERATOR_INIT_RH_PROJECTID/operator-init:$VERSION
+
+if ! docker pull $redhatImage 2>/dev/null; then
+	echo "tagging and pushing operator-init image..."
+	quayImage=quay.io/tigera/operator-init:$VERSION
+	docker pull $quayImage
+	docker tag $quayImage $redhatImage
+	docker push $redhatImage
+else
+	echo "operator-init image exists on scan.connect.redhat.com, skipping tagging/pushing"
+fi

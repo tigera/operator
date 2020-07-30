d87c9e840bc51ce9bcd9be3ee6bd299f83da10da
ozdanborne <dan@projectcalico.org>
2020-07-30 13:03:44 -0400

    make typha and kubecontrollers optional

--- pkg/controller/migration/convert/convert_test.go
+++ pkg/controller/migration/convert/convert_test.go
@@ -304,7 +304,7 @@ func emptyComponents() components {
 			*emptyNodeSpec(),
 			make(map[string]checkedFields),
 		},
-		kubeControllers: *emptyKubeControllerSpec(),
-		typha:           *emptyTyphaDeployment(),
+		kubeControllers: emptyKubeControllerSpec(),
+		typha:           emptyTyphaDeployment(),
 	}
 }

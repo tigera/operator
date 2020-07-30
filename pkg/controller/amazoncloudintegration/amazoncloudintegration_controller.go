d70618689f21739b3d67255444d804fcbcb62f26
Karthik Ramasubramanian <karthik@tigera.io>
2020-07-30 16:49:57 -0700

    Add support for only reconciling our desired annotations
    Annotations have other use cases by other controllers and systems.
    Instead of going into a reconcile battle with other controllers, we only
    reconcile annotations that our components specify and leave other
    annotations as-is.
    
    This also removes support for the earlier concept of allowed
    annotations, which explicity allowed only some pre determined
    annotations.

--- pkg/controller/amazoncloudintegration/amazoncloudintegration_controller.go
+++ pkg/controller/amazoncloudintegration/amazoncloudintegration_controller.go
@@ -187,7 +187,7 @@ func (r *ReconcileAmazonCloudIntegration) Reconcile(request reconcile.Request) (
 		return reconcile.Result{}, err
 	}
 
-	if err := handler.CreateOrUpdate(context.Background(), component, r.status, utils.NoUserAddedMetadata); err != nil {
+	if err := handler.CreateOrUpdate(context.Background(), component, r.status); err != nil {
 		r.SetDegraded("Error creating / updating resource", err, reqLogger)
 		return reconcile.Result{}, err
 	}

1237be4be573a58bae1e41d5d2ddc79973e597e5
ozdanborne <dan@projectcalico.org>
2020-07-31 16:17:29 -0400

    feedback

--- pkg/controller/migration/convert/mtu.go
+++ pkg/controller/migration/convert/mtu.go
@@ -16,7 +16,7 @@ func handleMTU(c *components, install *Installation) error {
 	for _, src := range []string{"FELIX_IPINIPMTU", "FELIX_VXLANMTU", "FELIX_WIREGUARDMTU"} {
 		mtu, err := getMTU(c, containerCalicoNode, src)
 		if err != nil {
-			return fmt.Errorf("failed to parse %s: %v", src, err)
+			return ErrIncompatibleCluster{fmt.Sprintf("failed to parse mtu from %s: %v", src, err)}
 		}
 
 		// if this mtu source is not set, ignore
@@ -26,7 +26,7 @@ func handleMTU(c *components, install *Installation) error {
 
 		// compare against current mtu.
 		if curMTU != nil && *curMTU != *mtu {
-			return fmt.Errorf("detected mtu %s=%d does not match detected mtu %s=%d", src, *mtu, curMTUSrc, *curMTU)
+			return ErrIncompatibleCluster{fmt.Sprintf("mtu %s=%d does not match mtu %s=%d", src, *mtu, curMTUSrc, *curMTU)}
 		}
 
 		curMTU, curMTUSrc = mtu, src
@@ -36,9 +36,10 @@ func handleMTU(c *components, install *Installation) error {
 		if c.calicoCNIConfig.MTU == -1 {
 			// if MTU is -1, we assume it was us who replaced it when doing initial CNI
 			// config loading. We need to pull it from the correct source
-			mtu, err := getMTU(c, containerInstallCNI, "CNI_MTU")
+			var src = "CNI_MTU"
+			mtu, err := getMTU(c, containerInstallCNI, src)
 			if err != nil {
-				return err
+				return ErrIncompatibleCluster{fmt.Sprintf("failed to parse %s: %v", src, err)}
 			}
 
 			if mtu == nil {
@@ -49,7 +50,7 @@ func handleMTU(c *components, install *Installation) error {
 
 			// compare against current mtu.
 			if curMTU != nil && *curMTU != *mtu {
-				return fmt.Errorf("detected mtu %s=%d does not match detected mtu %s=%d", "CNI_MTU", *mtu, curMTUSrc, *curMTU)
+				return ErrIncompatibleCluster{fmt.Sprintf("mtu %s=%d does not match mtu %s=%d", src, *mtu, curMTUSrc, *curMTU)}
 			}
 			curMTU, curMTUSrc = mtu, "CNI_MTU"
 
@@ -58,7 +59,8 @@ func handleMTU(c *components, install *Installation) error {
 			// use the hardcoded value.
 			mtu := int32(c.calicoCNIConfig.MTU)
 			if curMTU != nil && *curMTU != mtu {
-				return ErrIncompatibleCluster{"MTUs across IPIP, VXLAN, and Wireguard must match."}
+				// return ErrIncompatibleCluster{"MTUs across IPIP, VXLAN, and Wireguard must match."}
+				return ErrIncompatibleCluster{fmt.Sprintf("mtu '%d' specified in CNI config does not match mtu %s=%d", mtu, curMTUSrc, *curMTU)}
 			}
 			// set detected MTU for later comparison against other sources
 			curMTU = &mtu
@@ -75,7 +77,9 @@ func handleMTU(c *components, install *Installation) error {
 	return nil
 }
 
-// getMTU retrieves an mtu value from the calico-node container, returning the
+// getMTU retrieves an mtu value from an env var on a container.
+// if the specified env var does not exist, it will return nil.
+// since env vars are strings, this function also parses it into an int32 pointer.
 func getMTU(c *components, container, key string) (*int32, error) {
 	m, err := c.node.getEnv(ctx, c.client, container, key)
 	if err != nil {
@@ -86,7 +90,6 @@ func getMTU(c *components, container, key string) (*int32, error) {
 		return nil, nil
 	}
 
-	// if it's not nil, convert it to a number, and compare it against the currently detected mtu
 	i, err := strconv.ParseInt(*m, 10, 32)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't convert %s to integer: %v", *m, err)

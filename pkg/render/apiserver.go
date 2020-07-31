572e864c8a9c8c7e9f3d2e72f4f47c681c7778d0
Casey Davenport <caseydavenport@users.noreply.github.com>
2020-07-31 14:50:15 -0700

    Fix DNS policy for host-networked pods (#790)

--- pkg/render/apiserver.go
+++ pkg/render/apiserver.go
@@ -542,12 +542,16 @@ func (c *apiServerComponent) apiServer() *appsv1.Deployment {
 	var replicas int32 = 1
 
 	hostNetwork := false
+	dnsPolicy := corev1.DNSClusterFirst
 	if c.installation.Spec.KubernetesProvider == operator.ProviderEKS &&
 		c.installation.Spec.CNI.Type == operator.PluginCalico {
 		// Workaround the fact that webhooks don't work for non-host-networked pods
 		// when in this networking mode on EKS, because the control plane nodes don't run
 		// Calico.
 		hostNetwork = true
+
+		// Adjust DNS policy so we can access in-cluster services.
+		dnsPolicy = corev1.DNSClusterFirstWithHostNet
 	}
 
 	d := &appsv1.Deployment{
@@ -577,6 +581,7 @@ func (c *apiServerComponent) apiServer() *appsv1.Deployment {
 					Annotations: c.tlsAnnotations,
 				},
 				Spec: corev1.PodSpec{
+					DNSPolicy: dnsPolicy,
 					NodeSelector: map[string]string{
 						"kubernetes.io/os": "linux",
 					},

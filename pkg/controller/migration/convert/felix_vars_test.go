3f45de70ce0a371d6adb39b50b7f2b503bf7779d
ozdanborne <dan@projectcalico.org>
2020-08-18 14:48:41 -0400

    type conversion tests

--- pkg/controller/migration/convert/felix_vars_test.go
+++ pkg/controller/migration/convert/felix_vars_test.go
@@ -1,8 +1,11 @@
 package convert
 
 import (
+	"time"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("Parser", func() {
@@ -23,6 +26,16 @@ var _ = Describe("Parser", func() {
 			Op:    "replace",
 			Path:  "/spec/useInternalDataplaneDriver",
 			Value: true,
+		}))
+	})
+
+	It("should convert a duration", func() {
+		fe, err := patchFromVal("routerefreshinterval", "4s")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(fe).To(Equal(patch{
+			Op:    "replace",
+			Path:  "/spec/routeRefreshInterval",
+			Value: metav1.Duration{4 * time.Second},
 		}))
 	})
 })

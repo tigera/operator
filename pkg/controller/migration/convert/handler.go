a1961cce6f65667bd9580bf983e36779d60a6a16
Dan (Turk) Osborne <turk@tigera.io>
2020-09-09 09:37:32 -0400

    remove custom Install type, use operatorv1.Installation (#870)

--- pkg/controller/migration/convert/handler.go
+++ pkg/controller/migration/convert/handler.go
@@ -1,5 +1,7 @@
 package convert
 
+import operatorv1 "github.com/tigera/operator/pkg/apis/operator/v1"
+
 // handlers are grouped by feature or product and check various
 // fields on Calico components to construct a Installation resource that
 // represents the currently install resources.
@@ -7,7 +9,7 @@ package convert
 // - mark incompatible clusters by returning a IncompatibleClusterError
 // - carry user config forward by setting the Installation resource according to their installed config
 // - mark variables as 'checked' so that the final env var catch-all doesn't throw an 'unexpected env var' error
-type handler func(*components, *Installation) error
+type handler func(*components, *operatorv1.Installation) error
 
 var handlers = []handler{
 	handleNetwork,

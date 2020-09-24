1b028c020b2cd36ed63cdf111f06dddddc86ce5c
Erik Stidham <erik@tigera.io>
2020-09-24 10:14:15 -0500

    Update generated files

--- pkg/apis/operator/v1beta1/zz_generated.deepcopy.go
+++ pkg/apis/operator/v1beta1/zz_generated.deepcopy.go
@@ -40,7 +40,7 @@ func (in *AmazonCloudIntegration) DeepCopyObject() runtime.Object {
 func (in *AmazonCloudIntegrationList) DeepCopyInto(out *AmazonCloudIntegrationList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]AmazonCloudIntegration, len(*in))

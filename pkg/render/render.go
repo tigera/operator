0f959be9aa76201e620e3cc5dafe6c609156c654
Rene Dekker <rene@tigera.io>
2020-10-27 13:41:56 -0700

    [master] Dex PR follow-up (#966)
    * Log error when fetching installation leads to an error.
    
    * Deprecate Manager.Auth, but add Status.State.
    
    * Fix issue where MCM was blocked by secret
    
    * Use core controller pattern of fetching->filling->updating->...-> rather than abusing the GetAuthentication for this
    
    * CRD update for manager
    
    * Dissolve the K8sAttributes interface for now, until we have a plan for a complete rollout
    
    * Copy pull secrets over.
    
    * Authentication_controller added missing err handling.

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -250,16 +250,3 @@ func appendNotNil(components []Component, c Component) []Component {
 	}
 	return components
 }
-
-type K8sAttributes interface {
-	// RequiredEnv returns env that is used to configure pods with dex options.
-	RequiredEnv(prefix string) []corev1.EnvVar
-	// RequiredAnnotations returns annotations that make your the pods get refreshed if any of the config/secrets change.
-	RequiredAnnotations() map[string]string
-	// RequiredSecrets returns secrets that you need to render for dex.
-	RequiredSecrets(namespace string) []*corev1.Secret
-	// RequiredVolumeMounts returns volume mounts that are related to dex.
-	RequiredVolumeMounts() []corev1.VolumeMount
-	// RequiredVolumes returns volumes that are related to dex.
-	RequiredVolumes() []corev1.Volume
-}

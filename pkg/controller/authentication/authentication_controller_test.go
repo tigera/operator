0a92c73656a78b6bded48f36727dd0bc734c1700
Rene Dekker <rene@tigera.io>
2020-11-02 13:52:35 -0800

    Add new flag to Authentication spec in order to support OKTA

--- pkg/controller/authentication/authentication_controller_test.go
+++ pkg/controller/authentication/authentication_controller_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2019 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package authentication_test
 
 import (
@@ -83,6 +97,7 @@ var _ = Describe("authentication controller tests", func() {
 			})).ToNot(HaveOccurred())
 
 			Expect(cli.Create(ctx, idpSecret)).ToNot(HaveOccurred())
+			Expect(cli.Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "tigera-dex"}})).ToNot(HaveOccurred())
 
 			// Apply an authentication spec that triggers all the logic in the updateAuthenticationWithDefaults() func.
 			Expect(cli.Create(ctx, &operatorv1.Authentication{

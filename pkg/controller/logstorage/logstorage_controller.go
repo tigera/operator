6e8629a09a22af20225d15e174d5942545de1728
ozdanborne <dan@projectcalico.org>
2020-11-23 14:15:39 -0500

    compute installation instead of reading from status

--- pkg/controller/logstorage/logstorage_controller.go
+++ pkg/controller/logstorage/logstorage_controller.go
@@ -318,7 +318,7 @@ func (r *ReconcileLogStorage) Reconcile(request reconcile.Request) (reconcile.Re
 		r.status.OnCRFound()
 	}
 
-	installationCR, err := installation.GetInstallation(context.Background(), r.client)
+	variant, installationCR, err := installation.GetInstallation(context.Background(), r.client)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			r.status.SetDegraded("Installation not found", err.Error())
@@ -350,7 +350,7 @@ func (r *ReconcileLogStorage) Reconcile(request reconcile.Request) (reconcile.Re
 	}
 
 	// These checks ensure that we're in the correct state to continue to the render function without causing a panic
-	if installationCR.Status.Variant != operatorv1.TigeraSecureEnterprise {
+	if variant != operatorv1.TigeraSecureEnterprise {
 		r.status.SetDegraded(fmt.Sprintf("Waiting for network to be %s", operatorv1.TigeraSecureEnterprise), "")
 		return reconcile.Result{}, nil
 	} else if ls == nil && managementClusterConnection == nil {
@@ -501,7 +501,7 @@ func (r *ReconcileLogStorage) Reconcile(request reconcile.Request) (reconcile.Re
 
 	component := render.LogStorage(
 		ls,
-		installationCR.Status.Computed,
+		installationCR,
 		managementCluster,
 		managementClusterConnection,
 		elasticsearch,

0f1ddf65647d2dc5316d82564ccb8921f321849e
Suraiya Hameed <22776421+Suraiya-Hameed@users.noreply.github.com>
2020-12-21 15:49:26 -0800

    format file - make fix

--- pkg/controller/utils/utils_test.go
+++ pkg/controller/utils/utils_test.go
@@ -16,6 +16,7 @@ package utils
 
 import (
 	"context"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/tigera/operator/pkg/apis"
@@ -25,6 +26,7 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	batchv1beta "k8s.io/api/batch/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"

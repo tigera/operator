dd47de5c9780340d901b5280b28196832abd9203
Casey Davenport <davenport.cas@gmail.com>
2021-02-03 16:16:20 -0800

    Update to v3.16.7

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -18,27 +18,27 @@ import "github.com/tigera/operator/version"
 
 var (
 	ComponentCalicoCNI = component{
-		Version: "v3.16.6",
+		Version: "v3.16.7",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoKubeControllers = component{
-		Version: "v3.16.6",
+		Version: "v3.16.7",
 		Image:   "calico/kube-controllers",
 	}
 
 	ComponentCalicoNode = component{
-		Version: "v3.16.6",
+		Version: "v3.16.7",
 		Image:   "calico/node",
 	}
 
 	ComponentCalicoTypha = component{
-		Version: "v3.16.6",
+		Version: "v3.16.7",
 		Image:   "calico/typha",
 	}
 
 	ComponentFlexVolume = component{
-		Version: "v3.16.6",
+		Version: "v3.16.7",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 

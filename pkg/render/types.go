d751554b3e45d625c4952e7a1cc8a446db6d7b12
Rene Dekker <rene@tigera.io>
2021-02-05 15:14:16 -0800

    Remove CountTrues fn. Add configuration test for dex.

--- pkg/render/types.go
+++ pkg/render/types.go
@@ -7,14 +7,3 @@ func Bool(b bool) *bool {
 func Int64(i int64) *int64 {
 	return &i
 }
-
-// Count the number of true booleans among the input arguments.
-func CountTrues(bools ...bool) int8 {
-	var sum int8 = 0
-	for _, b := range bools {
-		if b {
-			sum++
-		}
-	}
-	return sum
-}

acb09fe93e42c464597654157b6230bb640c2933
Alina Militaru <asincu@users.noreply.github.com>
2021-02-24 00:48:57 -0800

    Adding watches and rollback logic

--- pkg/controller/logcollector/logcollector_controller.go
+++ pkg/controller/logcollector/logcollector_controller.go
@@ -113,6 +113,10 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	if err != nil {
 		return fmt.Errorf("logcollector-controller failed to watch the node resource: %w", err)
 	}
+
+	if err = utils.AddLicenseWatch(c); err != nil {
+		return fmt.Errorf("logcollector-controller failed to watch LicenseKey resource: %v", err)
+	}
 	return nil
 }
 
@@ -189,10 +193,10 @@ func (r *ReconcileLogCollector) Reconcile(ctx context.Context, request reconcile
 	if err != nil {
 		if errors.IsNotFound(err) {
 			r.status.SetDegraded("License not found", err.Error())
-			return reconcile.Result{RequeueAfter: 10 * time.Second}, nil
+			return reconcile.Result{}, nil
 		}
 		r.status.SetDegraded("Error querying license", err.Error())
-		return reconcile.Result{RequeueAfter: 10 * time.Second}, nil
+		return reconcile.Result{}, nil
 	}
 
 	// Fetch the Installation instance. We need this for a few reasons.
@@ -238,7 +242,7 @@ func (r *ReconcileLogCollector) Reconcile(ctx context.Context, request reconcile
 		return reconcile.Result{}, err
 	}
 
-	var exportLogs = utils.IsFeatureActive(license, common.ExportLogs)
+	var exportLogs = utils.IsFeatureActive(license, common.ExportLogsFeature)
 	var s3Credential *render.S3Credential
 	if exportLogs && instance.Spec.AdditionalStores != nil {
 		if instance.Spec.AdditionalStores.S3 != nil {
@@ -388,6 +392,7 @@ func (r *ReconcileLogCollector) Reconcile(ctx context.Context, request reconcile
 		installation,
 		r.clusterDomain,
 		rmeta.OSTypeLinux,
+		exportLogs,
 	)
 
 	if err = imageset.ApplyImageSet(ctx, r.client, variant, component); err != nil {
@@ -420,6 +425,7 @@ func (r *ReconcileLogCollector) Reconcile(ctx context.Context, request reconcile
 			installation,
 			r.clusterDomain,
 			rmeta.OSTypeWindows,
+			exportLogs,
 		)
 
 		if err = imageset.ApplyImageSet(ctx, r.client, variant, component); err != nil {

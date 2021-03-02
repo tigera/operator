ed77f8e551529a2229b5903181321e0a21f3c82b
Laurence Man <laurence@tigera.io>
2021-03-01 16:48:51 -0800

    Revert "Revert "[cherry-pick v1.14] remove OIDC elsticsearch user configmap and secret" (#1173)"
    This reverts commit cc58f99257fedbccd4f67794827acb40fe43e468.

--- pkg/render/logstorage.go
+++ pkg/render/logstorage.go
@@ -159,9 +159,7 @@ func LogStorage(
 	clusterDomain string,
 	applyTrial bool,
 	dexCfg DexRelyingPartyConfig,
-	elasticLicenseType ElasticsearchLicenseType,
-	oidcUserConfigMap *corev1.ConfigMap,
-	oidcUserSecret *corev1.Secret) Component {
+	elasticLicenseType ElasticsearchLicenseType) Component {
 
 	return &elasticsearchComponent{
 		logStorage:                  logStorage,
@@ -182,8 +180,6 @@ func LogStorage(
 		applyTrial:                  applyTrial,
 		dexCfg:                      dexCfg,
 		elasticLicenseType:          elasticLicenseType,
-		oidcUserConfigMap:           oidcUserConfigMap,
-		oidcUserSecret:              oidcUserSecret,
 	}
 }
 
@@ -206,8 +202,6 @@ type elasticsearchComponent struct {
 	applyTrial                  bool
 	dexCfg                      DexRelyingPartyConfig
 	elasticLicenseType          ElasticsearchLicenseType
-	oidcUserConfigMap           *corev1.ConfigMap
-	oidcUserSecret              *corev1.Secret
 	esImage                     string
 	esOperatorImage             string
 	kibanaImage                 string
@@ -378,17 +372,6 @@ func (es *elasticsearchComponent) Objects() ([]client.Object, []client.Object) {
 
 		toCreate = append(toCreate, es.oidcUserRole()...)
 		toCreate = append(toCreate, es.oidcUserRoleBinding()...)
-
-		// OIDCUsersConfigMapName and OIDCUsersEsSecreteName should be created only once
-		// when the external IdP is set and Elasticsearch uses basic license.
-		// If external IdP is not configured or if Elasticsearch is not using Basic license, delete these objects if available.
-		if es.elasticLicenseType == ElasticsearchLicenseTypeBasic && es.dexCfg != nil {
-			toCreate = append(toCreate, es.oidcUserConfigMap)
-			toCreate = append(toCreate, es.oidcUserSecret)
-		} else {
-			toDelete = append(toDelete, es.oidcUserConfigMap)
-			toDelete = append(toDelete, es.oidcUserSecret)
-		}
 
 		// If we converted from a ManagedCluster to a Standalone or Management then we need to delete the elasticsearch
 		// service as it differs between these cluster types
@@ -1480,13 +1463,13 @@ func (es elasticsearchComponent) oidcUserRole() []client.Object {
 					APIGroups:     []string{""},
 					Resources:     []string{"configmaps"},
 					ResourceNames: []string{OIDCUsersConfigMapName},
-					Verbs:         []string{"get", "list", "watch"},
+					Verbs:         []string{"get", "list", "watch", "create", "delete"},
 				},
 				{
 					APIGroups:     []string{""},
 					Resources:     []string{"secrets"},
 					ResourceNames: []string{OIDCUsersEsSecreteName},
-					Verbs:         []string{"update", "get"},
+					Verbs:         []string{"get", "list", "watch", "create", "update", "delete"},
 				},
 			},
 		},

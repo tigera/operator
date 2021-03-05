1477a8ddf2b850e35c3ab78bf656c9b82b09b343
Erik Stidham <erik@tigera.io>
2021-03-05 15:06:52 -0600

    Drop encapsulation default change

--- pkg/controller/installation/core_controller.go
+++ pkg/controller/installation/core_controller.go
@@ -470,7 +470,7 @@ func fillDefaults(instance *operator.Installation) error {
 
 		if v4pool != nil {
 			if v4pool.Encapsulation == "" {
-				if instance.Spec.CNI.Type == operator.PluginCalico && instance.Spec.CNI.IPAM.Type == operator.IPAMPluginCalico {
+				if instance.Spec.CNI.Type == operator.PluginCalico {
 					v4pool.Encapsulation = operator.EncapsulationIPIP
 				} else {
 					v4pool.Encapsulation = operator.EncapsulationNone

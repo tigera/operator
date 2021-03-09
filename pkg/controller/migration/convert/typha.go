14abe1a8bec74aaa2d553977759463e2919e4767
Erik Stidham <erik@tigera.io>
2021-03-09 15:41:28 -0600

    Add Typha prometheus config

--- pkg/controller/migration/convert/typha.go
+++ pkg/controller/migration/convert/typha.go
@@ -2,11 +2,17 @@ package convert
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 
 	operatorv1 "github.com/tigera/operator/api/v1"
 	"github.com/tigera/operator/pkg/controller/utils"
+)
+
+const (
+	containerTypha = "calico-typha"
 )
 
 func checkTypha(c *components, _ *operatorv1.Installation) error {
@@ -39,4 +45,38 @@ func checkTypha(c *components, _ *operatorv1.Installation) error {
 			nodeCount, curReplicas, utils.GetExpectedTyphaScale(nodeCount)),
 		component: ComponentTypha,
 	}
+}
+
+// handleTyphaMetrics is a migration handler which detects custom prometheus settings for typha and
+// carries those options forward via the TyphaMetricsPort field.
+func handleTyphaMetrics(c *components, install *operatorv1.Installation) error {
+	if c.typha == nil {
+		return nil
+	}
+	metricsEnabled, err := getEnv(ctx, c.client, c.typha.Spec.Template.Spec, ComponentTypha, containerTypha, "TYPHA_PROMETHEUSMETRICSENABLED")
+	if err != nil {
+		return err
+	}
+	if metricsEnabled != nil && strings.ToLower(*metricsEnabled) == "true" {
+		var _9091 int32 = 9091
+		install.Spec.TyphaMetricsPort = &_9091
+		port, err := getEnv(ctx, c.client, c.typha.Spec.Template.Spec, ComponentTypha, containerTypha, "TYPHA_PROMETHEUSMETRICSPORT")
+		if err != nil {
+			return err
+		}
+		if port != nil {
+			p, err := strconv.ParseInt(*port, 10, 32)
+			if err != nil || p <= 0 || p > 65535 {
+				return ErrIncompatibleCluster{
+					err:       fmt.Sprintf("invalid port defined in TYPHA_PROMETHEUSMETRICSPORT=%s", *port),
+					component: ComponentTypha,
+					fix:       "adjust it to be within the range of 1-65535 or remove the env var",
+				}
+			}
+			i := int32(p)
+			install.Spec.TyphaMetricsPort = &i
+		}
+	}
+
+	return nil
 }

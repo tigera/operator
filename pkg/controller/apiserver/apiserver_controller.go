a51e5b07bb7095b2a7e2dbc702d0fff37a938e11
Casey Davenport <davenport.cas@gmail.com>
2021-03-16 11:31:11 -0700

    Changes to run API server for Calico

--- pkg/controller/apiserver/apiserver_controller.go
+++ pkg/controller/apiserver/apiserver_controller.go
@@ -47,22 +47,19 @@ var log = logf.Log.WithName("controller_apiserver")
 // Add creates a new APIServer Controller and adds it to the Manager. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager, opts options.AddOptions) error {
-	if !opts.EnterpriseCRDExists {
-		// No need to start this controller.
-		return nil
-	}
 	return add(mgr, newReconciler(mgr, opts))
 }
 
 // newReconciler returns a new reconcile.Reconciler
 func newReconciler(mgr manager.Manager, opts options.AddOptions) *ReconcileAPIServer {
 	r := &ReconcileAPIServer{
-		client:          mgr.GetClient(),
-		scheme:          mgr.GetScheme(),
-		provider:        opts.DetectedProvider,
-		amazonCRDExists: opts.AmazonCRDExists,
-		status:          status.New(mgr.GetClient(), "apiserver", opts.KubernetesVersion),
-		clusterDomain:   opts.ClusterDomain,
+		client:              mgr.GetClient(),
+		scheme:              mgr.GetScheme(),
+		provider:            opts.DetectedProvider,
+		amazonCRDExists:     opts.AmazonCRDExists,
+		enterpriseCRDsExist: opts.EnterpriseCRDExists,
+		status:              status.New(mgr.GetClient(), "apiserver", opts.KubernetesVersion),
+		clusterDomain:       opts.ClusterDomain,
 	}
 	r.status.Run()
 	return r
@@ -110,16 +107,18 @@ func add(mgr manager.Manager, r *ReconcileAPIServer) error {
 		}
 	}
 
-	// Watch for changes to primary resource ManagementCluster
-	err = c.Watch(&source.Kind{Type: &operatorv1.ManagementCluster{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return fmt.Errorf("apiserver-controller failed to watch primary resource: %v", err)
-	}
+	if r.enterpriseCRDsExist {
+		// Watch for changes to primary resource ManagementCluster
+		err = c.Watch(&source.Kind{Type: &operatorv1.ManagementCluster{}}, &handler.EnqueueRequestForObject{})
+		if err != nil {
+			return fmt.Errorf("apiserver-controller failed to watch primary resource: %v", err)
+		}
 
-	// Watch for changes to primary resource ManagementClusterConnection
-	err = c.Watch(&source.Kind{Type: &operatorv1.ManagementClusterConnection{}}, &handler.EnqueueRequestForObject{})
-	if err != nil {
-		return fmt.Errorf("apiserver-controller failed to watch primary resource: %v", err)
+		// Watch for changes to primary resource ManagementClusterConnection
+		err = c.Watch(&source.Kind{Type: &operatorv1.ManagementClusterConnection{}}, &handler.EnqueueRequestForObject{})
+		if err != nil {
+			return fmt.Errorf("apiserver-controller failed to watch primary resource: %v", err)
+		}
 	}
 
 	if err = imageset.AddImageSetWatch(c); err != nil {
@@ -137,12 +136,13 @@ var _ reconcile.Reconciler = &ReconcileAPIServer{}
 type ReconcileAPIServer struct {
 	// This client, initialized using mgr.Client() above, is a split client
 	// that reads objects from the cache and writes to the apiserver
-	client          client.Client
-	scheme          *runtime.Scheme
-	provider        operatorv1.Provider
-	amazonCRDExists bool
-	status          status.StatusManager
-	clusterDomain   string
+	client              client.Client
+	scheme              *runtime.Scheme
+	provider            operatorv1.Provider
+	amazonCRDExists     bool
+	enterpriseCRDsExist bool
+	status              status.StatusManager
+	clusterDomain       string
 }
 
 // Reconcile reads that state of the cluster for a APIServer object and makes changes based on the state read
@@ -180,10 +180,6 @@ func (r *ReconcileAPIServer) Reconcile(ctx context.Context, request reconcile.Re
 		r.status.SetDegraded("Error querying installation", err.Error())
 		return reconcile.Result{}, err
 	}
-	if variant != operatorv1.TigeraSecureEnterprise {
-		r.status.SetDegraded(fmt.Sprintf("Waiting for network to be %s", operatorv1.TigeraSecureEnterprise), "")
-		return reconcile.Result{}, nil
-	}
 
 	var tlsSecret *v1.Secret
 	if network.CertificateManagement == nil {
@@ -208,42 +204,6 @@ func (r *ReconcileAPIServer) Reconcile(ctx context.Context, request reconcile.Re
 		})
 	}
 
-	managementCluster, err := utils.GetManagementCluster(ctx, r.client)
-	if err != nil {
-		log.Error(err, "Error reading ManagementCluster")
-		r.status.SetDegraded("Error reading ManagementCluster", err.Error())
-		return reconcile.Result{}, err
-	}
-
-	managementClusterConnection, err := utils.GetManagementClusterConnection(ctx, r.client)
-	if err != nil {
-		log.Error(err, "Error reading ManagementClusterConnection")
-		r.status.SetDegraded("Error reading ManagementClusterConnection", err.Error())
-		return reconcile.Result{}, err
-	}
-
-	if managementClusterConnection != nil && managementCluster != nil {
-		err = fmt.Errorf("having both a ManagementCluster and a ManagementClusterConnection is not supported")
-		log.Error(err, "")
-		r.status.SetDegraded(err.Error(), "")
-		return reconcile.Result{}, err
-	}
-
-	var tunnelCASecret *v1.Secret
-	if managementCluster != nil {
-		tunnelCASecret, err = utils.ValidateCertPair(r.client,
-			rmeta.OperatorNamespace(),
-			render.VoltronTunnelSecretName,
-			render.VoltronTunnelSecretKeyName,
-			render.VoltronTunnelSecretCertName,
-		)
-		if err != nil {
-			log.Error(err, "Invalid TLS Cert")
-			r.status.SetDegraded("Error validating TLS certificate", err.Error())
-			return reconcile.Result{}, err
-		}
-	}
-
 	pullSecrets, err := utils.GetNetworkingPullSecrets(network, r.client)
 	if err != nil {
 		log.Error(err, "Error retrieving Pull secrets")
@@ -251,15 +211,56 @@ func (r *ReconcileAPIServer) Reconcile(ctx context.Context, request reconcile.Re
 		return reconcile.Result{}, err
 	}
 
+	// Query enterprise-only data.
+	var tunnelCASecret *v1.Secret
 	var amazon *operatorv1.AmazonCloudIntegration
-	if r.amazonCRDExists {
-		amazon, err = utils.GetAmazonCloudIntegration(ctx, r.client)
-		if errors.IsNotFound(err) {
-			amazon = nil
-		} else if err != nil {
-			log.Error(err, "Error reading AmazonCloudIntegration")
-			r.status.SetDegraded("Error reading AmazonCloudIntegration", err.Error())
+	var managementCluster *operatorv1.ManagementCluster
+	var managementClusterConnection *operatorv1.ManagementClusterConnection
+	if variant == operatorv1.TigeraSecureEnterprise {
+		managementCluster, err = utils.GetManagementCluster(ctx, r.client)
+		if err != nil {
+			log.Error(err, "Error reading ManagementCluster")
+			r.status.SetDegraded("Error reading ManagementCluster", err.Error())
 			return reconcile.Result{}, err
+		}
+
+		managementClusterConnection, err = utils.GetManagementClusterConnection(ctx, r.client)
+		if err != nil {
+			log.Error(err, "Error reading ManagementClusterConnection")
+			r.status.SetDegraded("Error reading ManagementClusterConnection", err.Error())
+			return reconcile.Result{}, err
+		}
+
+		if managementClusterConnection != nil && managementCluster != nil {
+			err = fmt.Errorf("having both a ManagementCluster and a ManagementClusterConnection is not supported")
+			log.Error(err, "")
+			r.status.SetDegraded(err.Error(), "")
+			return reconcile.Result{}, err
+		}
+
+		if managementCluster != nil {
+			tunnelCASecret, err = utils.ValidateCertPair(r.client,
+				rmeta.OperatorNamespace(),
+				render.VoltronTunnelSecretName,
+				render.VoltronTunnelSecretKeyName,
+				render.VoltronTunnelSecretCertName,
+			)
+			if err != nil {
+				log.Error(err, "Invalid TLS Cert")
+				r.status.SetDegraded("Error validating TLS certificate", err.Error())
+				return reconcile.Result{}, err
+			}
+		}
+
+		if r.amazonCRDExists {
+			amazon, err = utils.GetAmazonCloudIntegration(ctx, r.client)
+			if errors.IsNotFound(err) {
+				amazon = nil
+			} else if err != nil {
+				log.Error(err, "Error reading AmazonCloudIntegration")
+				r.status.SetDegraded("Error reading AmazonCloudIntegration", err.Error())
+				return reconcile.Result{}, err
+			}
 		}
 	}
 

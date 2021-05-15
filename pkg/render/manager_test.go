f892083d1a85bffe576a345018b206412236d9c2
Rob Brockbank <rob@tigera.io>
2021-05-14 18:59:29 -0700

    Update tigera-manager role to include felixconfiguration, k8s node, k8s events and hostendpointss for service graph processing

--- pkg/render/manager_test.go
+++ pkg/render/manager_test.go
@@ -155,13 +155,20 @@ var _ = Describe("Tigera Secure Manager rendering tests", func() {
 				Verbs: []string{"get", "list"},
 			},
 			{
+				APIGroups: []string{"projectcalico.org"},
+				Resources: []string{
+					"hostendpoints",
+				},
+				Verbs: []string{"list"},
+			},
+			{
 				APIGroups: []string{"networking.k8s.io"},
 				Resources: []string{"networkpolicies"},
 				Verbs:     []string{"get", "list"},
 			},
 			{
 				APIGroups: []string{""},
-				Resources: []string{"serviceaccounts", "namespaces"},
+				Resources: []string{"serviceaccounts", "namespaces", "nodes", "events"},
 				Verbs:     []string{"list"},
 			},
 			{
@@ -310,13 +317,20 @@ var _ = Describe("Tigera Secure Manager rendering tests", func() {
 				Verbs: []string{"get", "list"},
 			},
 			{
+				APIGroups: []string{"projectcalico.org"},
+				Resources: []string{
+					"hostendpoints",
+				},
+				Verbs: []string{"list"},
+			},
+			{
 				APIGroups: []string{"networking.k8s.io"},
 				Resources: []string{"networkpolicies"},
 				Verbs:     []string{"get", "list"},
 			},
 			{
 				APIGroups: []string{""},
-				Resources: []string{"serviceaccounts", "namespaces"},
+				Resources: []string{"serviceaccounts", "namespaces", "nodes", "events"},
 				Verbs:     []string{"list"},
 			},
 			{

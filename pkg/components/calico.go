b19a8d25afb4b8d44da6099e55af6d88719b8c2a
Karthik Ramasubramanian <karthik@tigera.io>
2021-05-20 16:01:14 -0700

    Update components for Enterprise 3.5.3 release

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -17,30 +17,30 @@ package components
 import "github.com/tigera/operator/version"
 
 var (
-	CalicoRelease string = "v3.17.3"
+	CalicoRelease string = "v3.17.4"
 
 	ComponentCalicoCNI = component{
-		Version: "v3.17.3",
+		Version: "v3.17.4",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoKubeControllers = component{
-		Version: "v3.17.3",
+		Version: "v3.17.4",
 		Image:   "calico/kube-controllers",
 	}
 
 	ComponentCalicoNode = component{
-		Version: "v3.17.3",
+		Version: "v3.17.4",
 		Image:   "calico/node",
 	}
 
 	ComponentCalicoTypha = component{
-		Version: "v3.17.3",
+		Version: "v3.17.4",
 		Image:   "calico/typha",
 	}
 
 	ComponentFlexVolume = component{
-		Version: "v3.17.3",
+		Version: "v3.17.4",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 	ComponentOperatorInit = component{

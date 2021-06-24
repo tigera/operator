c7434081a50bea6af067e6e1697c8a61b454ea5e
Vlad Bereznyakov <vladimir@tigera.io>
2021-06-23 23:11:41 -0700

    Implement Operator logic to install ES Gateway

--- pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
+++ pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
@@ -102,7 +102,7 @@ var _ = Describe("Elasticsearch metrics", func() {
 									Env: []corev1.EnvVar{
 										{Name: "ELASTIC_INDEX_SUFFIX", Value: "cluster"},
 										{Name: "ELASTIC_SCHEME", Value: "https"},
-										{Name: "ELASTIC_HOST", Value: "tigera-secure-es-http.tigera-elasticsearch.svc"},
+										{Name: "ELASTIC_HOST", Value: "tigera-secure-es-gateway-http.tigera-elasticsearch.svc"},
 										{Name: "ELASTIC_PORT", Value: "9200"},
 										{Name: "ELASTIC_ACCESS_MODE", Value: "serviceuser"},
 										{Name: "ELASTIC_SSL_VERIFY", Value: "true"},
@@ -152,7 +152,7 @@ var _ = Describe("Elasticsearch metrics", func() {
 									Name: "elastic-ca-cert-volume",
 									VolumeSource: corev1.VolumeSource{
 										Secret: &corev1.SecretVolumeSource{
-											SecretName: "tigera-secure-es-http-certs-public",
+											SecretName: "tigera-secure-es-gateway-http-certs-public",
 											Items:      []corev1.KeyToPath{{Key: "tls.crt", Path: "ca.pem"}},
 										},
 									},

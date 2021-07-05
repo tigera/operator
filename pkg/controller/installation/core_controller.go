3ced8136051e91c7bc6762a64d45b3a567dc5423
edbert <ecandra@protonmail.com>
2021-07-05 14:07:49 -0700

    fix(core_controller.go): removed code not in original PR

--- pkg/controller/installation/core_controller.go
+++ pkg/controller/installation/core_controller.go
@@ -454,14 +454,6 @@ func fillDefaults(instance *operator.Installation) error {
 		}
 	}
 
-	// Default the CalicoNetworkSpec based on the CNI plugin.
-	if instance.Spec.CalicoNetwork == nil {
-		switch instance.Spec.CNI.Type {
-		case operator.PluginCalico:
-			instance.Spec.CalicoNetwork = &operator.CalicoNetworkSpec{}
-		}
-	}
-
 	if instance.Spec.CNI.IPAM == nil {
 		instance.Spec.CNI.IPAM = &operator.IPAMSpec{}
 	}

a5793ccef885eb4e286e54f2ac227da5ffb04c59
Rene Dekker <rene@tigera.io>
2021-07-27 15:53:13 -0700

    Fix groups claim issues

--- pkg/render/dex_config.go
+++ pkg/render/dex_config.go
@@ -100,8 +100,6 @@ type DexRelyingPartyConfig interface {
 	RequestedScopes() []string
 	// UsernameClaim returns the part of the JWT that represents a unique username.
 	UsernameClaim() string
-	// GroupsClaim returns the part of the JWT that represents the list of user groups.
-	GroupsClaim() string
 	DexKeyValidatorConfig
 }
 
@@ -205,14 +203,6 @@ func (d *dexBaseCfg) UsernameClaim() string {
 	return claim
 }
 
-func (d *dexBaseCfg) GroupsClaim() string {
-	claim := defaultGroupsClaim
-	if d.connectorType == connectorTypeOIDC && d.authentication.Spec.OIDC.GroupsClaim != "" {
-		claim = d.authentication.Spec.OIDC.GroupsClaim
-	}
-	return claim
-}
-
 func (d *dexBaseCfg) ClientSecret() []byte {
 	return d.dexSecret.Data[ClientSecretSecretField]
 }
@@ -255,7 +245,7 @@ func (d *dexConfig) RequiredAnnotations() map[string]string {
 // RequiredAnnotations returns the annotations that are relevant for a relying party config.
 func (d *dexRelyingPartyConfig) RequiredAnnotations() map[string]string {
 	var annotations = map[string]string{
-		authenticationAnnotation: AnnotationHash([]interface{}{d.GroupsClaim(), d.UsernameClaim(), d.ManagerURI(), d.RequestedScopes()}),
+		authenticationAnnotation: AnnotationHash([]interface{}{d.UsernameClaim(), d.ManagerURI(), d.RequestedScopes()}),
 		dexTLSSecretAnnotation:   AnnotationHash(d.tlsSecret.Data),
 	}
 	if d.dexSecret != nil {
@@ -267,7 +257,7 @@ func (d *dexRelyingPartyConfig) RequiredAnnotations() map[string]string {
 // RequiredAnnotations returns the annotations that are relevant for a validator config.
 func (d *dexKeyValidatorConfig) RequiredAnnotations() map[string]string {
 	var annotations = map[string]string{
-		authenticationAnnotation: AnnotationHash([]interface{}{d.GroupsClaim(), d.UsernameClaim(), d.ManagerURI()}),
+		authenticationAnnotation: AnnotationHash([]interface{}{d.UsernameClaim(), d.ManagerURI()}),
 		dexTLSSecretAnnotation:   AnnotationHash(d.tlsSecret.Data),
 	}
 	return annotations

de7b55bf3a118ae77ab8fdf567dcb3279b1998d2
Mike Stephen <mike.stephen@tigera.io>
2021-08-03 17:32:04 +0100

    Adds support for wireguard on EKS with AWS CNI

--- pkg/render/node_test.go
+++ pkg/render/node_test.go
@@ -1046,6 +1046,7 @@ var _ = Describe("Node rendering tests", func() {
 			{Name: "FELIX_IPTABLESMANGLEALLOWACTION", Value: "Return"},
 			{Name: "FELIX_ROUTESOURCE", Value: "WorkloadIPs"},
 			{Name: "FELIX_BPFEXTTOSERVICECONNMARK", Value: "0x80"},
+			{Name: "FELIX_WIREGUARDHOSTENCRYPTIONENABLED", Value: "true"},
 		}
 		Expect(ds.Spec.Template.Spec.Containers[0].Env).To(ConsistOf(expectedNodeEnv))
 
@@ -1485,6 +1486,7 @@ var _ = Describe("Node rendering tests", func() {
 			{Name: "FELIX_IPTABLESMANGLEALLOWACTION", Value: "Return"},
 			{Name: "FELIX_ROUTESOURCE", Value: "WorkloadIPs"},
 			{Name: "FELIX_BPFEXTTOSERVICECONNMARK", Value: "0x80"},
+			{Name: "FELIX_WIREGUARDHOSTENCRYPTIONENABLED", Value: "true"},
 		}
 		Expect(ds.Spec.Template.Spec.Containers[0].Env).To(ConsistOf(expectedNodeEnv))
 

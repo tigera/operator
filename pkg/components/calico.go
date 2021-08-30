9eb48b75a39aa1f19e99b5a767a6d183e4a8d0b7
Laurence Man <laurence@tigera.io>
2021-08-30 15:55:14 -0700

    Update config to use Calico v3.18.5 (#1487)

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -17,30 +17,30 @@ package components
 import "github.com/tigera/operator/version"
 
 var (
-	CalicoRelease string = "v3.18.4"
+	CalicoRelease string = "v3.18.5"
 
 	ComponentCalicoCNI = component{
-		Version: "v3.18.4",
+		Version: "v3.18.5",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoKubeControllers = component{
-		Version: "v3.18.4",
+		Version: "v3.18.5",
 		Image:   "calico/kube-controllers",
 	}
 
 	ComponentCalicoNode = component{
-		Version: "v3.18.4",
+		Version: "v3.18.5",
 		Image:   "calico/node",
 	}
 
 	ComponentCalicoTypha = component{
-		Version: "v3.18.4",
+		Version: "v3.18.5",
 		Image:   "calico/typha",
 	}
 
 	ComponentFlexVolume = component{
-		Version: "v3.18.4",
+		Version: "v3.18.5",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 	ComponentOperatorInit = component{

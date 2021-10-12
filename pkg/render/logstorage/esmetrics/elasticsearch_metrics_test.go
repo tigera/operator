208b51550bb605d4bb9e134ce550a189093360cc
Laurence Man <laurence@tigera.io>
2021-10-12 10:06:49 -0700

    Update test

--- pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
+++ pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
@@ -73,6 +73,7 @@ var _ = Describe("Elasticsearch metrics", func() {
 					},
 				},
 				&appsv1.Deployment{
+					TypeMeta: metav1.TypeMeta{Kind: "Deployment", APIVersion: "apps/v1"},
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "tigera-elasticsearch-metrics",
 						Namespace: render.ElasticsearchNamespace,

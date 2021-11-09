dda25e09599eaf9468fab721083b05b29e20ff90
Laurence Man <laurence@tigera.io>
2021-11-09 15:02:35 -0800

    Fix windows-upgrader image (#1619)

--- pkg/components/references.go
+++ pkg/components/references.go
@@ -39,7 +39,8 @@ func GetReference(c component, registry, imagePath, imagePrefix string, is *oper
 			ComponentCalicoTypha,
 			ComponentCalicoKubeControllers,
 			ComponentFlexVolume,
-			ComponentCalicoAPIServer:
+			ComponentCalicoAPIServer,
+			ComponentWindows:
 
 			registry = CalicoRegistry
 		case ComponentElasticsearchOperator:

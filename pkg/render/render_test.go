d1deea04dd5e03f42b80624333cf314ece9d32c0
Jader Silva <jader@tigera.io>
2021-11-10 10:46:55 -0300

    Refactor render args into a struct

--- pkg/render/render_test.go
+++ pkg/render/render_test.go
@@ -63,7 +63,7 @@ func allCalicoComponents(
 	logCollector *operatorv1.LogCollector,
 ) ([]render.Component, error) {
 
-	namespaces := render.Namespaces(cr, pullSecrets)
+	namespaces := render.Namespaces(&render.NamespaceConfiguration{Installation: cr, PullSecrets: pullSecrets})
 
 	objs := []client.Object{}
 	if typhaNodeTLS.CAConfigMap != nil {

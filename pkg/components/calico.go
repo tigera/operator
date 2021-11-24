77cf78c17ae1b869a8a95bf0d115149f11ad08b4
Laurence Man <laurence@tigera.io>
2021-11-24 14:57:24 -0800

    Update calico version to v3.20.3

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -17,35 +17,35 @@ package components
 import "github.com/tigera/operator/version"
 
 var (
-	CalicoRelease string = "v3.20.2"
+	CalicoRelease string = "v3.20.3"
 
 	ComponentCalicoCNI = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoKubeControllers = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/kube-controllers",
 	}
 
 	ComponentCalicoNode = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/node",
 	}
 
 	ComponentCalicoTypha = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/typha",
 	}
 
 	ComponentFlexVolume = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 
 	ComponentCalicoAPIServer = component{
-		Version: "v3.20.2",
+		Version: "v3.20.3",
 		Image:   "calico/apiserver",
 	}
 	ComponentOperatorInit = component{

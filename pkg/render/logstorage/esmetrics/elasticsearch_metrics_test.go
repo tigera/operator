04f3aa1de512fcf56facd365b181fe29d6f7823e
Erik Stidham <erik@tigera.io>
2021-12-04 20:48:51 -0600

    Remove labels that are added by component handler

--- pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
+++ pkg/render/logstorage/esmetrics/elasticsearch_metrics_test.go
@@ -134,7 +134,6 @@ var _ = Describe("Elasticsearch metrics", func() {
 					},
 					Template: corev1.PodTemplateSpec{
 						ObjectMeta: metav1.ObjectMeta{
-							Labels: map[string]string{"k8s-app": ElasticsearchMetricsName},
 							Annotations: map[string]string{
 								"hash.operator.tigera.io/elasticsearch-configmap": "ae0242f242af19c4916434cb08e8f68f8c15f61d",
 								"hash.operator.tigera.io/elasticsearch-secrets":   "9718549725e37ca6a5f12ba2405392a04d7b5521",

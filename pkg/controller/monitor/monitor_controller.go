46dfd1914246b7f6923c22077850dade1980a6fb
rene-dekker <rene@tigera.io>
2022-02-07 12:44:31 -0800

    Add code documentation for service monitor. Refactor anonymous func.

--- pkg/controller/monitor/monitor_controller.go
+++ pkg/controller/monitor/monitor_controller.go
@@ -379,24 +379,20 @@ func (r *ReconcileMonitor) Reconcile(ctx context.Context, request reconcile.Requ
 // getCertificateBundle creates a configmap with a bundle for mTLS with other components.
 func getCertificateBundle(ctx context.Context, cli client.Client, cm *operatorv1.CertificateManagement) (*corev1.ConfigMap, error) {
 	pem := strings.Builder{}
-	addPem := func(secretName string) error {
+	for _, secretName := range []string{render.NodePrometheusTLSServerSecret, render.FluentdPrometheusTLSSecretName, esmetrics.ElasticsearchMetricsServerTLSSecret} {
 		secret := &corev1.Secret{}
 		err := cli.Get(ctx, types.NamespacedName{Name: secretName, Namespace: common.OperatorNamespace()}, secret)
 		if errors.IsNotFound(err) {
 			if cm == nil {
-				return err
+				return nil, err
 			}
 		} else if err != nil {
-			return err
+			return nil, err
 		} else {
 			pem.WriteString(fmt.Sprintf("\n\n# %v\n", secretName))
 			pem.Write(secret.Data[corev1.TLSCertKey])
 		}
-		return err
-	}
-
-	for _, secret := range []string{render.NodePrometheusTLSServerSecret, render.FluentdPrometheusTLSSecretName, esmetrics.ElasticsearchMetricsServerTLSSecret} {
-		addPem(secret)
+		return nil, err
 	}
 	if cm != nil {
 		pem.WriteString("\n\n# Certificate management CA cert\n")

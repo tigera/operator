ce87990c6557f7edeecf8f8dcfb4ce1234ba40ee
Laurence Man <laurence@tigera.io>
2022-03-01 17:38:27 -0800

    Remove broken RH image push

--- hack/maybe-build-release.sh
+++ hack/maybe-build-release.sh
@@ -28,20 +28,3 @@ make release VERSION=${tag}
 
 echo "Publish release ${tag}"
 make release-publish-images VERSION=${tag}
-
-echo "Tagging and pushing operator images to RedHat Connect for certification..."
-
-docker login -u unused scan.connect.redhat.com --password-stdin <<< ${OPERATOR_RH_REGISTRY_KEY}
-redhatImage=scan.connect.redhat.com/$OPERATOR_RH_PROJECTID/operator:${tag}
-
-# Pushes to scan.connect.redhat.com fail if the image exists already.
-# If it already exists, skip tagging and pushing.
-if ! docker pull $redhatImage 2>/dev/null; then
-	echo "Tagging and pushing operator image..."
-	quayImage=quay.io/tigera/operator:${tag}
-	docker pull $quayImage
-	docker tag $quayImage $redhatImage
-	docker push $redhatImage
-else
-	echo "operator image exists on scan.connect.redhat.com, skipping tagging/pushing"
-fi

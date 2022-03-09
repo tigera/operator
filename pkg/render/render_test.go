5f1b97e66f1859797ed2ff56ac426f0731414dfd
rene-dekker <rene@tigera.io>
2022-03-09 10:05:30 -0800

    Fix fdescribe

--- pkg/render/render_test.go
+++ pkg/render/render_test.go
@@ -124,7 +124,7 @@ func allCalicoComponents(
 	return []render.Component{namespaces, secretsAndConfigMaps, render.Typha(typhaCfg), render.Node(nodeCfg), kubecontrollers.NewCalicoKubeControllers(kcCfg), render.Windows(winCfg), nodeCertComponent}, nil
 }
 
-var _ = FDescribe("Rendering tests", func() {
+var _ = Describe("Rendering tests", func() {
 	var instance *operatorv1.InstallationSpec
 	var logBuffer bytes.Buffer
 	var logWriter *bufio.Writer

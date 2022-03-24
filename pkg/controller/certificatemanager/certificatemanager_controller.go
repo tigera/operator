9896422ed91ed765ad1a94ccb480376c09162980
rene-dekker <rene@tigera.io>
2022-03-24 11:46:08 -0700

    Add a controller to create the ca

--- pkg/controller/certificatemanager/certificatemanager_controller.go
+++ pkg/controller/certificatemanager/certificatemanager_controller.go
@@ -14,62 +14,120 @@
 
 package certificatemanager
 
-//
-//import (
-//	"context"
-//	"fmt"
-//
-//	"github.com/tigera/operator/pkg/controller/options"
-//	"github.com/tigera/operator/pkg/controller/status"
-//	"k8s.io/apimachinery/pkg/runtime"
-//	"sigs.k8s.io/controller-runtime/pkg/client"
-//	"sigs.k8s.io/controller-runtime/pkg/manager"
-//	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-//)
-//
-//// Add creates a new certificate manager Controller and adds it to the Manager.
-//func Add(mgr manager.Manager, opts options.AddOptions) error {
-//	ri, err := newReconciler(mgr, opts)
-//	if err != nil {
-//		return fmt.Errorf("failed to create certificate reconciler: %w", err)
-//	}
-//	return add(mgr, ri)
-//}
-//
-//// newReconciler returns a new reconcile.Reconciler that will ensure that a tigera-ca-private secret is created in the
-//// tigera-operator namespace if necessary.
-//func newReconciler(mgr manager.Manager, opts options.AddOptions) (*ReconcileCertificateManager, error) {
-//	statusManager := status.New(mgr.GetClient(), "certificate", opts.KubernetesVersion)
-//	return &ReconcileCertificateManager{
-//		client:        mgr.GetClient(),
-//		scheme:        mgr.GetScheme(),
-//		watches:       make(map[runtime.Object]struct{}),
-//		status:,
-//		clusterDomain: opts.ClusterDomain,
-//	}, nil
-//}
-//
-//// add adds a new Controller to mgr with r as the reconcile.Reconciler
-//func add(mgr manager.Manager, r *ReconcileCertificateManager) error {
-//
-//}
-//
-//// ReconcileCertificateManager will ensure that a tigera-ca-private secret is created in the
-//// tigera-operator namespace if necessary.
-//type ReconcileCertificateManager struct {
-//	client        client.Client
-//	scheme        *runtime.Scheme
-//	watches       map[runtime.Object]struct{}
-//	status        status.StatusManager
-//	clusterDomain string
-//}
-//
-//func (r *ReconcileCertificateManager) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
-//	certificateManager, err := Create(r.client, &instance.Spec, r.clusterDomain)
-//	if err != nil {
-//		log.Error(err, "unable to create the Tigera CA")
-//		r.status.SetDegraded("Unable to create the Tigera CA", err.Error())
-//		return reconcile.Result{}, err
-//	}
-//
-//}
+import (
+	"context"
+	"fmt"
+	"time"
+
+	operator "github.com/tigera/operator/api/v1"
+	"github.com/tigera/operator/pkg/common"
+	"github.com/tigera/operator/pkg/controller/options"
+	"github.com/tigera/operator/pkg/controller/status"
+	"github.com/tigera/operator/pkg/controller/utils"
+	rcertificatemanagement "github.com/tigera/operator/pkg/render/certificatemanagement"
+	"github.com/tigera/operator/pkg/tls/certificatemanagement"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+const controllerName = "certificatemanager_controller"
+
+// Add creates a new certificate manager Controller and adds it to the Manager.
+func Add(mgr manager.Manager, opts options.AddOptions) error {
+	return add(mgr, newReconciler(mgr, opts))
+}
+
+// newReconciler returns a new reconcile.Reconciler that will ensure that a tigera-ca-private secret is created in the
+// tigera-operator namespace if necessary.
+func newReconciler(mgr manager.Manager, opts options.AddOptions) *ReconcileCertificateManager {
+	statusManager := status.New(mgr.GetClient(), "ca-certificate", opts.KubernetesVersion)
+	statusManager.Run(opts.ShutdownContext)
+	return &ReconcileCertificateManager{
+		client:        mgr.GetClient(),
+		scheme:        mgr.GetScheme(),
+		watches:       make(map[runtime.Object]struct{}),
+		status:        statusManager,
+		clusterDomain: opts.ClusterDomain,
+	}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r *ReconcileCertificateManager) error {
+	c, err := controller.New(controllerName, mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return fmt.Errorf("failed to create %s: %w", controllerName, err)
+	}
+
+	err = c.Watch(&source.Kind{Type: &operator.Installation{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return fmt.Errorf("%s failed to watch installation resource: %w", controllerName, err)
+	}
+
+	if err = utils.AddSecretsWatch(c, certificatemanagement.CASecretName, common.OperatorNamespace()); err != nil {
+		return fmt.Errorf("%s failed to watch Secret resource %s: %w", controllerName, certificatemanagement.CASecretName, err)
+	}
+	return nil
+}
+
+// ReconcileCertificateManager will ensure that a tigera-ca-private secret is created in the
+// tigera-operator namespace if necessary.
+type ReconcileCertificateManager struct {
+	client        client.Client
+	scheme        *runtime.Scheme
+	watches       map[runtime.Object]struct{}
+	status        status.StatusManager
+	clusterDomain string
+}
+
+func (r *ReconcileCertificateManager) Reconcile(ctx context.Context, _ reconcile.Request) (reconcile.Result, error) {
+	installation := &operator.Installation{}
+	if err := r.client.Get(ctx, utils.DefaultInstanceKey, installation); err != nil {
+		if errors.IsNotFound(err) {
+			log.Error(err, "Installation config not found")
+			r.status.OnCRNotFound()
+			return reconcile.Result{}, nil
+		}
+		log.Error(err, "An error occurred when querying the Installation resource")
+		return reconcile.Result{}, err
+	}
+
+	certificateManager, err := Create(r.client, &installation.Spec, r.clusterDomain)
+	if err != nil {
+		log.Error(err, "unable to create the Tigera CA")
+		r.status.SetDegraded("Unable to create the Tigera CA", err.Error())
+		return reconcile.Result{}, err
+	}
+
+	component := rcertificatemanagement.CertificateManagement(&rcertificatemanagement.Config{
+		Namespace:       common.OperatorNamespace(),
+		ServiceAccounts: []string{},
+		KeyPairOptions: []rcertificatemanagement.KeyPairOption{
+			// this controller is responsible for rendering the tigera-ca-private secret.
+			rcertificatemanagement.NewKeyPairOption(certificateManager.KeyPair(), true, false),
+		},
+	})
+
+	hdl := utils.NewComponentHandler(log, r.client, r.scheme, installation)
+	if err := hdl.CreateOrUpdateOrDelete(ctx, component, nil); err != nil {
+		r.status.SetDegraded("Error creating / updating resource", err.Error())
+		return reconcile.Result{}, err
+	}
+
+	r.status.ClearDegraded()
+
+	r.status.ReadyToMonitor()
+
+	if !r.status.IsAvailable() {
+		// Schedule a kick to check again in the near future. Hopefully by then things will be available.
+		return reconcile.Result{RequeueAfter: 30 * time.Second}, nil
+	}
+
+	return reconcile.Result{}, nil
+}

f74dcd0f428d15f476c8cefd60021d12081207c8
Casey Davenport <davenport.cas@gmail.com>
2022-04-12 11:09:49 -0700

    Increase Typha max fall behind window

--- pkg/render/typha.go
+++ pkg/render/typha.go
@@ -330,7 +330,8 @@ func (c *typhaComponent) typhaRole() *rbacv1.ClusterRole {
 	}
 	if c.installation.KubernetesProvider != operator.ProviderOpenShift {
 		// Allow access to the pod security policy in case this is enforced on the cluster
-		role.Rules = append(role.Rules, rbacv1.PolicyRule{APIGroups: []string{"policy"},
+		role.Rules = append(role.Rules, rbacv1.PolicyRule{
+			APIGroups:     []string{"policy"},
 			Resources:     []string{"podsecuritypolicies"},
 			Verbs:         []string{"use"},
 			ResourceNames: []string{common.TyphaDeploymentName},
@@ -504,6 +505,7 @@ func (c *typhaComponent) typhaEnvVars() []v1.EnvVar {
 	}
 
 	typhaEnv := []v1.EnvVar{
+		{Name: "TYPHA_SERVERMAXFALLBEHINDSECS", Value: "600"},
 		{Name: "TYPHA_LOGSEVERITYSCREEN", Value: "info"},
 		{Name: "TYPHA_LOGFILEPATH", Value: "none"},
 		{Name: "TYPHA_LOGSEVERITYSYS", Value: "none"},
@@ -541,7 +543,8 @@ func (c *typhaComponent) typhaEnvVars() []v1.EnvVar {
 		if c.installation.CalicoNetwork != nil && c.installation.CalicoNetwork.MultiInterfaceMode != nil {
 			typhaEnv = append(typhaEnv, v1.EnvVar{
 				Name:  "MULTI_INTERFACE_MODE",
-				Value: c.installation.CalicoNetwork.MultiInterfaceMode.Value()})
+				Value: c.installation.CalicoNetwork.MultiInterfaceMode.Value(),
+			})
 		}
 	}
 

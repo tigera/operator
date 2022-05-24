3996641a6952a09c8d90d1c24f2fa763e4fd8e19
Rob Brockbank <rob@tigera.io>
2022-05-24 14:23:22 -0700

    Add openshift layer to default view in cluster-wide settings

--- pkg/render/guardian_test.go
+++ pkg/render/guardian_test.go
@@ -16,6 +16,7 @@ package render_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	operatorv1 "github.com/tigera/operator/api/v1"
@@ -35,7 +36,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-var _ = Describe("Rendering tests", func() {
+var _ = DescribeTable("Rendering tests", func(cfg render.GuardianConfiguration) {
 	var g render.Component
 	var resources []client.Object
 
@@ -59,36 +60,35 @@ var _ = Describe("Rendering tests", func() {
 		Expect(err).NotTo(HaveOccurred())
 		bundle := certificateManager.CreateTrustedBundle()
 
-		cfg := &render.GuardianConfiguration{
-			URL: addr,
-			PullSecrets: []*corev1.Secret{{
-				TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "pull-secret",
-					Namespace: common.OperatorNamespace(),
-				},
-			}},
-			Installation:      &i,
-			TunnelSecret:      secret,
-			TrustedCertBundle: bundle,
-		}
-		g = render.Guardian(cfg)
+		cfg.URL = addr
+		cfg.PullSecrets = []*corev1.Secret{{
+			TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "pull-secret",
+				Namespace: common.OperatorNamespace(),
+			},
+		}}
+		cfg.Installation = &i
+		cfg.TunnelSecret = secret
+		cfg.TrustedCertBundle = bundle
+
+		g = render.Guardian(&cfg)
 		Expect(g.ResolveImages(nil)).To(BeNil())
 		resources, _ = g.Objects()
 	}
 
-	BeforeEach(func() {
-		renderGuardian(operatorv1.InstallationSpec{Registry: "my-reg/"})
-	})
-
 	It("should render all resources for a managed cluster", func() {
-		expectedResources := []struct {
+		renderGuardian(operatorv1.InstallationSpec{Registry: "my-reg/"})
+
+		type expectedResource struct {
 			name    string
 			ns      string
 			group   string
 			version string
 			kind    string
-		}{
+		}
+
+		expectedResources := []expectedResource{
 			{name: render.GuardianNamespace, ns: "", group: "", version: "v1", kind: "Namespace"},
 			{name: "pull-secret", ns: render.GuardianNamespace, group: "", version: "v1", kind: "Secret"},
 			{name: render.GuardianServiceAccountName, ns: render.GuardianNamespace, group: "", version: "v1", kind: "ServiceAccount"},
@@ -107,6 +107,12 @@ var _ = Describe("Rendering tests", func() {
 			{name: render.ManagerClusterSettingsLayerTigera, ns: "", group: "projectcalico.org", version: "v3", kind: "UISettings"},
 			{name: render.ManagerClusterSettingsViewDefault, ns: "", group: "projectcalico.org", version: "v3", kind: "UISettings"},
 		}
+		if cfg.Openshift {
+			expectedResources = append(expectedResources, expectedResource{
+				name: render.ManagerClusterSettingsLayerOpenshift, ns: "", group: "projectcalico.org", version: "v3", kind: "UISettings",
+			})
+		}
+
 		Expect(len(resources)).To(Equal(len(expectedResources)))
 		for i, expectedRes := range expectedResources {
 			rtest.ExpectResource(resources[i], expectedRes.name, expectedRes.ns, expectedRes.group, expectedRes.version, expectedRes.kind)
@@ -117,6 +123,8 @@ var _ = Describe("Rendering tests", func() {
 	})
 
 	It("should render controlPlaneTolerations", func() {
+		renderGuardian(operatorv1.InstallationSpec{Registry: "my-reg/"})
+
 		t := corev1.Toleration{
 			Key:      "foo",
 			Operator: corev1.TolerationOpEqual,
@@ -128,4 +136,11 @@ var _ = Describe("Rendering tests", func() {
 		deployment := rtest.GetResource(resources, render.GuardianDeploymentName, render.GuardianNamespace, "apps", "v1", "Deployment").(*appsv1.Deployment)
 		Expect(deployment.Spec.Template.Spec.Tolerations).Should(ContainElements(t, rmeta.TolerateCriticalAddonsOnly, rmeta.TolerateMaster))
 	})
-})
+},
+	Entry("Openshift", render.GuardianConfiguration{
+		Openshift: true,
+	}),
+	Entry("Not Openshift", render.GuardianConfiguration{
+		Openshift: false,
+	}),
+)

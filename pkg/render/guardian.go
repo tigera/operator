cae94b87e02b0c2e4703c7b930914d96637f5d8c
Jiawei Huang <jiawei@tigera.io>
2022-06-21 20:45:47 -0700

    Run as non-privilege and non-root user
    This changeset set `securityContext` as non-privilege and non-root user
    for IDS controller, prometheus, and kibana pods. It fixes an enterprise
    component developmend issue on CIS profile hardened RKE2 cluster. When
    the container is running as root or non-numeric user (nobody or kibana),
    it failed non-root validation.

--- pkg/render/guardian.go
+++ pkg/render/guardian.go
@@ -27,8 +27,8 @@ import (
 	operatorv1 "github.com/tigera/operator/api/v1"
 	"github.com/tigera/operator/pkg/components"
 	rmeta "github.com/tigera/operator/pkg/render/common/meta"
-	"github.com/tigera/operator/pkg/render/common/podsecuritycontext"
 	"github.com/tigera/operator/pkg/render/common/secret"
+	"github.com/tigera/operator/pkg/render/common/securitycontext"
 	"github.com/tigera/operator/pkg/tls/certificatemanagement"
 )
 
@@ -268,7 +268,7 @@ func (c *GuardianComponent) container() []corev1.Container {
 				InitialDelaySeconds: 10,
 				PeriodSeconds:       5,
 			},
-			SecurityContext: podsecuritycontext.NewBaseContext(),
+			SecurityContext: securitycontext.NewBaseContext(),
 		},
 	}
 }

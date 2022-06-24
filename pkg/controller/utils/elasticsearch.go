a87eaecf0d167afeef0f2143a90a773d914b926b
varalakshmi kumar <vara@tigera.io>
2022-06-24 09:43:40 -0700

    Fixing Comment for BGP DNS log

--- pkg/controller/utils/elasticsearch.go
+++ pkg/controller/utils/elasticsearch.go
@@ -168,7 +168,7 @@ func (es *esClient) listILMPolicies(ls *operatorv1.LogStorage) map[string]policy
 	minorPctOfTotalDisk := 0.1
 	pctOfDisk := minorPctOfTotalDisk / float64(numOfIndicesWithMinorSpace)
 
-	// Retention is not set in LogStorage for bgp, dns, benchmark and events logs, set default values used by curator
+	// Retention is not set in LogStorage for benchmark and events logs, set default values used by curator
 	return map[string]policyDetail{
 		"tigera_secure_ee_flows": buildILMPolicy(totalEsStorage, majorPctOfTotalDisk, 0.85, int(*ls.Spec.Retention.Flows)),
 		"tigera_secure_ee_dns":   buildILMPolicy(totalEsStorage, majorPctOfTotalDisk, 0.05, int(*ls.Spec.Retention.DNSLogs)),

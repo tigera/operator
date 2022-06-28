f98dd93ed642bc408f4722c4a251925821c03953
Casey Davenport <davenport.cas@gmail.com>
2022-06-28 15:37:27 -0700

    Add UTs

--- pkg/render/manager.go
+++ pkg/render/manager.go
@@ -168,7 +168,7 @@ func (c *managerComponent) Objects() ([]client.Object, []client.Object) {
 		// In order to switch to a restricted namespace, we need to set:
 		// - securityContext.capabilities.drop=["ALL"]
 		// - securityContext.seccompProfile.type to "RuntimeDefault" or "Localhost"
-		CreateNamespace(ManagerNamespace, c.cfg.Installation.KubernetesProvider, PSSRestricted),
+		CreateNamespace(ManagerNamespace, c.cfg.Installation.KubernetesProvider, PSSBaseline),
 	}
 	objs = append(objs, secret.ToRuntimeObjects(secret.CopyToNamespace(ManagerNamespace, c.cfg.PullSecrets...)...)...)
 

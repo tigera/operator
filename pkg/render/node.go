6290d59a5da6f3d8614ae6b43373a2207f6e13b3
Mazdak Nasab <mazdak.nasab@gmail.com>
2022-07-04 19:34:21 -0700

    Add /proc and /var/run/calico to mount-bpffs volumens

--- pkg/render/node.go
+++ pkg/render/node.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019-2022 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -856,6 +856,8 @@ func (c *nodeComponent) nodeVolumes() []corev1.Volume {
 			corev1.Volume{Name: "sys-fs", VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/sys/fs", Type: &dirOrCreate}}},
 			// Volume for the bpffs itself, used by the main node container.
 			corev1.Volume{Name: "bpffs", VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/sys/fs/bpf", Type: &dirMustExist}}},
+			// Volume used by mount-cgroupv2 init container to access root cgroup name space of node.
+			corev1.Volume{Name: "nodeproc", VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/proc"}}},
 		)
 	}
 
@@ -1007,6 +1009,18 @@ func (c *nodeComponent) bpffsInitContainer() corev1.Container {
 			// Bidirectional is required to ensure that the new mount we make at /sys/fs/bpf propagates to the host
 			// so that it outlives the init container.
 			MountPropagation: &bidirectional,
+		},
+		{
+			MountPath: "/var/run/calico",
+			Name:      "var-run-calico",
+			// Bidirectional is required to ensure that the new mount we make at /var/run/calico/cgroup propagates to the host
+			// so that it outlives the init container.
+			MountPropagation: &bidirectional,
+		},
+		{
+			MountPath: "/nodeproc",
+			Name:      "nodeproc",
+			ReadOnly:  true,
 		},
 	}
 

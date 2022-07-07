736926fad47b21d892a4e9d3b757ee5c344fdc46
freecaykes <edbert@tigera.io>
2022-07-07 14:41:39 -0700

    feat(render/ids): add metdata resource to AD role

--- pkg/render/intrusion_detection.go
+++ pkg/render/intrusion_detection.go
@@ -57,12 +57,13 @@ const (
 	IntrusionDetectionInstallerJobName = "intrusion-detection-es-job-installer"
 	IntrusionDetectionControllerName   = "intrusion-detection-controller"
 
-	ADJobPodTemplateBaseName     = "tigera.io.detectors"
-	adDetectorPrefixName         = "tigera.io.detector."
-	adDetectorServiceAccountName = "anomaly-detectors"
-	adDetectionJobsDefaultPeriod = 15 * time.Minute
-	ADResourceGroup              = "detectors.tigera.io"
-	ADDetectorsModelResourceName = "models"
+	ADJobPodTemplateBaseName      = "tigera.io.detectors"
+	adDetectorPrefixName          = "tigera.io.detector."
+	adDetectorServiceAccountName  = "anomaly-detectors"
+	adDetectionJobsDefaultPeriod  = 15 * time.Minute
+	ADResourceGroup               = "detectors.tigera.io"
+	ADDetectorsModelResourceName  = "models"
+	ADLogTypeMetaDataResourceName = "metadata"
 
 	ADAPIObjectName     = "anomaly-detection-api"
 	ADAPIObjectPortName = "anomaly-detection-api-https"
@@ -1412,6 +1413,19 @@ func (c *intrusionDetectionComponent) adDetectorAccessRole() *rbacv1.Role {
 				},
 				Resources: []string{
 					ADDetectorsModelResourceName,
+				},
+				Verbs: []string{
+					"get",
+					"create",
+					"update",
+				},
+			},
+			{
+				APIGroups: []string{
+					ADResourceGroup,
+				},
+				Resources: []string{
+					ADLogTypeMetaDataResourceName,
 				},
 				Verbs: []string{
 					"get",

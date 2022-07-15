a8cd3c2a746406e06b9a1b589d6ea4344713ebb6
Laurence Man <laurence@tigera.io>
2022-07-15 10:51:27 -0700

    Remove unused functions

--- pkg/common/k8svalidation/helper/helper.go
+++ pkg/common/k8svalidation/helper/helper.go
@@ -30,11 +30,9 @@ import (
 	"k8s.io/apimachinery/pkg/conversion"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation"
 
-	// "k8s.io/kubernetes/pkg/apis/core"
 	core "k8s.io/api/core/v1"
 )
 
@@ -156,63 +154,11 @@ func IsIntegerResourceName(str string) bool {
 	return integerResources.Has(str) || IsExtendedResourceName(core.ResourceName(str))
 }
 
-// IsServiceIPSet aims to check if the service's ClusterIP is set or not
-// the objective is not to perform validation here
-func IsServiceIPSet(service *core.Service) bool {
-	// This function assumes that the service is semantically validated
-	// it does not test if the IP is valid, just makes sure that it is set.
-	return len(service.Spec.ClusterIP) > 0 &&
-		service.Spec.ClusterIP != core.ClusterIPNone
-}
-
 var standardFinalizers = sets.NewString(
 	string(core.FinalizerKubernetes),
 	metav1.FinalizerOrphanDependents,
 	metav1.FinalizerDeleteDependents,
 )
-
-func ContainsAccessMode(modes []core.PersistentVolumeAccessMode, mode core.PersistentVolumeAccessMode) bool {
-	for _, m := range modes {
-		if m == mode {
-			return true
-		}
-	}
-	return false
-}
-
-// NodeSelectorRequirementsAsSelector converts the []NodeSelectorRequirement core type into a struct that implements
-// labels.Selector.
-func NodeSelectorRequirementsAsSelector(nsm []core.NodeSelectorRequirement) (labels.Selector, error) {
-	if len(nsm) == 0 {
-		return labels.Nothing(), nil
-	}
-	selector := labels.NewSelector()
-	for _, expr := range nsm {
-		var op selection.Operator
-		switch expr.Operator {
-		case core.NodeSelectorOpIn:
-			op = selection.In
-		case core.NodeSelectorOpNotIn:
-			op = selection.NotIn
-		case core.NodeSelectorOpExists:
-			op = selection.Exists
-		case core.NodeSelectorOpDoesNotExist:
-			op = selection.DoesNotExist
-		case core.NodeSelectorOpGt:
-			op = selection.GreaterThan
-		case core.NodeSelectorOpLt:
-			op = selection.LessThan
-		default:
-			return nil, fmt.Errorf("%q is not a valid node selector operator", expr.Operator)
-		}
-		r, err := labels.NewRequirement(expr.Key, op, expr.Values)
-		if err != nil {
-			return nil, err
-		}
-		selector = selector.Add(*r)
-	}
-	return selector, nil
-}
 
 func toResourceNames(resources core.ResourceList) []core.ResourceName {
 	result := []core.ResourceName{}
@@ -228,18 +174,4 @@ func toSet(resourceNames []core.ResourceName) sets.String {
 		result.Insert(string(resourceName))
 	}
 	return result
-}
-
-// toContainerResourcesSet returns a set of resources names in container resource requirements
-func toContainerResourcesSet(ctr *core.Container) sets.String {
-	resourceNames := toResourceNames(ctr.Resources.Requests)
-	resourceNames = append(resourceNames, toResourceNames(ctr.Resources.Limits)...)
-	return toSet(resourceNames)
-}
-
-func validFirstDigit(str string) bool {
-	if len(str) == 0 {
-		return false
-	}
-	return str[0] == '-' || (str[0] == '0' && str == "0") || (str[0] >= '1' && str[0] <= '9')
 }
--- pkg/common/k8svalidation/v1helper/v1helper.go
+++ pkg/common/k8svalidation/v1helper/v1helper.go
@@ -29,9 +29,6 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/apimachinery/pkg/util/validation"
-
-	// "k8s.io/kubernetes/pkg/apis/core/helper"
-	"github.com/tigera/operator/pkg/common/k8svalidation/helper"
 )
 
 // IsExtendedResourceName returns true if:
@@ -78,35 +75,6 @@ func HugePageResourceName(pageSize resource.Quantity) v1.ResourceName {
 	return v1.ResourceName(fmt.Sprintf("%s%s", v1.ResourceHugePagesPrefix, pageSize.String()))
 }
 
-// HugePageSizeFromResourceName returns the page size for the specified huge page
-// resource name.  If the specified input is not a valid huge page resource name
-// an error is returned.
-func HugePageSizeFromResourceName(name v1.ResourceName) (resource.Quantity, error) {
-	if !IsHugePageResourceName(name) {
-		return resource.Quantity{}, fmt.Errorf("resource name: %s is an invalid hugepage name", name)
-	}
-	pageSize := strings.TrimPrefix(string(name), v1.ResourceHugePagesPrefix)
-	return resource.ParseQuantity(pageSize)
-}
-
-// HugePageUnitSizeFromByteSize returns hugepage size has the format.
-// `size` must be guaranteed to divisible into the largest units that can be expressed.
-// <size><unit-prefix>B (1024 = "1KB", 1048576 = "1MB", etc).
-func HugePageUnitSizeFromByteSize(size int64) (string, error) {
-	// hugePageSizeUnitList is borrowed from opencontainers/runc/libcontainer/cgroups/utils.go
-	var hugePageSizeUnitList = []string{"B", "KB", "MB", "GB", "TB", "PB"}
-	idx := 0
-	len := len(hugePageSizeUnitList) - 1
-	for size%1024 == 0 && idx < len {
-		size /= 1024
-		idx++
-	}
-	if size > 1024 && idx < len {
-		return "", fmt.Errorf("size: %d%s must be guaranteed to divisible into the largest units", size, hugePageSizeUnitList[idx])
-	}
-	return fmt.Sprintf("%d%s", size, hugePageSizeUnitList[idx]), nil
-}
-
 // IsHugePageMedium returns true if the volume medium is in 'HugePages[-size]' format
 func IsHugePageMedium(medium v1.StorageMedium) bool {
 	if medium == v1.StorageMediumHugePages {
@@ -115,42 +83,11 @@ func IsHugePageMedium(medium v1.StorageMedium) bool {
 	return strings.HasPrefix(string(medium), string(v1.StorageMediumHugePagesPrefix))
 }
 
-// HugePageSizeFromMedium returns the page size for the specified huge page medium.
-// If the specified input is not a valid huge page medium an error is returned.
-func HugePageSizeFromMedium(medium v1.StorageMedium) (resource.Quantity, error) {
-	if !IsHugePageMedium(medium) {
-		return resource.Quantity{}, fmt.Errorf("medium: %s is not a hugepage medium", medium)
-	}
-	if medium == v1.StorageMediumHugePages {
-		return resource.Quantity{}, fmt.Errorf("medium: %s doesn't have size information", medium)
-	}
-	pageSize := strings.TrimPrefix(string(medium), string(v1.StorageMediumHugePagesPrefix))
-	return resource.ParseQuantity(pageSize)
-}
-
 // IsOvercommitAllowed returns true if the resource is in the default
 // namespace and is not hugepages.
 func IsOvercommitAllowed(name v1.ResourceName) bool {
 	return IsNativeResource(name) &&
 		!IsHugePageResourceName(name)
-}
-
-// IsAttachableVolumeResourceName returns true when the resource name is prefixed in attachable volume
-func IsAttachableVolumeResourceName(name v1.ResourceName) bool {
-	return strings.HasPrefix(string(name), v1.ResourceAttachableVolumesPrefix)
-}
-
-// IsServiceIPSet aims to check if the service's ClusterIP is set or not
-// the objective is not to perform validation here
-func IsServiceIPSet(service *v1.Service) bool {
-	return service.Spec.ClusterIP != v1.ClusterIPNone && service.Spec.ClusterIP != ""
-}
-
-// LoadBalancerStatusEqual evaluates the given load balancers' ingress IP addresses
-// and hostnames and returns true if equal or false if otherwise
-// TODO: make method on LoadBalancerStatus?
-func LoadBalancerStatusEqual(l, r *v1.LoadBalancerStatus) bool {
-	return ingressSliceEqual(l.Ingress, r.Ingress)
 }
 
 func ingressSliceEqual(lhs, rhs []v1.LoadBalancerIngress) bool {
@@ -175,35 +112,10 @@ func ingressEqual(lhs, rhs *v1.LoadBalancerIngress) bool {
 	return true
 }
 
-// removeDuplicateAccessModes returns an array of access modes without any duplicates
-func removeDuplicateAccessModes(modes []v1.PersistentVolumeAccessMode) []v1.PersistentVolumeAccessMode {
-	accessModes := []v1.PersistentVolumeAccessMode{}
-	for _, m := range modes {
-		if !ContainsAccessMode(accessModes, m) {
-			accessModes = append(accessModes, m)
-		}
-	}
-	return accessModes
-}
-
 func ContainsAccessMode(modes []v1.PersistentVolumeAccessMode, mode v1.PersistentVolumeAccessMode) bool {
 	for _, m := range modes {
 		if m == mode {
 			return true
-		}
-	}
-	return false
-}
-
-// NodeSelectorRequirementKeysExistInNodeSelectorTerms checks if a NodeSelectorTerm with key is already specified in terms
-func NodeSelectorRequirementKeysExistInNodeSelectorTerms(reqs []v1.NodeSelectorRequirement, terms []v1.NodeSelectorTerm) bool {
-	for _, req := range reqs {
-		for _, term := range terms {
-			for _, r := range term.MatchExpressions {
-				if r.Key == req.Key {
-					return true
-				}
-			}
 		}
 	}
 	return false
@@ -225,114 +137,5 @@ func TopologySelectorRequirementsAsSelector(tsm []v1.TopologySelectorLabelRequir
 		selector = selector.Add(*r)
 	}
 
-	return selector, nil
-}
-
-// MatchTopologySelectorTerms checks whether given labels match topology selector terms in ORed;
-// nil or empty term matches no objects; while empty term list matches all objects.
-func MatchTopologySelectorTerms(topologySelectorTerms []v1.TopologySelectorTerm, lbls labels.Set) bool {
-	if len(topologySelectorTerms) == 0 {
-		// empty term list matches all objects
-		return true
-	}
-
-	for _, req := range topologySelectorTerms {
-		// nil or empty term selects no objects
-		if len(req.MatchLabelExpressions) == 0 {
-			continue
-		}
-
-		labelSelector, err := TopologySelectorRequirementsAsSelector(req.MatchLabelExpressions)
-		if err != nil || !labelSelector.Matches(lbls) {
-			continue
-		}
-
-		return true
-	}
-
-	return false
-}
-
-// AddOrUpdateTolerationInPodSpec tries to add a toleration to the toleration list in PodSpec.
-// Returns true if something was updated, false otherwise.
-func AddOrUpdateTolerationInPodSpec(spec *v1.PodSpec, toleration *v1.Toleration) bool {
-	podTolerations := spec.Tolerations
-
-	var newTolerations []v1.Toleration
-	updated := false
-	for i := range podTolerations {
-		if toleration.MatchToleration(&podTolerations[i]) {
-			if helper.Semantic.DeepEqual(toleration, podTolerations[i]) {
-				return false
-			}
-			newTolerations = append(newTolerations, *toleration)
-			updated = true
-			continue
-		}
-
-		newTolerations = append(newTolerations, podTolerations[i])
-	}
-
-	if !updated {
-		newTolerations = append(newTolerations, *toleration)
-	}
-
-	spec.Tolerations = newTolerations
-	return true
-}
-
-// AddOrUpdateTolerationInPod tries to add a toleration to the pod's toleration list.
-// Returns true if something was updated, false otherwise.
-func AddOrUpdateTolerationInPod(pod *v1.Pod, toleration *v1.Toleration) bool {
-	return AddOrUpdateTolerationInPodSpec(&pod.Spec, toleration)
-}
-
-// GetMatchingTolerations returns true and list of Tolerations matching all Taints if all are tolerated, or false otherwise.
-func GetMatchingTolerations(taints []v1.Taint, tolerations []v1.Toleration) (bool, []v1.Toleration) {
-	if len(taints) == 0 {
-		return true, []v1.Toleration{}
-	}
-	if len(tolerations) == 0 && len(taints) > 0 {
-		return false, []v1.Toleration{}
-	}
-	result := []v1.Toleration{}
-	for i := range taints {
-		tolerated := false
-		for j := range tolerations {
-			if tolerations[j].ToleratesTaint(&taints[i]) {
-				result = append(result, tolerations[j])
-				tolerated = true
-				break
-			}
-		}
-		if !tolerated {
-			return false, []v1.Toleration{}
-		}
-	}
-	return true, result
-}
-
-// ScopedResourceSelectorRequirementsAsSelector converts the ScopedResourceSelectorRequirement api type into a struct that implements
-// labels.Selector.
-func ScopedResourceSelectorRequirementsAsSelector(ssr v1.ScopedResourceSelectorRequirement) (labels.Selector, error) {
-	selector := labels.NewSelector()
-	var op selection.Operator
-	switch ssr.Operator {
-	case v1.ScopeSelectorOpIn:
-		op = selection.In
-	case v1.ScopeSelectorOpNotIn:
-		op = selection.NotIn
-	case v1.ScopeSelectorOpExists:
-		op = selection.Exists
-	case v1.ScopeSelectorOpDoesNotExist:
-		op = selection.DoesNotExist
-	default:
-		return nil, fmt.Errorf("%q is not a valid scope selector operator", ssr.Operator)
-	}
-	r, err := labels.NewRequirement(string(ssr.ScopeName), op, ssr.Values)
-	if err != nil {
-		return nil, err
-	}
-	selector = selector.Add(*r)
 	return selector, nil
 }

a4b3268db75c8cec7ca6691a6e15ee67fdc99fdc
Pedro Coutinho <pedro@tigera.io>
2022-07-19 15:19:34 -0700

    Add IPv6 Wireguard and IPv6 VXLAN custom MTU support
    Set IPv6 VXLAN and Wireguard MTU if IPv6 is going to be enabled.
    
    Enable IPv6 in render/node_test.go in order to correctly test IPv6 VXLAN and Wireguard MTUs.

--- pkg/controller/migration/convert/calico_default_config_test.go
+++ pkg/controller/migration/convert/calico_default_config_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package convert
 
 import (
@@ -197,7 +211,23 @@ func calicoDefaultConfig() []runtime.Object {
 										},
 									},
 								},
+								{Name: "FELIX_VXLANMTUV6",
+									ValueFrom: &corev1.EnvVarSource{
+										ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{Name: "calico-config"},
+											Key:                  "veth_mtu",
+										},
+									},
+								},
 								{Name: "FELIX_WIREGUARDMTU",
+									ValueFrom: &corev1.EnvVarSource{
+										ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
+											LocalObjectReference: corev1.LocalObjectReference{Name: "calico-config"},
+											Key:                  "veth_mtu",
+										},
+									},
+								},
+								{Name: "FELIX_WIREGUARDMTUV6",
 									ValueFrom: &corev1.EnvVarSource{
 										ConfigMapKeyRef: &corev1.ConfigMapKeySelector{
 											LocalObjectReference: corev1.LocalObjectReference{Name: "calico-config"},

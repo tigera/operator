c6b8ef87ed9a5dc726811afa9df25339b85a2fcc
Pedro Coutinho <pedro@tigera.io>
2022-07-19 15:19:34 -0700

    Add IPv6 wireguard support

--- pkg/controller/migration/convert/mtu_test.go
+++ pkg/controller/migration/convert/mtu_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package convert
 
 import (
@@ -48,7 +62,9 @@ var _ = Describe("mtu handler", func() {
 	},
 		table.Entry("ipip", "FELIX_IPINIPMTU"),
 		table.Entry("vxlan", "FELIX_VXLANMTU"),
+		table.Entry("vxlanV6", "FELIX_VXLANMTUV6"),
 		table.Entry("wireguard", "FELIX_WIREGUARDMTU"),
+		table.Entry("wireguardV6", "FELIX_WIREGUARDMTUV6"),
 	)
 
 	It("should error if given conflicting mtu values between env vars", func() {

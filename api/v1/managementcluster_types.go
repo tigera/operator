88bfe1da52efac99354cf9c16f8752446275094b
ozdanborne <dan@projectcalico.org>
2022-07-22 15:23:29 -0400

    voltron public ca support

--- api/v1/managementcluster_types.go
+++ api/v1/managementcluster_types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Tigera, Inc. All rights reserved.
+// Copyright (c) 2012,2015-2022 Tigera, Inc. All rights reserved.
 /*
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,7 +27,57 @@ type ManagementClusterSpec struct {
 	// Valid examples are: "0.0.0.0:31000", "example.com:32000", "[::1]:32500"
 	// +optional
 	Address string `json:"address,omitempty"`
+
+	// TunnelCertType configures the type of certificate that voltron should use to serve its tunnel.
+	//
+	// When set to SelfSigned, voltron will use the same self-signed cert bundle which guardian client certs are signed with.
+	//
+	// When set to CASigned, the operator will expect a ca bundle secret to be created and use have voltron use it to serve TLS.
+	// The apiserver will no longer include the management cluster cert in the resource bundle produced when a new managed cluster resource is created.
+	//
+	// If modified from SelfSigned to CASigned, all managed clusters will disconnect as they will no longer be able to verify Voltron's identity.
+	// To reconnect existing managed clusters, generate a new cert bundle which the tigera-apiserver will generate without including the management cluster's cert.
+	//
+	// Default: SelfSigned
+	// +optional
+	TunnelCertType CAType `json:"tunnelCertType,omitempty"`
+
+	TLS TLS `json:"tls,omitempty"`
 }
+
+type TLS struct {
+	// secretName indicates the name of the secret in the tigera-operator namespace that contains a certificate bundle which should be used to serve voltron’s tunnel.
+	//
+	// Currently, only two values are supported: tigera-management-cluster-connection and manager-tls.
+	//
+	// When set to tigera-management-cluster-connection voltron will use the same cert bundle which Guardian client certs are signed with.
+	//
+	// When set to manager-tls, voltron will use the same cert bundle which Manager UI is served with.
+	//
+	// If changed on a running cluster with connected managed clusters, all managed clusters will disconnect as they will no longer be able to verify Voltron's identity.
+	// To reconnect existing managed clusters, change the tls.ca of the  managed clusters’ ManagementClusterConnection resource.
+	//
+	// Default: tigera-management-cluster-connection
+	// +optional
+	SecretName string `json:"secretName,omitempty"`
+
+	// ca indicates how the tls.secretName cert which Voltron is using was signed.
+	// This information is used by the Tigera API Server when generating ManagementClusterConnection resources as it does in response to the creation of a ManagedCluster resource.
+	// See information on ManagementClusterConnectionTLS for more information on the effects of its values.
+	//
+	// Currently, 'Public' is only supported with a tls.secretName of manager-tls, and Tigera is only supported with a tls.secretName of tigera-management-cluster-connection.
+	//
+	// Note that using a self-signed bundle in tls.secretName=manager-tls while setting tls.ca=Public will result in tunnel clients being unable to verify the tunnel server’s identity. If using tls.ca=Public with tls.secretName=manager-tls, a publicly signed cert bundle must be uploaded as manager-tls, as otherwise the operator will generate a self-signed one resulting in tunnel clients being unable to verify voltron’s identity.
+	// Default: SelfSigned
+	CA CAType `json:"ca,omitempty"`
+}
+
+type CAType string
+
+const (
+	CATypeTigera CAType = "Tigera"
+	CATypePublic CAType = "Public"
+)
 
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status

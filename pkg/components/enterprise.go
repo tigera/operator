ffa8cb747e83c58373b45ef26eb7eb80a703534b
rene-dekker <rene@tigera.io>
2022-09-21 14:24:42 -0700

    Set EE images to v3.12.4 & Update CRDs

--- pkg/components/enterprise.go
+++ pkg/components/enterprise.go
@@ -15,40 +15,40 @@
 package components
 
 var (
-	EnterpriseRelease string = "v3.12.3"
+	EnterpriseRelease string = "v3.12.4"
 
 	ComponentAPIServer = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cnx-apiserver",
 	}
 
 	ComponentComplianceBenchmarker = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/compliance-benchmarker",
 	}
 
 	ComponentComplianceController = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/compliance-controller",
 	}
 
 	ComponentComplianceReporter = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/compliance-reporter",
 	}
 
 	ComponentComplianceServer = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/compliance-server",
 	}
 
 	ComponentComplianceSnapshotter = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/compliance-snapshotter",
 	}
 
 	ComponentDeepPacketInspection = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/deep-packet-inspection",
 	}
 
@@ -63,12 +63,12 @@ var (
 	}
 
 	ComponentElasticTseeInstaller = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/intrusion-detection-job-installer",
 	}
 
 	ComponentElasticsearch = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/elasticsearch",
 	}
 
@@ -78,77 +78,77 @@ var (
 	}
 
 	ComponentElasticsearchOperator = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/eck-operator",
 	}
 
 	ComponentEsCurator = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/es-curator",
 	}
 
 	ComponentEsProxy = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/es-proxy",
 	}
 
 	ComponentESGateway = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/es-gateway",
 	}
 
 	ComponentFluentd = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/fluentd",
 	}
 
 	ComponentFluentdWindows = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/fluentd-windows",
 	}
 
 	ComponentGuardian = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/guardian",
 	}
 
 	ComponentIntrusionDetectionController = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/intrusion-detection-controller",
 	}
 
 	ComponentKibana = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/kibana",
 	}
 
 	ComponentManager = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cnx-manager",
 	}
 
 	ComponentDex = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/dex",
 	}
 
 	ComponentManagerProxy = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/voltron",
 	}
 
 	ComponentPacketCapture = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/packetcapture-api",
 	}
 
 	ComponentL7Collector = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/l7-collector",
 	}
 
 	ComponentEnvoyProxy = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/envoy",
 	}
 
@@ -158,12 +158,12 @@ var (
 	}
 
 	ComponentPrometheus = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/prometheus",
 	}
 
 	ComponentTigeraPrometheusService = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/prometheus-service",
 	}
 
@@ -173,47 +173,47 @@ var (
 	}
 
 	ComponentPrometheusAlertmanager = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/alertmanager",
 	}
 
 	ComponentQueryServer = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cnx-queryserver",
 	}
 
 	ComponentTigeraKubeControllers = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/kube-controllers",
 	}
 
 	ComponentTigeraNode = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cnx-node",
 	}
 
 	ComponentTigeraTypha = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/typha",
 	}
 
 	ComponentTigeraCNI = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cni",
 	}
 
 	ComponentCloudControllers = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/cloud-controllers",
 	}
 
 	ComponentElasticsearchMetrics = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/elasticsearch-metrics",
 	}
 
 	ComponentTigeraWindows = component{
-		Version: "v3.12.3",
+		Version: "v3.12.4",
 		Image:   "tigera/calico-windows-upgrade",
 	}
 	EnterpriseComponents = []component{

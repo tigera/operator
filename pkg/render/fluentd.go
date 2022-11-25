fd2cdfb508dc528ac9bd0663f4e7c190e54bdb5e
rene-dekker <rene@tigera.io>
2022-11-25 12:37:10 -0800

    Add bundle as item inside fluentd. Add hash for system certs

--- pkg/render/fluentd.go
+++ pkg/render/fluentd.go
@@ -1017,6 +1017,7 @@ func trustedBundleVolume(bundle certificatemanagement.TrustedBundle) corev1.Volu
 	volume.ConfigMap.Items = []corev1.KeyToPath{
 		{Key: certificatemanagement.TrustedCertConfigMapKeyName, Path: certificatemanagement.TrustedCertConfigMapKeyName},
 		{Key: certificatemanagement.TrustedCertConfigMapKeyName, Path: SplunkFluentdSecretCertificateKey},
+		{Key: certificatemanagement.RHELRootCertificateBundleName, Path: certificatemanagement.RHELRootCertificateBundleName},
 	}
 	return volume
 }

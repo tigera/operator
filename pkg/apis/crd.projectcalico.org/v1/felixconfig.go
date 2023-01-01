ed0f34c62f9b447d55573b03247587c04063657c
Seth Malaki <seth@tigera.io>
2023-01-01 21:06:16 +0000

    Application-Layer Policies via per-host/per-node envoy support
    - update ApplicationLayer spec to allow toggling L7 features (e.g. WAF/L7 Logging and *new* ALP *new*)
    - corrected volumeMounts / volumes matching where we completely separate needed volumes between WAF/Log/ALP
    - try handling policySyncPathPrefix value in addition to (already existing) TPROXYMode

--- pkg/apis/crd.projectcalico.org/v1/felixconfig.go
+++ pkg/apis/crd.projectcalico.org/v1/felixconfig.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2017-2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -42,8 +42,9 @@ const (
 type TPROXYModeOption string
 
 const (
-	TPROXYModeOptionEnabled  TPROXYModeOption = "Enabled"
-	TPROXYModeOptionDisabled TPROXYModeOption = "Disabled"
+	TPROXYModeOptionEnabled            TPROXYModeOption = "Enabled"
+	TPROXYModeOptionDisabled           TPROXYModeOption = "Disabled"
+	TPROXYModeOptionEnabledAllServices TPROXYModeOption = "EnabledAllServices"
 )
 
 // +genclient

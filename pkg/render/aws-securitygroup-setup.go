0cbf7b717373faa3a6e3fc9009334494d8a2e0de
Jiawei Huang <jiawei@tigera.io>
2023-01-26 17:57:46 -0800

    Apply more restrictive SecurityContext to components
    This changeset add more restrictive SecurityContext to Calico opensource
    and enterprise components. Least privilege principle is applied to this
    change.
    
    Data plane components are mostly running as root user and application
    layer compoennts are running as non-root users. `Capabilities` and
    `SeccompProfile` are also filled with sensible defaults in
    `SecurityContext`s.

--- pkg/render/aws-securitygroup-setup.go
+++ pkg/render/aws-securitygroup-setup.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019,2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019-2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -110,8 +110,7 @@ func (c *awsSGSetupComponent) setupJob() *batchv1.Job {
 								Value: "/etc/kubernetes/kubeconfig",
 							},
 						},
-						// UID 1001 is used in the operator Dockerfile.
-						SecurityContext: securitycontext.NewBaseContext(1001, 0),
+						SecurityContext: securitycontext.NewNonRootContext(),
 					}},
 				},
 			},

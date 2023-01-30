7b462d9b4b53609037cd8b6e0996604aa9d9a8e9
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-01-30 11:54:56 -0800

    Pass user and password to connect to ES (#2431)
    * Pass user and password to connect to ES
    
    * [CODE REVIEW] Cosmetic changes

--- pkg/render/logstorage.go
+++ pkg/render/logstorage.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2022,2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1816,6 +1816,11 @@ func (es *elasticsearchComponent) elasticsearchAllowTigeraPolicy() *v3.NetworkPo
 		{
 			Action:      v3.Allow,
 			Protocol:    &networkpolicy.TCPProtocol,
+			Destination: networkpolicy.LinseedEntityRule,
+		},
+		{
+			Action:      v3.Allow,
+			Protocol:    &networkpolicy.TCPProtocol,
 			Destination: networkpolicy.KubeAPIServerServiceSelectorEntityRule,
 		},
 	}...)
@@ -1845,6 +1850,12 @@ func (es *elasticsearchComponent) elasticsearchAllowTigeraPolicy() *v3.NetworkPo
 					Action:      v3.Allow,
 					Protocol:    &networkpolicy.TCPProtocol,
 					Source:      networkpolicy.ESGatewaySourceEntityRule,
+					Destination: elasticSearchIngressDestinationEntityRule,
+				},
+				{
+					Action:      v3.Allow,
+					Protocol:    &networkpolicy.TCPProtocol,
+					Source:      networkpolicy.LinseedSourceEntityRule,
 					Destination: elasticSearchIngressDestinationEntityRule,
 				},
 				{

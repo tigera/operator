90e1e7acd5d020873cf12850f414ee287e4912d2
rene-dekker <rene@tigera.io>
2023-02-03 08:17:11 -0800

    Use the right registry for OSS cni-fips

--- pkg/components/references.go
+++ pkg/components/references.go
@@ -1,10 +1,10 @@
-// Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,6 +36,7 @@ func GetReference(c component, registry, imagePath, imagePrefix string, is *oper
 		switch c {
 		case ComponentCalicoNode,
 			ComponentCalicoCNI,
+			ComponentCalicoCNIFIPS,
 			ComponentCalicoTypha,
 			ComponentCalicoKubeControllers,
 			ComponentFlexVolume,

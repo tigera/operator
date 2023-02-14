3769674aeaf96116c3695e56b38c4add8969ddb4
Erik Stidham <erik@tigera.io>
2023-02-14 14:44:14 -0600

    Trying pinning v3.25.0 for calico node

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2020-2023 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ var (
 	}
 
 	ComponentCalicoNode = component{
-		Version: "master",
+		Version: "v3.25.0",
 		Image:   "calico/node",
 	}
 

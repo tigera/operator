fd8229d0454618e21c8cb2a936bd76aaa93b6e93
rene-dekker <rene@tigera.io>
2023-02-21 11:51:20 -0800

    Update comment for elasticsearch probe timeout.

--- pkg/render/logstorage.go
+++ pkg/render/logstorage.go
@@ -592,7 +592,7 @@ func (es elasticsearchComponent) podTemplate() corev1.PodTemplateSpec {
 					Command: []string{"/usr/bin/readiness-probe"},
 				},
 			},
-			// 30s (init) + 10 * 10s (timeout) + 9 * 5s (period) which is approximately 3 minutes
+			// 30s (init) + 10 * 20s (timeout) + 9 * 5s (period) which is approximately 4 minutes
 			// to account for a slow elasticsearch start.
 			FailureThreshold:    10,
 			InitialDelaySeconds: 30,

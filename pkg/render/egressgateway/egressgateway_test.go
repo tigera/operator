085c378f076281677d56fe97bf46316b2328b5e9
sridhar <sridhar@tigera.io>
2023-02-23 11:49:28 -0800

    UT for pull secrets

--- pkg/render/egressgateway/egressgateway_test.go
+++ pkg/render/egressgateway/egressgateway_test.go
@@ -37,6 +37,7 @@ var _ = Describe("Egress Gateway rendering tests", func() {
 	var healthTimeoutDS int32 = 30
 	var interval int32 = 20
 	var timeout int32 = 40
+	var pullSecrets []*corev1.Secret
 	rbac := "rbac.authorization.k8s.io"
 	logSeverity := operatorv1.LogLevelInfo
 	labels := map[string]string{"egress-code": "red"}
@@ -87,6 +88,16 @@ var _ = Describe("Egress Gateway rendering tests", func() {
 		}
 		egw.Name = "egress-test"
 		egw.Namespace = "test-ns"
+
+		pullSecrets = []*corev1.Secret{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-secret",
+					Namespace: "test-ns",
+				},
+				TypeMeta: metav1.TypeMeta{Kind: "Secret", APIVersion: "v1"},
+			}}
+
 	})
 
 	It("should render EGW deployment", func() {
@@ -97,6 +108,7 @@ var _ = Describe("Egress Gateway rendering tests", func() {
 			version string
 			kind    string
 		}{
+			{"test-secret", "test-ns", "", "v1", "Secret"},
 			{"egress-test", "test-ns", "", "v1", "ServiceAccount"},
 			{"egress-test", "test-ns", "apps", "v1", "Deployment"},
 		}
@@ -113,7 +125,7 @@ var _ = Describe("Egress Gateway rendering tests", func() {
 		}
 
 		component := egressgateway.EgressGateway(&egressgateway.Config{
-			PullSecrets:  nil,
+			PullSecrets:  pullSecrets,
 			Installation: installation,
 			OSType:       rmeta.OSTypeLinux,
 			EgressGW:     egw,
@@ -314,5 +326,4 @@ var _ = Describe("Egress Gateway rendering tests", func() {
 			rtest.ExpectResource(resources[i], expectedRes.name, expectedRes.ns, expectedRes.group, expectedRes.version, expectedRes.kind)
 		}
 	})
-
 })

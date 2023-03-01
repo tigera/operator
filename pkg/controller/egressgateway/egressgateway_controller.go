3d815cec3ace3a491226369684e415a6f7bc203a
sridhartigera <63839878+sridhartigera@users.noreply.github.com>
2023-03-01 12:53:09 -0800

    Cherry-pick PR 2499 (#2512)
    * Copy pull secrets to the EGW namespace (#2499)
    
    * Fix make gen-versions

--- pkg/controller/egressgateway/egressgateway_controller.go
+++ pkg/controller/egressgateway/egressgateway_controller.go
@@ -310,13 +310,13 @@ func (r *ReconcileEgressGateway) Reconcile(ctx context.Context, request reconcil
 		return reconcile.Result{}, err
 	}
 
-	// Fetch any existing default FelixConfiguration object.
-	fc := &crdv1.FelixConfiguration{}
-	err = r.client.Get(ctx, types.NamespacedName{Name: "default"}, fc)
-	if err != nil && !errors.IsNotFound(err) {
-		r.status.SetDegraded(operatorv1.ResourceReadError, "Error reading FelixConfiguration", err, reqLogger)
+	// patch and get the felix configuration
+	fc, err := r.patchFelixConfig(ctx)
+	if err != nil {
+		reqLogger.Error(err, "Error patching felix configuration")
+		r.status.SetDegraded(operatorv1.ResourcePatchError, "Error patching felix configuration", err, reqLogger)
 		for _, egw := range egwsToReconcile {
-			setDegraded(r.client, ctx, &egw, reconcileErr, fmt.Sprintf("Error reading felix configuration err = %s", err.Error()))
+			setDegraded(r.client, ctx, &egw, reconcileErr, fmt.Sprintf("Error patching felix configuration err = %s", err.Error()))
 		}
 		return reconcile.Result{}, err
 	}
@@ -421,6 +421,26 @@ func (r *ReconcileEgressGateway) reconcileEgressGateway(ctx context.Context, egw
 	egw.Status.State = operatorv1.TigeraStatusReady
 	setAvailable(r.client, ctx, egw, string(operatorv1.AllObjectsAvailable), "All objects available")
 	return nil
+}
+
+func (r *ReconcileEgressGateway) patchFelixConfig(ctx context.Context) (*crdv1.FelixConfiguration, error) {
+	// Fetch any existing default FelixConfiguration object.
+	fc := &crdv1.FelixConfiguration{}
+	err := r.client.Get(ctx, types.NamespacedName{Name: "default"}, fc)
+	if err != nil && !errors.IsNotFound(err) {
+		r.status.SetDegraded(operatorv1.ResourceReadError, "Unable to read FelixConfiguration", err, log)
+		return nil, err
+	}
+
+	patchFrom := client.MergeFrom(fc.DeepCopy())
+	if fc.Spec.PolicySyncPathPrefix != "" {
+		return fc, nil
+	}
+	fc.Spec.PolicySyncPathPrefix = "/var/run/nodeagent"
+	if err := r.client.Patch(ctx, fc, patchFrom); err != nil {
+		return nil, err
+	}
+	return fc, nil
 }
 
 // getRequestedEgressGateway returns the namespaced EgressGateway instance.

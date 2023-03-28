ff4d2f5da210076395888f6187b5d77c6a82e212
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-03-27 17:53:11 -0700

    Allow connection between DPI and linseed

--- pkg/render/intrusiondetection/dpi/dpi_test.go
+++ pkg/render/intrusiondetection/dpi/dpi_test.go
@@ -18,6 +18,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/tigera/operator/pkg/tls/certificatemanagement"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -157,6 +158,7 @@ var _ = Describe("DPI rendering tests", func() {
 		clusterDomain = "cluster.local"
 		installation  *operatorv1.InstallationSpec
 		typhaNodeTLS  *render.TyphaNodeTLS
+		dpiCertSecret certificatemanagement.KeyPairInterface
 		cfg           *dpi.DPIConfig
 	)
 
@@ -176,7 +178,9 @@ var _ = Describe("DPI rendering tests", func() {
 		Expect(err).NotTo(HaveOccurred())
 		typhaKeyPair, err := certificateManager.GetOrCreateKeyPair(cli, render.TyphaTLSSecretName, common.OperatorNamespace(), []string{render.FelixCommonName})
 		Expect(err).NotTo(HaveOccurred())
-		trustedBundle := certificateManager.CreateTrustedBundle(nodeKeyPair, typhaKeyPair)
+		dpiCertSecret, err = certificateManager.GetOrCreateKeyPair(cli, render.DPITLSSecretName, common.OperatorNamespace(), []string{""})
+		Expect(err).NotTo(HaveOccurred())
+		trustedBundle := certificateManager.CreateTrustedBundle(nodeKeyPair, typhaKeyPair, dpiCertSecret)
 		typhaNodeTLS = &render.TyphaNodeTLS{
 			TyphaSecret:   typhaKeyPair,
 			NodeSecret:    nodeKeyPair,
@@ -192,8 +196,8 @@ var _ = Describe("DPI rendering tests", func() {
 			HasNoLicense:       false,
 			HasNoDPIResource:   false,
 			ESClusterConfig:    esConfigMap,
-			ESSecrets:          nil,
 			ClusterDomain:      dns.DefaultClusterDomain,
+			DPICertSecret:      dpiCertSecret,
 		}
 	})
 
@@ -220,6 +224,7 @@ var _ = Describe("DPI rendering tests", func() {
 
 		ds := rtest.GetResource(resources, dpi.DeepPacketInspectionName, dpi.DeepPacketInspectionNamespace, "apps", "v1", "DaemonSet").(*appsv1.DaemonSet)
 		Expect(ds.Spec.Template.Spec.Containers[0].Env).Should(ContainElements(
+			// TODO: Alina - checks vars
 			corev1.EnvVar{Name: "ELASTIC_INDEX_SUFFIX", Value: "clusterTestName"},
 		))
 		Expect(len(ds.Spec.Template.Spec.Containers)).Should(Equal(1))

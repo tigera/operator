66f5325b31b54aa7155efd7b221511723efb30c3
Jiawei Huang <jiawei@tigera.io>
2023-04-17 19:09:07 -0700

    Update k8s dependencies to 1.26 release train

--- pkg/common/k8svalidation/validation.go
+++ pkg/common/k8svalidation/validation.go
@@ -1,7 +1,7 @@
 package k8svalidation
 
 /*
-Copyright (c) 2022 Tigera, Inc. All rights reserved.
+Copyright (c) 2022-2023 Tigera, Inc. All rights reserved.
 
 Copyright 2014 The Kubernetes Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -111,8 +111,8 @@ func validatePodAffinityTerms(podAffinityTerms []core.PodAffinityTerm, fldPath *
 func validatePodAffinityTerm(podAffinityTerm core.PodAffinityTerm, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
 
-	allErrs = append(allErrs, unversionedvalidation.ValidateLabelSelector(podAffinityTerm.LabelSelector, fldPath.Child("labelSelector"))...)
-	allErrs = append(allErrs, unversionedvalidation.ValidateLabelSelector(podAffinityTerm.NamespaceSelector, fldPath.Child("namespaceSelector"))...)
+	allErrs = append(allErrs, unversionedvalidation.ValidateLabelSelector(podAffinityTerm.LabelSelector, unversionedvalidation.LabelSelectorValidationOptions{}, fldPath.Child("labelSelector"))...)
+	allErrs = append(allErrs, unversionedvalidation.ValidateLabelSelector(podAffinityTerm.NamespaceSelector, unversionedvalidation.LabelSelectorValidationOptions{}, fldPath.Child("namespaceSelector"))...)
 
 	for _, name := range podAffinityTerm.Namespaces {
 		for _, msg := range validateNamespaceName(name, false) {

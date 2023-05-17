e4f1d8d094aa3d431f3d6cbf82cb876c118c5f66
Vara <vara@tigera.io>
2023-05-17 12:32:53 -0700

    Move Default namespaces list to Manager

--- pkg/controller/manager/manager_controller.go
+++ pkg/controller/manager/manager_controller.go
@@ -360,21 +360,9 @@ func (r *ReconcileManager) Reconcile(ctx context.Context, request reconcile.Requ
 	}
 
 	// Populate a list of namespaces to be displayed in the service graph Tigera infrastructure layer.
-	sgLayerTigeraNameSpaces := map[string]bool{
-		"tigera-compliance":            true,
-		"tigera-dpi":                   true,
-		"tigera-eck-operator":          true,
-		"tigera-elasticsearch":         true,
-		"tigera-fluentd":               true,
-		"tigera-intrusion-detection":   true,
-		"tigera-kibana":                true,
-		"tigera-operator":              true,
-		"tigera-packetcapture":         true,
-		"tigera-policy-recommendation": true,
-		"tigera-prometheus":            true,
-		"tigera-system":                true,
-		"calico-system":                true,
-		"tigera-manager":               true,
+	sgLayerTigeraNameSpaces := render.DefaultSGLayerTigeraNamespaces()
+	if !sgLayerTigeraNameSpaces[render.ManagerNamespace] {
+		sgLayerTigeraNameSpaces[render.ManagerNamespace] = true
 	}
 
 	// Fetch the Authentication spec. If present, we use to configure user authentication.

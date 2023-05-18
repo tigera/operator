28bc56a0cb98212b338b55cce6f3b8db804077e8
Daniel Fox <dan.fox@tigera.io>
2023-05-18 11:14:28 -0700

    More release-v3.26 updates

--- pkg/components/calico.go
+++ pkg/components/calico.go
@@ -20,55 +20,55 @@ package components
 import "github.com/tigera/operator/version"
 
 var (
-	CalicoRelease string = "v3.25.1"
+	CalicoRelease string = "v3.26.0"
 
 	ComponentCalicoCNI = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoCNIFIPS = component{
-		Version: "v3.25.1-fips",
+		Version: "release-v3.26-fips",
 		Image:   "calico/cni",
 	}
 
 	ComponentCalicoKubeControllers = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/kube-controllers",
 	}
 
 	ComponentCalicoNode = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/node",
 	}
 
 	ComponentCalicoTypha = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/typha",
 	}
 
 	ComponentFlexVolume = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/pod2daemon-flexvol",
 	}
 
 	ComponentCalicoAPIServer = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/apiserver",
 	}
 
 	ComponentWindowsUpgrade = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/windows-upgrade",
 	}
 
 	ComponentCalicoCSI = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/csi",
 	}
 
 	ComponentCalicoCSIRegistrar = component{
-		Version: "v3.25.1",
+		Version: "release-v3.26",
 		Image:   "calico/node-driver-registrar",
 	}
 	ComponentOperatorInit = component{

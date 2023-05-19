ae2b66580c4e840c8473ad014265d0c17a966ca7
Seth Malaki <seth@tigera.io>
2023-05-19 19:47:57 +0100

    rs-979: [applicationlayer] configurable envoy xff settings

--- pkg/controller/applicationlayer/applicationlayer_controller.go
+++ pkg/controller/applicationlayer/applicationlayer_controller.go
@@ -282,6 +282,8 @@ func (r *ReconcileApplicationLayer) Reconcile(ctx context.Context, request recon
 		LogRequestsPerInterval: lcSpec.LogRequestsPerInterval,
 		LogIntervalSeconds:     lcSpec.LogIntervalSeconds,
 		ModSecurityConfigMap:   modSecurityRuleSet,
+		UseRemoteAddressXFF:    applicationLayer.Spec.EnvoySettings.XFFUseRemoteAddress,
+		NumTrustedHopsXFF:      applicationLayer.Spec.EnvoySettings.XFFNumTrustedHops,
 		UsePSP:                 r.usePSP,
 	}
 	component := applicationlayer.ApplicationLayer(config)
@@ -358,6 +360,10 @@ func updateApplicationLayerWithDefaults(al *operatorv1.ApplicationLayer) {
 
 	if al.Spec.ApplicationLayerPolicy == nil {
 		al.Spec.ApplicationLayerPolicy = &defaultApplicationLayerPolicyStatusType
+	}
+
+	if al.Spec.EnvoySettings == nil {
+		al.Spec.EnvoySettings = &operatorv1.EnvoySettings{}
 	}
 }
 

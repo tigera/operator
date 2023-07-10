2069769c65ef4691f4a5a579d4035f4e712b0ca4
Casey Davenport <davenport.cas@gmail.com>
2023-07-10 16:17:26 -0700

    Add Tenant CR and switch to it as the trigger instead of Manager

--- pkg/controller/logstorage/linseed/linseed_controller.go
+++ pkg/controller/logstorage/linseed/linseed_controller.go
@@ -39,7 +39,6 @@ import (
 	octrl "github.com/tigera/operator/pkg/controller"
 	"github.com/tigera/operator/pkg/controller/certificatemanager"
 	logstoragecommon "github.com/tigera/operator/pkg/controller/logstorage/common"
-	omanager "github.com/tigera/operator/pkg/controller/manager"
 	"github.com/tigera/operator/pkg/controller/options"
 	"github.com/tigera/operator/pkg/controller/status"
 	"github.com/tigera/operator/pkg/controller/utils"
@@ -102,14 +101,14 @@ func Add(mgr manager.Manager, opts options.AddOptions) error {
 		return fmt.Errorf("logstorage-controller failed to watch logstorage Tigerastatus: %w", err)
 	}
 	if opts.MultiTenant {
-		if err = c.Watch(&source.Kind{Type: &operatorv1.Manager{}}, &handler.EnqueueRequestForObject{}); err != nil {
-			return fmt.Errorf("log-storage-controller failed to watch Manager resource: %w", err)
+		if err = c.Watch(&source.Kind{Type: &operatorv1.Tenant{}}, &handler.EnqueueRequestForObject{}); err != nil {
+			return fmt.Errorf("log-storage-controller failed to watch Tenant resource: %w", err)
 		}
 	}
 
 	// The namespace(s) we need to monitor depend upon what tenancy mode we're running in.
 	// For single-tenant, everything is installed in the tigera-manager namespace.
-	installNS := render.ManagerNamespace
+	installNS := render.ElasticsearchNamespace
 	truthNS := common.OperatorNamespace()
 	if opts.MultiTenant {
 		// For multi-tenant, the manager could be installed in any number of namespaces.
@@ -170,8 +169,8 @@ func (r *LinseedSubController) Reconcile(ctx context.Context, request reconcile.
 
 	// When running in multi-tenant mode, we need to install Linseed in tenant Namespaces. However, the LogStorage
 	// resource is still cluster-scoped (since ES is a cluster-wide resource), so we need to look elsewhere to determine
-	// which tenant namespaces require a Linseed instance. We use the manager API to determine the set of namespaces that should have a Linseed.
-	var manager *operatorv1.Manager
+	// which tenant namespaces require a Linseed instance. We use the tenant API to determine the set of namespaces that should have a Linseed.
+	var tenant *operatorv1.Tenant
 	var err error
 	if r.multiTenant {
 		if request.Namespace == "" {
@@ -180,13 +179,13 @@ func (r *LinseedSubController) Reconcile(ctx context.Context, request reconcile.
 		}
 
 		// Check if there is a manager in this namespace.
-		manager, err = omanager.GetManager(ctx, r.client, request.Namespace)
+		tenant, err = utils.GetTenant(ctx, r.client, request.Namespace)
 		if errors.IsNotFound(err) {
-			// No manager in this namespace. Ignore the update.
-			reqLogger.Info("No Manager in this Namespace, skip")
+			// No tenant in this namespace. Ignore the update.
+			reqLogger.Info("No Tenant in this Namespace, skip")
 			return reconcile.Result{}, nil
 		} else if err != nil {
-			r.status.SetDegraded(operatorv1.ResourceReadError, "An error occurred while querying Manager", err, reqLogger)
+			r.status.SetDegraded(operatorv1.ResourceReadError, "An error occurred while querying Tenant", err, reqLogger)
 			return reconcile.Result{}, err
 		}
 	}
@@ -210,13 +209,13 @@ func (r *LinseedSubController) Reconcile(ctx context.Context, request reconcile.
 	r.status.OnCRFound()
 
 	req := octrl.NewRequest(request.NamespacedName, r.multiTenant, render.ElasticsearchNamespace)
-	args := ReconcileArgs{LogStorage: ls, Manager: manager}
+	args := ReconcileArgs{LogStorage: ls, Tenant: tenant}
 	return r.reconcile(ctx, reqLogger, args, req)
 }
 
 type ReconcileArgs struct {
 	LogStorage *operatorv1.LogStorage
-	Manager    *operatorv1.Manager
+	Tenant     *operatorv1.Tenant
 }
 
 func (r *LinseedSubController) reconcile(ctx context.Context, reqLogger logr.Logger, args ReconcileArgs, request octrl.Request) (reconcile.Result, error) {
@@ -318,7 +317,7 @@ func (r *LinseedSubController) reconcile(ctx context.Context, reqLogger logr.Log
 	// In standard installs, the LogStorage owns Linseed. For multi-tenant, it's owned by the Manager instance.
 	var hdler utils.ComponentHandler
 	if r.multiTenant {
-		hdler = utils.NewComponentHandler(reqLogger, r.client, r.scheme, args.Manager)
+		hdler = utils.NewComponentHandler(reqLogger, r.client, r.scheme, args.Tenant)
 	} else {
 		hdler = utils.NewComponentHandler(reqLogger, r.client, r.scheme, args.LogStorage)
 	}

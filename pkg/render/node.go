0af3ebe47d36956ac593544d3511ea82cd4d6ac6
Laszlo Vigh <dzac@leves.info>
2023-08-02 09:40:49 +0000

    replace value of FELIX_HEALTHPORT instead of appending it

--- pkg/render/node.go
+++ pkg/render/node.go
@@ -1656,7 +1656,12 @@ func (c *nodeComponent) nodeEnvVars() []corev1.EnvVar {
 	switch c.cfg.Installation.KubernetesProvider {
 	case operatorv1.ProviderOpenShift:
 		// For Openshift, we need special configuration since our default port is already in use.
-		nodeEnv = append(nodeEnv, corev1.EnvVar{Name: "FELIX_HEALTHPORT", Value: "9199"})
+		for i := range nodeEnv {
+			if nodeEnv[i].Name == "FELIX_HEALTHPORT" {
+				nodeEnv[i].Value = "9199"
+				break
+			}
+		}
 	// For AKS/AzureVNET and EKS/VPCCNI, we must explicitly ask felix to add host IP's to wireguard ifaces
 	case operatorv1.ProviderAKS:
 		if c.cfg.Installation.CNI.Type == operatorv1.PluginAzureVNET {

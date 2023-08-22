60e94862ed2a85913cc0611cc55616f77d43629b
Rene Dekker <rene@tigera.io>
2023-08-22 15:56:12 -0700

    Update ES&KB to v7.17.11. (#2817)

--- pkg/components/enterprise.go
+++ pkg/components/enterprise.go
@@ -56,11 +56,11 @@ var (
 	}
 
 	ComponentEckElasticsearch = component{
-		Version: "7.17.9",
+		Version: "7.17.11",
 	}
 
 	ComponentEckKibana = component{
-		Version: "7.17.9",
+		Version: "7.17.11",
 	}
 
 	ComponentElasticTseeInstaller = component{

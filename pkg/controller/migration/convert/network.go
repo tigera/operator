5550649e4eaa78055b769e2d979d0cba3ae37834
cyclinder <qifeng.guo@daocloud.io>
2023-09-20 10:18:55 +0800

    change default cni name from k8s-pod-network to calico

--- pkg/controller/migration/convert/network.go
+++ pkg/controller/migration/convert/network.go
@@ -198,11 +198,11 @@ func handleCalicoCNI(c *components, install *operatorv1.Installation) error {
 		install.Spec.CalicoNetwork.HostPorts = &hp
 	}
 
-	if c.cni.ConfigName != "k8s-pod-network" {
+	if c.cni.ConfigName != "calico" {
 		return ErrIncompatibleCluster{
-			err:       fmt.Sprintf("only 'k8s-pod-network' is supported as CNI name, found %s", c.cni.ConfigName),
+			err:       fmt.Sprintf("only 'calico' is supported as CNI name, found %s", c.cni.ConfigName),
 			component: ComponentCNIConfig,
-			fix:       "set CNI config name to 'k8s-pod-network'",
+			fix:       "set CNI config name to 'calico'",
 		}
 	}
 

78eb28074de7c5618f4342bbe3f67741d0373ff6
Steven Boland <steven@tigera.io>
2023-10-16 11:37:28 +0100

    remove dead code

--- pkg/controller/installation/bpf.go
+++ pkg/controller/installation/bpf.go
@@ -26,16 +26,30 @@ import (
 	"github.com/tigera/operator/pkg/controller/migration/convert"
 	"github.com/tigera/operator/pkg/render"
 	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func bpfUpgradeWithoutDisruption(r *ReconcileInstallation, ctx context.Context, install *operator.Installation, ds *appsv1.DaemonSet, fc *crdv1.FelixConfiguration, reqLogger logr.Logger) error {
 
-	// Extract
+	// Use case #1
+	patchFelixConfig, err := queryDaemonsetEnvVar(r, ctx, ds, fc, reqLogger)
+	if err != nil {
+		return err
+	}
+
+	if !patchFelixConfig {
+
+	}
+
+	return nil
+}
+
+func queryDaemonsetEnvVar(r *ReconcileInstallation, ctx context.Context, ds *appsv1.DaemonSet, fc *crdv1.FelixConfiguration, reqLogger logr.Logger) (bool, error) {
+
 	dsBpfEnabledEnvVar, err := convert.GetEnv(ctx, r.client, ds.Spec.Template.Spec, convert.ComponentCalicoNode, common.NodeDaemonSetName, "FELIX_BPFENABLED")
 	if err != nil {
 		reqLogger.Error(err, "An error occurred when querying Calico-Node environment variable FELIX_BPFENABLED")
+		return false, err
 
 	}
 
@@ -44,21 +58,18 @@ func bpfUpgradeWithoutDisruption(r *ReconcileInstallation, ctx context.Context, 
 		dsBpfEnabledStatus, err = strconv.ParseBool(*dsBpfEnabledEnvVar)
 		if err != nil {
 			reqLogger.Error(err, "An error occurred when converting Calico-Node environment variable FELIX_BPFENABLED")
-			return err
+			return false, err
 		}
 	}
 
-	// Use case #1
 	if dsBpfEnabledStatus && fc.Spec.BPFEnabled == nil {
 		err = patchFelixConfiguration(r, ctx, fc, reqLogger, dsBpfEnabledStatus)
-		if err == nil {
-			// TODO - better msg
-			reqLogger.Info("Successfully patched FelixConfig")
+		if err != nil {
+			return false, err
 		}
-		return err
 	}
 
-	return err
+	return true, nil
 }
 
 func patchFelixConfiguration(r *ReconcileInstallation, ctx context.Context, fc *crdv1.FelixConfiguration, reqLogger logr.Logger, patchBpfEnabled bool) error {
@@ -86,103 +97,3 @@ func patchFelixConfiguration(r *ReconcileInstallation, ctx context.Context, fc *
 
 	return nil
 }
-
-func AdrianaX(ds *appsv1.DaemonSet) (string, error) {
-	return "false", nil
-}
-
-func (r *ReconcileInstallation) Adriana1(ctx context.Context, install *operator.Installation, fc *crdv1.FelixConfiguration, log logr.Logger) error {
-	r.Adriana2(ctx, log)
-	return setStevepro(r, ctx, install, fc, log)
-}
-
-// Exctract env var
-func (r *ReconcileInstallation) Adriana2(ctx context.Context, log logr.Logger) error {
-	var err error
-
-	// Get the calico-node daemonset.
-	calicoNodeDaemonset := appsv1.DaemonSet{}
-	err = r.client.Get(ctx, types.NamespacedName{Namespace: common.CalicoNamespace, Name: common.NodeDaemonSetName}, &calicoNodeDaemonset)
-	if err != nil {
-		// TODO log error
-		return err
-	}
-
-	// Attempt to extract the env var.
-	bpfEnabledEnvVar, err := convert.GetEnv(ctx, r.client, calicoNodeDaemonset.Spec.Template.Spec, convert.ComponentCalicoNode, common.NodeDaemonSetName, "FELIX_BPFENABLED")
-	if err != nil {
-		log.Error(err, "An error occurred when querying Calico-Node environment variable FELIX_BPFENABLED")
-
-	}
-	bpfEnabledStatus := false
-	if bpfEnabledEnvVar != nil {
-		bpfEnabledStatus, err = strconv.ParseBool(*bpfEnabledEnvVar)
-		if err != nil {
-			log.Error(err, "An error occurred when converting Calico-Node environment variable FELIX_BPFENABLED")
-			return err
-		}
-	}
-	_ = bpfEnabledStatus
-
-	return nil
-}
-
-func setStevepro(r *ReconcileInstallation, ctx context.Context, install *operator.Installation, fc *crdv1.FelixConfiguration, log logr.Logger) error {
-	patchFrom := client.MergeFrom(fc.DeepCopy())
-
-	bpfEnabled := common.BpfDataplaneEnabled(&install.Spec)
-
-	// Managed fields "light".
-	var fcAnnotations map[string]string
-	if fc.Annotations == nil {
-		fcAnnotations = make(map[string]string)
-	} else {
-		fcAnnotations = fc.Annotations
-	}
-	fcAnnotations[render.BpfOperatorAnnotation] = "true"
-	fc.SetAnnotations(fcAnnotations)
-
-	// TODO - check from #1
-	//bpfEnabled = false
-	fc.Spec.BPFEnabled = &bpfEnabled
-	if err := r.client.Patch(ctx, fc, patchFrom); err != nil {
-		log.Info("adriana-1.30.64 setStevepro ERROR:")
-		return err
-	}
-	return nil
-}
-
-/*
-func setStevepro1(r *ReconcileInstallation, ctx context.Context, install *operator.Installation, fc *crdv1.FelixConfiguration, log logr.Logger) error {
-
-	log.Info("adriana-1.30.64 setStevepro beg")
-	patchFrom := client.MergeFrom(fc.DeepCopy())
-
-	if fc.Annotations != nil {
-		an := fc.Annotations
-		an[render.BpfOperatorAnnotation] = "true"
-		fc.SetAnnotations(an)
-	}
-
-	log.Info("adriana-1.30.64 setStevepro bpf BEG")
-	bpfEnabled := common.BpfDataplaneEnabled(&install.Spec)
-	if bpfEnabled {
-		log.Info("adriana-1.30.64 setStevepro bpf val=TRUE")
-	} else {
-		log.Info("adriana-1.30.64 setStevepro bpf val=false")
-	}
-	//log.Info("adriana-1.30.64 setStevepro bpf val", bpfEnabled)
-	log.Info("adriana-1.30.64 setStevepro bpf end")
-	//bpfEnabled := true
-	fc.Spec.BPFLogLevel = "Info"
-	fc.Spec.BPFEnabled = &bpfEnabled
-	if err := r.client.Patch(ctx, fc, patchFrom); err != nil {
-		log.Info("adriana-1.30.64 setStevepro ERROR:")
-		return err
-	}
-	log.Info("adriana-1.30.64 setStevepro patched:")
-	log.Info("adriana-1.30.64 setStevepro end")
-
-	return nil
-}
-*/

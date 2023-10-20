5f95c0f6314bda480a10f891b62d71b65d60bd26
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-10-20 14:24:55 -0700

    Various fixes for multi-tenancy (#2936)
    * Various fixes for multi-tenancy
    
    * Use projectcalico.org/name for namspace selection and add more env variables for multi-tenant

--- pkg/render/logstorage/linseed/linseed.go
+++ pkg/render/logstorage/linseed/linseed.go
@@ -309,6 +309,7 @@ func (l *linseed) linseedDeployment() *appsv1.Deployment {
 		// If a tenant was provided, set the expected tenant ID and enable the shared index backend.
 		envVars = append(envVars, corev1.EnvVar{Name: "LINSEED_EXPECTED_TENANT_ID", Value: l.cfg.Tenant.Spec.ID})
 		envVars = append(envVars, corev1.EnvVar{Name: "BACKEND", Value: "elastic-single-index"})
+		envVars = append(envVars, corev1.EnvVar{Name: "LINSEED_MULTI_CLUSTER_FORWARDING_ENDPOINT", Value: fmt.Sprintf("https://tigera-manager.%s.svc:9443", l.cfg.Tenant.Namespace)})
 	}
 
 	var initContainers []corev1.Container

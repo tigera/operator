1136fd67a06d77518f735cf0f78a86d714eb1145
Alina Militaru <41362174+asincu@users.noreply.github.com>
2023-11-30 13:45:54 -0800

    Allow linseed to read secrets in tigera-operator namespace
    This permission is needed because Linseed Controller needs to read
    secrets before copying tigera-voltron-linseed-certs-public certificate
    to the managed cluster.

--- pkg/render/logstorage.go
+++ pkg/render/logstorage.go
@@ -22,6 +22,8 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/tigera/operator/pkg/common"
+
 	cmnv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/common/v1"
 	esv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/elasticsearch/v1"
 	kbv1 "github.com/elastic/cloud-on-k8s/v2/pkg/apis/kibana/v1"
@@ -2118,7 +2120,8 @@ func (m *managedClusterLogStorage) elasticsearchExternalService() *corev1.Servic
 func (m managedClusterLogStorage) linseedExternalRolesAndBindings() ([]*rbacv1.ClusterRole, []*rbacv1.RoleBinding) {
 	// Create separate ClusterRoles for necessary configmap and secret operations, then bind them to the namespaces
 	// where they are required so that we're only granting exactly which permissions we need in the namespaces in which
-	// they're required
+	// they're required. Other controllers may also bind this cluster role to their own namespace if they require
+	// linseed access tokens.
 	secretsRole := &rbacv1.ClusterRole{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: TigeraLinseedSecretsClusterRole,
@@ -2147,12 +2150,12 @@ func (m managedClusterLogStorage) linseedExternalRolesAndBindings() ([]*rbacv1.C
 		},
 	}
 
-	// Bind the secrets permission to the tigera-fluentd namespace. Other controllers may also bind
-	// this cluster role to their own namespace if they require linseed access tokens.
+	// Bind the secrets permission to the operator namespace. This binding now adds permissions for Linseed to create
+	// its public cert secret in the tigera-operator namespace
 	secretBinding := &rbacv1.RoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "tigera-linseed",
-			Namespace: fluentdName,
+			Namespace: common.OperatorNamespace(),
 		},
 		RoleRef: rbacv1.RoleRef{
 			APIGroup: "rbac.authorization.k8s.io",
@@ -2188,5 +2191,5 @@ func (m managedClusterLogStorage) linseedExternalRolesAndBindings() ([]*rbacv1.C
 		},
 	}
 
-	return []*rbacv1.ClusterRole{secretsRole, configMapsRole}, []*rbacv1.RoleBinding{secretBinding, configMapBinding}
+	return []*rbacv1.ClusterRole{secretsRole, configMapsRole}, []*rbacv1.RoleBinding{configMapBinding, secretBinding}
 }

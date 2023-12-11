0932196fe1ed263aefcaac92e4542b3841600f11
Bart Biskupek <bart@tigera.io>
2023-12-11 09:33:33 +0000

    cluster name in webhooks-processor configuration on managed cluster.

--- pkg/render/intrusion_detection.go
+++ pkg/render/intrusion_detection.go
@@ -793,7 +793,15 @@ func (c *intrusionDetectionComponent) webhooksControllerContainer() corev1.Conta
 
 	volumeMounts := c.cfg.TrustedCertBundle.VolumeMounts(c.SupportedOSType())
 	volumeMounts = append(volumeMounts, c.cfg.IntrusionDetectionCertSecret.VolumeMount(c.SupportedOSType()))
+
+	// additional settings required for the managed cluster:
 	if c.cfg.ManagedCluster {
+		envVars = append(envVars,
+			corev1.EnvVar{
+				Name:  "LINSEED_CLUSTER",
+				Value: c.cfg.ESClusterConfig.ClusterName(),
+			},
+		)
 		volumeMounts = append(volumeMounts,
 			corev1.VolumeMount{
 				Name:      LinseedTokenVolumeName,

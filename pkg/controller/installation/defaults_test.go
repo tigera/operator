bc2b51b95ccd7d371f8609a89302b1b69831d875
Casey Davenport <davenport.cas@gmail.com>
2024-01-04 08:48:37 -0800

    Fix static checks

--- pkg/controller/installation/defaults_test.go
+++ pkg/controller/installation/defaults_test.go
@@ -538,7 +538,8 @@ var _ = Describe("Defaulting logic tests", func() {
 					CNI: &operator.CNISpec{},
 				},
 			}
-			updateInstallationForAWSNode(instance, &appsv1.DaemonSet{})
+			err := updateInstallationForAWSNode(instance, &appsv1.DaemonSet{})
+			Expect(err).NotTo(HaveOccurred())
 			Expect(instance.Spec.CNI.Type).To(Equal(operator.PluginAmazonVPC))
 		})
 	})

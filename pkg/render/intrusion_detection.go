921fb30bb1ba798bcc406aa0df41a3479f7687dc
Bart Biskupek <bartolini@users.noreply.github.com>
2024-01-09 16:45:47 +0000

    webhooks should be able to watch config maps and secrets for changes. (#3092)

--- pkg/render/intrusion_detection.go
+++ pkg/render/intrusion_detection.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2023 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019-2024 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -508,7 +508,7 @@ func (c *intrusionDetectionComponent) intrusionDetectionClusterRole() *rbacv1.Cl
 			rbacv1.PolicyRule{
 				APIGroups: []string{""},
 				Resources: []string{"secrets", "configmaps"},
-				Verbs:     []string{"get"},
+				Verbs:     []string{"get", "watch"},
 			},
 			rbacv1.PolicyRule{
 				APIGroups: []string{"crd.projectcalico.org"},

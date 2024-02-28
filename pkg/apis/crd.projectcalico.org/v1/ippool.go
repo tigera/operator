71c8d15bfbbc8d48cff8bbba26ce5d18da2a3324
Casey Davenport <davenport.cas@gmail.com>
2024-02-28 14:39:09 -0800

    Add AllowedUses to IP pools

--- pkg/apis/crd.projectcalico.org/v1/ippool.go
+++ pkg/apis/crd.projectcalico.org/v1/ippool.go
@@ -70,7 +70,18 @@ type IPPoolSpec struct {
 	// using an IP pool that is backed by a subnet that belongs to another availability zone. If AWSSubnetID
 	// is specified, then the CIDR of the IP pool must be contained within the specified AWS subnet.
 	AWSSubnetID string `json:"awsSubnetID,omitempty" validate:"omitempty"`
+
+	// AllowedUse controls what the IP pool will be used for.  If not specified or empty, defaults to
+	// ["Tunnel", "Workload"] for back-compatibility
+	AllowedUses []IPPoolAllowedUse `json:"allowedUses,omitempty" validate:"omitempty"`
 }
+
+type IPPoolAllowedUse string
+
+const (
+	IPPoolAllowedUseWorkload IPPoolAllowedUse = "Workload"
+	IPPoolAllowedUseTunnel   IPPoolAllowedUse = "Tunnel"
+)
 
 type VXLANMode string
 

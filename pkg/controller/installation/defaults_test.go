31cb869e50bbcaf516bacaa3e2697156f957aab2
Casey Davenport <caseydavenport@users.noreply.github.com>
2024-03-04 09:46:02 -0800

    Make test variable private that didn't need to be public (#3221)

--- pkg/controller/installation/defaults_test.go
+++ pkg/controller/installation/defaults_test.go
@@ -63,7 +63,6 @@ var _ = Describe("Defaulting logic tests", func() {
 		Expect(*instance.Spec.Logging.CNI.LogFileMaxCount).To(Equal(uint32(10)))
 		Expect(*instance.Spec.Logging.CNI.LogFileMaxAgeDays).To(Equal(uint32(30)))
 		Expect(*instance.Spec.Logging.CNI.LogFileMaxSize).To(Equal(resource.MustParse("100Mi")))
-
 	})
 
 	It("should properly fill defaults on an empty TigeraSecureEnterprise instance", func() {
@@ -97,16 +96,16 @@ var _ = Describe("Defaulting logic tests", func() {
 	It("should not override custom configuration", func() {
 		var mtu int32 = 1500
 		var nodeMetricsPort int32 = 9081
-		var false_ = false
+		false_ := false
 		var twentySeven int32 = 27
 		var oneTwoThree int32 = 123
 		var one intstr.IntOrString = intstr.FromInt(1)
 		var replicas int32 = 3
 		var logFileMaxCount uint32 = 5
 		var logFileMaxAgeDays uint32 = 10
-		var logFileMaxSize = resource.MustParse("50Mi")
-		var logSeverity = operator.LogLevelError
-		var LinuxpolicySetupTimeoutSeconds int32 = 1
+		logFileMaxSize := resource.MustParse("50Mi")
+		logSeverity := operator.LogLevelError
+		var linuxPolicySetupTimeoutSeconds int32 = 1
 
 		hpEnabled := operator.HostPortsEnabled
 		disabled := operator.BGPDisabled
@@ -160,7 +159,7 @@ var _ = Describe("Defaulting logic tests", func() {
 					},
 					HostPorts:                      &hpEnabled,
 					MultiInterfaceMode:             &miMode,
-					LinuxPolicySetupTimeoutSeconds: &LinuxpolicySetupTimeoutSeconds,
+					LinuxPolicySetupTimeoutSeconds: &linuxPolicySetupTimeoutSeconds,
 				},
 				ControlPlaneReplicas: &replicas,
 				NodeMetricsPort:      &nodeMetricsPort,
@@ -192,14 +191,14 @@ var _ = Describe("Defaulting logic tests", func() {
 	It("should not override custom configuration (BPF)", func() {
 		var mtu int32 = 1500
 		var nodeMetricsPort int32 = 9081
-		var false_ = false
+		false_ := false
 		var twentySeven int32 = 27
 		var one intstr.IntOrString = intstr.FromInt(1)
 		var replicas int32 = 3
 		var logFileMaxCount uint32 = 5
 		var logFileMaxAgeDays uint32 = 10
-		var logFileMaxSize = resource.MustParse("50Mi")
-		var logSeverity = operator.LogLevelError
+		logFileMaxSize := resource.MustParse("50Mi")
+		logSeverity := operator.LogLevelError
 
 		disabled := operator.BGPDisabled
 		miMode := operator.MultiInterfaceModeNone

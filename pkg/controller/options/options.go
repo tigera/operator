b8c095232a9147a40f7da98bdb3c44dafce0761a
Vara <vara@tigera.io>
2024-03-25 15:14:56 -0700

    EV-4646 deprecate AWS SG integeration

--- pkg/controller/options/options.go
+++ pkg/controller/options/options.go
@@ -28,7 +28,6 @@ import (
 type AddOptions struct {
 	DetectedProvider    v1.Provider
 	EnterpriseCRDExists bool
-	AmazonCRDExists     bool
 	ClusterDomain       string
 	KubernetesVersion   *common.VersionInfo
 	ManageCRDs          bool

ca0b214caa46baba0103a1eacc57f762311dd3c6
sridhar <sridhar@tigera.io>
2024-07-18 16:41:28 -0700

    Apply default configs only when it is not set.

--- pkg/controller/installation/core_controller.go
+++ pkg/controller/installation/core_controller.go
@@ -1681,23 +1681,25 @@ func (r *ReconcileInstallation) setDefaultsOnFelixConfiguration(ctx context.Cont
 		updated = true
 	}
 	vxlanVNI := 4096
-	if fc.Spec.VXLANVNI == nil {
-		fc.Spec.VXLANVNI = &vxlanVNI
-		updated = true
-	}
-
 	// MKE uses a vxlanVNI:4096 and vxlanPort:4789 for its docker swarm vxlan.
 	// This results in a conflict with calico's VXLAN and the vxlan.calico interface
 	// gets deleted. To fix this we change the vxlanVNI to 10000 as recommended by
 	// MKE docs (https://docs.mirantis.com/mke/3.7/cli-ref/mke-cli-install.html).
 	if install.Spec.KubernetesProvider == operator.ProviderDockerEE {
-		// Set bpfHostConntrackBypass to false for eBPF dataplane to work with MKE
-		if install.Spec.BPFEnabled() {
-			disableBPFHostConntrackBypass(fc)
-		}
 		vxlanVNI = 10000
+	}
+
+	if fc.Spec.VXLANVNI == nil {
 		fc.Spec.VXLANVNI = &vxlanVNI
 		updated = true
+	}
+
+	if install.Spec.KubernetesProvider == operator.ProviderDockerEE {
+		// Set bpfHostConntrackBypass to false for eBPF dataplane to work with MKE
+		if install.Spec.BPFEnabled() && fc.Spec.BPFHostConntrackBypass == nil {
+			disableBPFHostConntrackBypass(fc)
+			updated = true
+		}
 	}
 
 	if install.Spec.Variant == operator.TigeraSecureEnterprise {

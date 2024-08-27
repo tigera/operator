f2de42303dcaae3654670a203bab33f4d26759e4
Pasan Weerasinghe <pasanweerasinghe@gmail.com>
2024-08-27 16:56:58 -0700

    Update ES and KB versions

--- pkg/components/enterprise.go
+++ pkg/components/enterprise.go
@@ -69,12 +69,12 @@ var (
 	}
 
 	ComponentEckElasticsearch = component{
-		Version:  "7.17.21",
+		Version:  "7.17.22",
 		Registry: "",
 	}
 
 	ComponentEckKibana = component{
-		Version:  "7.17.21",
+		Version:  "7.17.22",
 		Registry: "",
 	}
 

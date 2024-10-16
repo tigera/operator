1cacb0e9d4c834404399b1c0da4e048d19ea1c23
Walter Neto <walter@tigera.io>
2024-10-16 14:57:30 +0100

    Cherry pick for https://github.com/tigera/operator/pull/3543

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -41,4 +41,7 @@ const (
 	// references with any that already exist on the object rather than replace the owner references. Further
 	// the controller in the owner reference will not be set.
 	MultipleOwnersLabel = "operator.tigera.io/multipleOwners"
+
+	// Sidecar common names
+	SidecarMutatingWebhookConfigName = "tigera-sidecar-webhook-configuration"
 )

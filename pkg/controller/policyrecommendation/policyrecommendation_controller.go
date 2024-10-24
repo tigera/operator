6534f6943063f63451e885f5f1fd4f68b61c5e82
Dimitri Nicolopoulos <dimitri@tigera.io>
2024-10-24 16:41:47 -0700

    [EV-5299] Update Degraded messages for Windows nodes

--- pkg/controller/policyrecommendation/policyrecommendation_controller.go
+++ pkg/controller/policyrecommendation/policyrecommendation_controller.go
@@ -288,14 +288,15 @@ func (r *ReconcilePolicyRecommendation) Reconcile(ctx context.Context, request r
 		return reconcile.Result{RequeueAfter: utils.StandardRetry}, nil
 	}
 
-	// Render a Policy Recommendation component for Windows if the cluster has Windows nodes.
+	// Check if there are any Windows nodes in the cluster. Policy Recommendation is not supported
+	// in combination with Windows nodes.
 	hasWindowsNodes, err := common.HasWindowsNodes(r.client)
 	if err != nil {
-		r.status.SetDegraded(operatorv1.ResourceNotReady, "OS not found", err, logc)
+		r.status.SetDegraded(operatorv1.ResourceReadError, "Unable to determine if there are Windows nodes", err, logc)
 		return reconcile.Result{}, err
 	}
 	if hasWindowsNodes {
-		r.status.SetDegraded(operatorv1.ResourceValidationError, "Unsupported OS (Windows)", err, logc)
+		r.status.SetDegraded(operatorv1.ResourceValidationError, "Policy Recommendation is not supported in combination with Windows nodes", err, logc)
 		return reconcile.Result{}, err
 	}
 

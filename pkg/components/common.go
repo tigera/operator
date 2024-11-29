1098a6645c10ed75885dfbc4756b0df19d1bda72
Daniel Fox <dan.fox@tigera.io>
2024-11-28 16:01:58 -0800

    Automatic version updates for v1.32.12 release (#3620)
    * Automatic version updates for v1.32.12 release
    * Bump controller-gen version to v0.14.0 for release process
    * Update operator CRDs for release
    * Regenerate autogenerated files

--- pkg/components/common.go
+++ pkg/components/common.go
@@ -16,7 +16,7 @@ package components
 
 var (
 	ComponentCSRInitContainer = component{
-		Version:  "v1.1.25",
+		Version:  "v1.1.26",
 		Image:    "tigera/key-cert-provisioner",
 		Registry: "",
 	}

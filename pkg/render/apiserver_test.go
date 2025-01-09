bbbebab8f9774bc83326322681a8a880b9eed1f0
Dimitri Nicolopoulos <dimitri@tigera.io>
2025-01-09 10:57:47 -0800

    Add serviceaccount list to network-admin and user-ui

--- pkg/render/apiserver_test.go
+++ pkg/render/apiserver_test.go
@@ -1325,6 +1325,11 @@ var (
 		},
 		{
 			APIGroups: []string{""},
+			Resources: []string{"serviceaccounts"},
+			Verbs:     []string{"list"},
+		},
+		{
+			APIGroups: []string{""},
 			Resources: []string{"services/proxy"},
 			ResourceNames: []string{
 				"https:tigera-api:8080", "calico-node-prometheus:9090",
@@ -1465,6 +1470,11 @@ var (
 		{
 			APIGroups: []string{""},
 			Resources: []string{"pods"},
+			Verbs:     []string{"list"},
+		},
+		{
+			APIGroups: []string{""},
+			Resources: []string{"serviceaccounts"},
 			Verbs:     []string{"list"},
 		},
 		{

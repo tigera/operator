7592202a38a3d966cd1aaa35bbc9891318dbeb6b
Lorcan McVeigh <lorcan.mcveigh@gmail.com>
2025-02-25 11:28:34 +0000

    Remove operator import in operator api module

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,6 @@ import (
 
 	v3 "github.com/tigera/api/pkg/apis/projectcalico/v3"
 
-	v1 "github.com/tigera/operator/api/v1"
 	"github.com/tigera/operator/internal/controller"
 	"github.com/tigera/operator/pkg/active"
 	"github.com/tigera/operator/pkg/apis"
@@ -45,7 +44,7 @@ import (
 	"github.com/tigera/operator/pkg/render/logstorage/eck"
 	"github.com/tigera/operator/version"
 
-	operatortigeraiov1 "github.com/tigera/operator/api/v1"
+	operatortigeraiov1 "github.com/tigera/operator/api"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -301,7 +300,7 @@ If a value other than 'all' is specified, the first CRD with a prefix of the spe
 
 		// Check if we need to do any cleanup.
 		client := mgr.GetClient()
-		instance := &v1.Installation{}
+		instance := &operatortigeraiov1.Installation{}
 		retries := 0
 		for {
 			if err := client.Get(ctx, utils.DefaultInstanceKey, instance); errors.IsNotFound(err) {

f97fb3de7b4d602e9d69406ecc716c6a60bdcf33
Lorcan McVeigh <lorcan.mcveigh@gmail.com>
2025-02-26 13:45:24 +0000

    Fix tests

--- pkg/render/monitor/monitor_test.go
+++ pkg/render/monitor/monitor_test.go
@@ -359,7 +359,7 @@ var _ = Describe("monitor rendering tests", func() {
 		Expect(prometheusObj.Spec.RuleSelector.MatchLabels["role"]).To(Equal("tigera-prometheus-rules"))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers).To(HaveLen(1))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Name).To(Equal("calico-node-alertmanager"))
-		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Namespace).To(Equal("tigera-prometheus"))
+		Expect(*prometheusObj.Spec.Alerting.Alertmanagers[0].Namespace).To(Equal("tigera-prometheus"))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Port).To(Equal(intstr.FromString("web")))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Scheme).To(Equal("HTTP"))
 		Expect(*prometheusObj.Spec.SecurityContext.RunAsGroup).To(BeEquivalentTo(10001))

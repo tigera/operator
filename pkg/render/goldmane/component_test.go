cd6ac6c10f7b5edf916a0307b0844369be22d7e6
Brian McMahon <brianmcmahon135@gmail.com>
2025-03-19 10:47:27 -0700

    move override test to main component tests

--- pkg/render/goldmane/component_test.go
+++ pkg/render/goldmane/component_test.go
@@ -21,6 +21,11 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	operatorv1 "github.com/tigera/operator/api/v1"
 	"github.com/tigera/operator/pkg/ptr"
 	"github.com/tigera/operator/pkg/render"
@@ -30,9 +35,6 @@ import (
 	"github.com/tigera/operator/pkg/render/goldmane"
 	"github.com/tigera/operator/pkg/render/whisker"
 	"github.com/tigera/operator/pkg/tls/certificatemanagement"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -138,6 +140,98 @@ var _ = Describe("ComponentRendering", func() {
 			},
 		),
 	)
+
+	It("Should apply overrides", func() {
+		affinity := &corev1.Affinity{
+			NodeAffinity: &corev1.NodeAffinity{
+				RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+					NodeSelectorTerms: []corev1.NodeSelectorTerm{
+						{
+							MatchExpressions: []corev1.NodeSelectorRequirement{
+								{
+									Key:      "custom-affinity-key",
+									Operator: corev1.NodeSelectorOpExists,
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		goldmaneResources := &corev1.ResourceRequirements{
+			Limits: corev1.ResourceList{
+				"storage": resource.MustParse("10Gi"),
+			},
+			Requests: corev1.ResourceList{
+				"storage": resource.MustParse("10Gi"),
+			},
+		}
+		nodeSelector := map[string]string{
+			"some-selector": "an override of a default nodeSelector key",
+		}
+		podLabels := map[string]string{
+			"extra-label": "extra",
+		}
+		podAnnotations := map[string]string{
+			"extra-annotation": "extra",
+		}
+		tolerations := []corev1.Toleration{
+			{
+				Key:      "foo",
+				Operator: corev1.TolerationOpEqual,
+				Value:    "bar",
+			},
+		}
+		topologyConstraints := []corev1.TopologySpreadConstraint{
+			{
+				MaxSkew:           1,
+				TopologyKey:       "topology.kubernetes.io/zone",
+				WhenUnsatisfiable: corev1.ScheduleAnyway,
+				LabelSelector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"foo": "bar",
+					},
+				},
+			},
+		}
+
+		priorityClassName := "priority-class"
+
+		overrides := &operatorv1.GoldmaneDeployment{
+			Spec: &operatorv1.GoldmaneDeploymentSpec{
+				Template: &operatorv1.GoldmaneDeploymentPodTemplateSpec{
+					Metadata: &operatorv1.Metadata{
+						Labels:      podLabels,
+						Annotations: podAnnotations,
+					},
+					Spec: &operatorv1.GoldmaneDeploymentPodSpec{
+						Affinity: affinity,
+						Containers: []operatorv1.GoldmaneDeploymentContainer{
+							{
+								Name:      "goldmane",
+								Resources: goldmaneResources,
+							},
+						},
+						NodeSelector:              nodeSelector,
+						TopologySpreadConstraints: topologyConstraints,
+						Tolerations:               tolerations,
+						PriorityClassName:         priorityClassName,
+					},
+				},
+			},
+		}
+
+		deployment, err := GetOverriddenGoldmaneDeployment(overrides)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(deployment.Spec.Template.ObjectMeta.Labels).To(Equal(podLabels))
+		Expect(deployment.Spec.Template.ObjectMeta.Annotations).To(Equal(podAnnotations))
+		Expect(deployment.Spec.Template.Spec.Affinity).To(Equal(affinity))
+		Expect(deployment.Spec.Template.Spec.TopologySpreadConstraints).To(Equal(topologyConstraints))
+		Expect(deployment.Spec.Template.Spec.NodeSelector).To(Equal(nodeSelector))
+		Expect(deployment.Spec.Template.Spec.Tolerations).To(Equal(tolerations))
+		Expect(deployment.Spec.Template.Spec.PriorityClassName).To(Equal(priorityClassName))
+		Expect(deployment.Spec.Template.Spec.Containers[0].Resources).To(Equal(*goldmaneResources))
+	})
 })
 
 func GetOverriddenGoldmaneDeployment(overrides *operatorv1.GoldmaneDeployment) (*appsv1.Deployment, error) {

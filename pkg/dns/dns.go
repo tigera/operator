43dfa4427af49bf86a7f8b6275e228511ef5ab2f
Casey Davenport <davenport.cas@gmail.com>
2025-04-04 09:53:08 -0700

    Set explicit DNS nameservers for calico/node when needed

--- pkg/dns/dns.go
+++ pkg/dns/dns.go
@@ -17,6 +17,7 @@ package dns
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
 	"regexp"
 )
@@ -57,6 +58,41 @@ func GetClusterDomain(resolvConfPath string) (string, error) {
 	}
 
 	return clusterDomain, nil
+}
+
+// IsDomainName checks if the given name is a domain name and returns truee if it is.
+func IsDomainName(name string) bool {
+	// Attempt to parse it as an IP.
+	if net.ParseIP(name) != nil {
+		return false
+	}
+	return true
+}
+
+// Nameservers returns the list of nameservers from the resolv.conf file.
+func Nameservers(resolvConfPath string) ([]string, error) {
+	var nameservers []string
+	file, err := os.Open(resolvConfPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reg := regexp.MustCompile(`^nameserver\s+([^\s]+)`)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		match := reg.FindStringSubmatch(scanner.Text())
+		if len(match) > 0 {
+			nameservers = append(nameservers, match[1])
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return nameservers, nil
 }
 
 // GetServiceDNSNames returns a list of a service's DNS names.

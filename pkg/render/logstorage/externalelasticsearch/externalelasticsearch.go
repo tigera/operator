10b344df87fe3730ced67a5f7bc71a71cae447af
Alina Militaru <41362174+asincu@users.noreply.github.com>
2025-04-08 13:33:40 -0700

    Add rbac to delete secrets (#3870)
    * Add rbac to delete secrets for external controller
    
    When connecting a managed cluster to a management single tenant cluster,
    es-kube-controllers are responsible for copying the voltron linseed
    certificate via the tunnel. Before this operation, es kube controllers
    will first reconcile users. Any failure in reconciling users will lead
    to the certificate not being copied over. The first step in user
    reconciliation is to delete secret for decommisioned users or
    components, like curator. A failure because of missing RBAC will result
    the certificate not copied over, namespaces for fluentD, commpliance and
    intrusion controller not being created in the managed cluster. Single
    tenant management clusters with external elasticsearch are configured
    using external elasticsearch controller and rendered.
    
    * Fix! Update CRDs

--- pkg/render/logstorage/externalelasticsearch/externalelasticsearch.go
+++ pkg/render/logstorage/externalelasticsearch/externalelasticsearch.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2023-2024 Tigera, Inc. All rights reserved.
+// Copyright (c) 2023-2025 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	operatorv1 "github.com/tigera/operator/api/v1"
+	"github.com/tigera/operator/pkg/common"
 	"github.com/tigera/operator/pkg/render"
 	relasticsearch "github.com/tigera/operator/pkg/render/common/elasticsearch"
 	rmeta "github.com/tigera/operator/pkg/render/common/meta"
@@ -29,11 +30,12 @@ import (
 
 // ExternalElasticsearch is used when Elasticsearch doesn't exist in this cluster, but we still need to set up resources
 // related to Elasticsearch in the cluster.
-func ExternalElasticsearch(install *operatorv1.InstallationSpec, clusterConfig *relasticsearch.ClusterConfig, pullSecrets []*corev1.Secret) render.Component {
+func ExternalElasticsearch(install *operatorv1.InstallationSpec, clusterConfig *relasticsearch.ClusterConfig, pullSecrets []*corev1.Secret, multiTenant bool) render.Component {
 	return &externalElasticsearch{
 		installation:  install,
 		clusterConfig: clusterConfig,
 		pullSecrets:   pullSecrets,
+		multiTenant:   multiTenant,
 	}
 }
 
@@ -41,6 +43,7 @@ type externalElasticsearch struct {
 	installation  *operatorv1.InstallationSpec
 	clusterConfig *relasticsearch.ClusterConfig
 	pullSecrets   []*corev1.Secret
+	multiTenant   bool
 }
 
 func (e externalElasticsearch) ResolveImages(is *operatorv1.ImageSet) error {
@@ -56,6 +59,23 @@ func (e externalElasticsearch) Objects() (toCreate, toDelete []client.Object) {
 	if len(e.pullSecrets) > 0 {
 		toCreate = append(toCreate, secret.ToRuntimeObjects(secret.CopyToNamespace(render.ElasticsearchNamespace, e.pullSecrets...)...)...)
 	}
+
+	if !e.multiTenant {
+		// We need to allow to es-kube-controllers to managed secrets in the management clusters
+		// before pushing the Linseed - Voltron certificate to the tunnel. This certificate is pushed
+		// from es-kube-controllers for Single Tenant Management cluster. These types of cluster
+		// are using external ES. Similar RBAC is created in logstorage renderer that gets executed
+		// for management clusters with zero tenants or single tenant management clusters with internal
+		// elasticsearch
+		roles, bindings := e.kubeControllersRolesAndBindings()
+		for _, r := range roles {
+			toCreate = append(toCreate, r)
+		}
+		for _, b := range bindings {
+			toCreate = append(toCreate, b)
+		}
+	}
+
 	return toCreate, toDelete
 }
 
@@ -111,4 +131,81 @@ func (e externalElasticsearch) oidcUserRoleBinding() client.Object {
 			},
 		},
 	}
+}
+
+func (e externalElasticsearch) kubeControllersRolesAndBindings() ([]*rbacv1.Role, []*rbacv1.RoleBinding) {
+
+	secretsRole := &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{Kind: "Role", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      render.CalicoKubeControllerSecret,
+			Namespace: render.ElasticsearchNamespace,
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"secrets"},
+				Verbs:     []string{"create", "delete", "deletecollection", "update"},
+			},
+		},
+	}
+
+	operatorSecretsRole := &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{Kind: "Role", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      render.CalicoKubeControllerSecret,
+			Namespace: common.OperatorNamespace(),
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups: []string{""},
+				Resources: []string{"secrets"},
+				Verbs:     []string{"create", "delete", "deletecollection", "update"},
+			},
+		},
+	}
+
+	secretBinding := &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{Kind: "RoleBinding", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      render.CalicoKubeControllerSecret,
+			Namespace: render.ElasticsearchNamespace,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     render.CalicoKubeControllerSecret,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "calico-kube-controllers",
+				Namespace: common.CalicoNamespace,
+			},
+		},
+	}
+
+	// Bind the secrets permission to the operator namespace. This binding now adds permissions for kube controllers to manipulate
+	// secrets in the tigera-operator namespace
+	operatorSecretBinding := &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{Kind: "RoleBinding", APIVersion: "rbac.authorization.k8s.io/v1"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      render.CalicoKubeControllerSecret,
+			Namespace: common.OperatorNamespace(),
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     render.CalicoKubeControllerSecret,
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "calico-kube-controllers",
+				Namespace: common.CalicoNamespace,
+			},
+		},
+	}
+
+	return []*rbacv1.Role{secretsRole, operatorSecretsRole}, []*rbacv1.RoleBinding{secretBinding, operatorSecretBinding}
 }

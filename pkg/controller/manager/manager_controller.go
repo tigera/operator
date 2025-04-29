62115c8e2785af14700683ca68c6d1832594fc27
Casey Davenport <davenport.cas@gmail.com>
2025-04-29 10:34:26 -0700

    Fix manager RBAC for mixed mode management clusters

--- pkg/controller/manager/manager_controller.go
+++ pkg/controller/manager/manager_controller.go
@@ -636,6 +636,10 @@ func (r *ReconcileManager) Reconcile(ctx context.Context, request reconcile.Requ
 	if err != nil {
 		return reconcile.Result{}, err
 	}
+	managedCalicoNamespaces, err := helper.ManagedCalicoNamespaces(r.client)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
 
 	routeConfig, err := getVoltronRouteConfig(ctx, r.client, helper.InstallNamespace())
 	if err != nil {

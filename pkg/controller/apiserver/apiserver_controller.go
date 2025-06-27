b4db65758814bec7c69be5cae665e41739cde6b5
rene-dekker <rene@tigera.io>
2025-06-27 14:12:44 -0700

    Fix security context for csr init container
    The CSR init container should have the same scc as the container using
    the private key, cert and ca files that are created. Otherwise,
    permission errors will prevent the main container from starting.

--- pkg/controller/apiserver/apiserver_controller.go
+++ pkg/controller/apiserver/apiserver_controller.go
@@ -275,9 +275,9 @@ func (r *ReconcileAPIServer) Reconcile(ctx context.Context, request reconcile.Re
 	}
 
 	// Since apiserver and queryserver may have different UID:GID at run-time, we need to produce this secret in separate volumes and with different permissions.
-	var qsTLSSecretCertificateManagementOnly certificatemanagement.KeyPairInterface
+	var queryServerTLSSecretCertificateManagementOnly certificatemanagement.KeyPairInterface
 	if installationSpec.CertificateManagement != nil {
-		qsTLSSecretCertificateManagementOnly, err = certificateManager.GetOrCreateKeyPair(r.client, "query-server-tls", common.OperatorNamespace(), dns.GetServiceDNSNames(render.APIServerServiceName, render.APIServerNamespace, r.clusterDomain))
+		queryServerTLSSecretCertificateManagementOnly, err = certificateManager.GetOrCreateKeyPair(r.client, "query-server-tls", common.OperatorNamespace(), dns.GetServiceDNSNames(render.APIServerServiceName, render.APIServerNamespace, r.clusterDomain))
 		if err != nil {
 			r.status.SetDegraded(operatorv1.ResourceCreateError, "Unable to get or create tls key pair", err, reqLogger)
 			return reconcile.Result{}, err
@@ -447,7 +447,7 @@ func (r *ReconcileAPIServer) Reconcile(ctx context.Context, request reconcile.Re
 		KeyValidatorConfig:          keyValidatorConfig,
 		KubernetesVersion:           r.kubernetesVersion,
 		CanCleanupOlderResources:    r.canCleanupLegacyNamespace(ctx, installationSpec.Variant, reqLogger),
-		QueryServerTLSKeyPairCertificateManagementOnly: qsTLSSecretCertificateManagementOnly,
+		QueryServerTLSKeyPairCertificateManagementOnly: queryServerTLSSecretCertificateManagementOnly,
 	}
 
 	var components []render.Component

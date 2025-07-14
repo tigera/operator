1c017ce950378d1a2b200d3ed0f9622b57bca8d9
Brian McMahon <brianmcmahon135@gmail.com>
2025-07-14 12:16:57 -0700

    Set prometheus reload strategy to ProcessSignal instead of default to http
    Without the relaod strategy explicitly set to process signal, it default to http. This has the unfortunate consequence of the life-cycle management API being exposed which could result in a ddos attach in certain situations.
    
    The strategy is used by the config reload in the same pod as prometheus.

--- pkg/render/monitor/monitor_test.go
+++ pkg/render/monitor/monitor_test.go
@@ -362,6 +362,7 @@ var _ = Describe("monitor rendering tests", func() {
 		Expect(*prometheusObj.Spec.Alerting.Alertmanagers[0].Namespace).To(Equal("tigera-prometheus"))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Port).To(Equal(intstr.FromString("web")))
 		Expect(prometheusObj.Spec.Alerting.Alertmanagers[0].Scheme).To(Equal("HTTP"))
+		Expect(*prometheusObj.Spec.ReloadStrategy).To(BeEquivalentTo(monitoringv1.ProcessSignalReloadStrategyType))
 		Expect(*prometheusObj.Spec.SecurityContext.RunAsGroup).To(BeEquivalentTo(10001))
 		Expect(*prometheusObj.Spec.SecurityContext.RunAsNonRoot).To(BeTrue())
 		Expect(*prometheusObj.Spec.SecurityContext.RunAsUser).To(BeEquivalentTo(10001))

d8752919bd94b03a267e1a9fbe51e312fd5860b4
Walter Neto <walter@tigera.io>
2025-11-10 15:39:41 +0000

    Erase unnecessary commented code

--- internal/controller/istio_controller.go
+++ internal/controller/istio_controller.go
@@ -35,23 +35,6 @@ type IstioReconciler struct {
 // +kubebuilder:rbac:groups=operator.tigera.io,resources=istios/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=operator.tigera.io,resources=istios/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Istio object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.19.0/pkg/reconcile
-/*func (r *IstioReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// TODO(user): your logic here
-
-	return ctrl.Result{}, nil
-}*/
-
 // SetupWithManager sets up the controller with the Manager.
 func (r *IstioReconciler) SetupWithManager(mgr ctrl.Manager, opts options.AddOptions) error {
 	return istio.Add(mgr, opts)

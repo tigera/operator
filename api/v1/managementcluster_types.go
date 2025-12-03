00130252d5b938ba224a1adc6402468766d9dd86
Casey Davenport <davenport.cas@gmail.com>
2025-12-03 15:59:52 -0800

    Copyright

--- api/v1/managementcluster_types.go
+++ api/v1/managementcluster_types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2012,2015-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2012,2015-2025 Tigera, Inc. All rights reserved.
 /*
 
 Licensed under the Apache License, Version 2.0 (the "License");

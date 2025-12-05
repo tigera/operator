2eed2bb5c8d1e20933be64cb466ec6e8299da5d2
Josh <105011265+Josh-L@users.noreply.github.com>
2025-12-05 10:57:39 -0500

    Minimal footprint - consolidate tigera-manager namespace and contents into calico-system on standalone and management clusters (#4153)
    * Migrate manager components into calico-system namespace from
    tigera-manager namespace and replace component names with calico- prefix
    instead of tigera prefix. Also includes:
    -Create named trusted bundle for Manager to avoid collisions with
    existing default named trusted bundle
    -Move responsibility for namespace, operator rolebinding, and pull
    secrets rendering to core controller and remove handling of these
    resources in policy recommendation and es-kubecontrollers to avoid
    thrashing/contention
    -Update constraints kubebuilder validation to accept new manager component names
    -Add legacy namespace to created items in single-tenant case
    -Add ExternalName service to created items

--- api/v1/managementcluster_types.go
+++ api/v1/managementcluster_types.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2012,2015-2022 Tigera, Inc. All rights reserved.
+// Copyright (c) 2012,2015-2022,2025 Tigera, Inc. All rights reserved.
 /*
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -36,7 +36,7 @@ type ManagementClusterSpec struct {
 type TLS struct {
 	// SecretName indicates the name of the secret in the tigera-operator namespace that contains the private key and certificate that the management cluster uses when it listens for incoming connections.
 	//
-	// When set to tigera-management-cluster-connection voltron will use the same cert bundle which Guardian client certs are signed with.
+	// When set to calico-management-cluster-connection voltron will use the same cert bundle which Guardian client certs are signed with.
 	//
 	// When set to manager-tls, voltron will use the same cert bundle which Manager UI is served with.
 	// This cert bundle must be a publicly signed cert created by the user.
@@ -46,11 +46,11 @@ type TLS struct {
 	// If changed on a running cluster with connected managed clusters, all managed clusters will disconnect as they will no longer be able to verify Voltron's identity.
 	// To reconnect existing managed clusters, change the tls.ca of the  managed clusters' ManagementClusterConnection resource.
 	//
-	// One of: tigera-management-cluster-connection, manager-tls
+	// One of: calico-management-cluster-connection, manager-tls, tigera-management-cluster-connection (deprecated)
 	//
-	// Default: tigera-management-cluster-connection
+	// Default: calico-management-cluster-connection
 	//
-	// +kubebuilder:validation:Enum=tigera-management-cluster-connection;manager-tls
+	// +kubebuilder:validation:Enum=calico-management-cluster-connection;manager-tls;tigera-management-cluster-connection
 	// +optional
 	SecretName string `json:"secretName,omitempty"`
 }

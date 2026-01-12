0c83245866520a1ed6f464e304186745025eca82
Patrick Zhan <patrick.zhan@tigera.io>
2026-01-12 11:41:21 -0800

    [EV-6029][CI-1843] allow traffic from idc to voltron in the management cluster (#4350)
    * fix(networkpolicy): Add a missing allow rule from IDC to Voltron when MCM is enabled.
    
    * Refactor allow-tigera policy handling to support management clusters and improve test flexibility.
    
    ---------
    
    Co-authored-by: Rene Dekker <rene@tigera.io>

--- pkg/render/kubecontrollers/kube-controllers_test.go
+++ pkg/render/kubecontrollers/kube-controllers_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2025 Tigera, Inc. All rights reserved.
+// Copyright (c) 2019-2026 Tigera, Inc. All rights reserved.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1042,10 +1042,12 @@ var _ = Describe("kube-controllers rendering tests", func() {
 				policy := testutils.GetAllowTigeraPolicyFromResources(policyName, resources)
 				expectedPolicy := testutils.SelectPolicyByClusterTypeAndProvider(
 					scenario,
-					expectedPolicyForUnmanaged,
-					expectedPolicyForUnmanagedOCP,
-					expectedPolicyForManaged,
-					expectedPolicyForManagedOCP,
+					map[string]*v3.NetworkPolicy{
+						"unmanaged":           expectedPolicyForUnmanaged,
+						"unmanaged-openshift": expectedPolicyForUnmanagedOCP,
+						"managed":             expectedPolicyForManaged,
+						"managed-openshift":   expectedPolicyForManagedOCP,
+					},
 				)
 				Expect(policy).To(Equal(expectedPolicy))
 			},

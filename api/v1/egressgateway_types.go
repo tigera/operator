6063f671133b542cedbe81056c5b8970c1eae54c
sridhar <sridhar@tigera.io>
2026-02-11 12:29:38 -0800

    Render resources specified in the EGW CR

--- api/v1/egressgateway_types.go
+++ api/v1/egressgateway_types.go
@@ -24,8 +24,8 @@ import (
 // EGWDeploymentContainer is a Egress Gateway Deployment container.
 type EGWDeploymentContainer struct {
 	// Name is an enum which identifies the EGW Deployment container by name.
-	// Supported values are: calico-egw
-	// +kubebuilder:validation:Enum=calico-egw
+	// Supported values are: calico-egw,egress-gateway
+	// +kubebuilder:validation:Enum=calico-egw;egress-gateway
 	Name string `json:"name"`
 
 	// Resources allows customization of limits and requests for compute resources such as cpu and memory.

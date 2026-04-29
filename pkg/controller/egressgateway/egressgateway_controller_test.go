79fb659413e38f8e2c61956f16d00b63be15b861
Dimitri Nicolopoulos <dimitri@tigera.io>
2026-04-29 15:22:03 -0700

    Fix error message casing in egress gateway validation test

--- pkg/controller/egressgateway/egressgateway_controller_test.go
+++ pkg/controller/egressgateway/egressgateway_controller_test.go
@@ -582,7 +582,7 @@ var _ = Describe("Egress Gateway controller tests", func() {
 
 		It("Should throw an error when failure detection is specified without ICMP and HTTP probes", func() {
 			mockStatus.On("SetDegraded", "Waiting for LicenseKeyAPI to be ready", "").Return().Maybe()
-			mockStatus.On("SetDegraded", operatorv1.ResourceValidationError, "Error validating egress gateway Name = calico-red, Namespace = calico-egress", "Either ICMP or HTTP probe must be configured", mock.Anything, mock.Anything).Return()
+			mockStatus.On("SetDegraded", operatorv1.ResourceValidationError, "Error validating egress gateway Name = calico-red, Namespace = calico-egress", "either ICMP or HTTP probe must be configured", mock.Anything, mock.Anything).Return()
 			Expect(c.Create(ctx, installation)).NotTo(HaveOccurred())
 			var replicas int32 = 2
 			labels := map[string]string{"egress-code": "red"}

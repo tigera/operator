872ac74aae1b7d39ea7cdfb4ef2aebf3567a6619
Casey Davenport <davenport.cas@gmail.com>
2026-05-26 12:57:07 -0700

    gofmt cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -514,7 +514,7 @@ If a value other than 'all' is specified, the first CRD with a prefix of the spe
 		MultiTenant:         multiTenant,
 		ElasticExternal:     discovery.UseExternalElastic(bootConfig),
 		UseV3CRDs:           v3CRDs,
-		APIDiscovery: apiDiscovery,
+		APIDiscovery:        apiDiscovery,
 	}
 
 	// Before we start any controllers, make sure our options are valid.

a5767f2913c0db85c3ca059cbf6657dc47a1dbf8
Casey Davenport <davenport.cas@gmail.com>
2026-05-28 16:11:25 -0700

    operator: add patch registry

---
+++ pkg/operator/patch.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2026 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operator
+
+import "sigs.k8s.io/controller-runtime/pkg/client"
+
+// PatchFunc post-processes the objects a render component produced. It may
+// mutate matched objects and/or append additional objects, and must return the
+// (possibly extended) slice. Implementations self-gate on ctx.Installation.Variant.
+type PatchFunc func(ctx Context, objs []client.Object) []client.Object
+
+var patches = map[string][]PatchFunc{}
+
+// Patch registers fn to run against the named component's objects. Multiple
+// patches may be registered for the same component; they run in registration order.
+func Patch(component string, fn PatchFunc) {
+	patches[component] = append(patches[component], fn)
+}
+
+// ApplyPatches runs every patch registered for the named component over objs.
+func ApplyPatches(component string, ctx Context, objs []client.Object) []client.Object {
+	for _, fn := range patches[component] {
+		objs = fn(ctx, objs)
+	}
+	return objs
+}
+
+// FindObject returns the first object of type T with the given name.
+func FindObject[T client.Object](objs []client.Object, name string) (T, bool) {
+	var zero T
+	for _, o := range objs {
+		if t, ok := o.(T); ok && o.GetName() == name {
+			return t, true
+		}
+	}
+	return zero, false
+}
+
+// ResetForTest clears all registries. Test-only.
+func ResetForTest() {
+	patches = map[string][]PatchFunc{}
+}
---
+++ pkg/operator/patch_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2026 Tigera, Inc. All rights reserved.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package operator_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/tigera/operator/pkg/operator"
+)
+
+var _ = Describe("patch registry", func() {
+	AfterEach(func() {
+		operator.ResetForTest()
+	})
+
+	It("applies a registered patch to the matching component", func() {
+		operator.Patch("test", func(ctx operator.Context, objs []client.Object) []client.Object {
+			cm, ok := operator.FindObject[*corev1.ConfigMap](objs, "cm")
+			Expect(ok).To(BeTrue())
+			cm.Data = map[string]string{"k": "v"}
+			return append(objs, &corev1.Secret{ObjectMeta: metav1.ObjectMeta{Name: "extra"}})
+		})
+
+		in := []client.Object{&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "cm"}}}
+		out := operator.ApplyPatches("test", operator.Context{}, in)
+
+		Expect(out).To(HaveLen(2))
+		cm := out[0].(*corev1.ConfigMap)
+		Expect(cm.Data).To(HaveKeyWithValue("k", "v"))
+		Expect(out[1].GetName()).To(Equal("extra"))
+	})
+
+	It("returns objects unchanged when no patch is registered", func() {
+		in := []client.Object{&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "cm"}}}
+		out := operator.ApplyPatches("unregistered", operator.Context{}, in)
+		Expect(out).To(Equal(in))
+	})
+})

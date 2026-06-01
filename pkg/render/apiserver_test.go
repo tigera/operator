7acf1b59fff33c9d390c299bb330f6ac93a0ece7
Casey Davenport <davenport.cas@gmail.com>
2026-06-01 14:49:10 -0700

    Remove FIPS mode support
    Deprecates the Installation fipsMode field and marks the installation
    degraded when FIPS mode is enabled. Removes the -fips image variants and
    the rendering paths that selected them.

--- pkg/render/apiserver_test.go
+++ pkg/render/apiserver_test.go
@@ -2485,20 +2485,6 @@ var _ = Describe("API server rendering tests (Calico)", func() {
 				Effect:   corev1.TaintEffectNoSchedule,
 			}))
 		})
-
-		It("should render the correct env and/or images when FIPS mode is enabled (OSS)", func() {
-			fipsEnabled := operatorv1.FIPSModeEnabled
-			cfg.Installation.FIPSMode = &fipsEnabled
-
-			component, err := render.APIServer(cfg)
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(component.ResolveImages(nil)).To(BeNil())
-			resources, _ := component.Objects()
-
-			d := rtest.GetResource(resources, "calico-apiserver", "calico-system", "apps", "v1", "Deployment").(*appsv1.Deployment)
-			Expect(d.Spec.Template.Spec.Containers[0].Image).To(ContainSubstring("-fips"))
-		})
 	})
 
 	Context("multi-tenant", func() {

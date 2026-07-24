0ebca71e20b3855baf65b5aa7cdceb44316235a6
Pedro Coutinho <pedro@tigera.io>
2026-07-24 10:51:16 -0700

    fix import

--- cmd/main.go
+++ cmd/main.go
@@ -53,6 +53,7 @@ import (
 	"github.com/tigera/operator/version"
 
 	operatortigeraiov1 "github.com/tigera/operator/api/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"

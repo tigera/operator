a22914075062dae8d092b6e081f6d6583f6681fa
Casey Davenport <davenport.cas@gmail.com>
2026-08-04 08:46:33 -0700

    Use the non-deprecated variant constant in the CSR test
    staticcheck flags TigeraSecureEnterprise, and the test just needs an
    enterprise variant.

--- pkg/controller/csr/csr_controller_test.go
+++ pkg/controller/csr/csr_controller_test.go
@@ -105,7 +105,7 @@ var _ = Describe("CSR controller tests", func() {
 			provider:         operatorv1.ProviderNone,
 			clusterDomain:    dns.DefaultClusterDomain,
 			allowedTLSAssets: allowedAssets(dns.DefaultClusterDomain),
-			variant:          operatorv1.TigeraSecureEnterprise,
+			variant:          operatorv1.CalicoEnterprise,
 		}
 	})
 

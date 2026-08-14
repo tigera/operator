a89ffc752b8395f76e6d3b7b9ffbb9b8b5306337
Casey Davenport <davenport.cas@gmail.com>
2026-08-14 13:26:58 -0400

    Move the Enterprise CR getters into pkg/enterprise/utils
    The shared controller utils package no longer reads Enterprise-only CRs. The
    namespace helper and the tenant event handler move along with them.

--- pkg/controller/monitor/monitor_controller.go
+++ pkg/controller/monitor/monitor_controller.go
@@ -44,6 +44,7 @@ import (
 	"github.com/tigera/operator/pkg/controller/utils/imageset"
 	"github.com/tigera/operator/pkg/ctrlruntime"
 	"github.com/tigera/operator/pkg/dns"
+	eutils "github.com/tigera/operator/pkg/enterprise/utils"
 	"github.com/tigera/operator/pkg/render"
 	rcertificatemanagement "github.com/tigera/operator/pkg/render/certificatemanagement"
 	rauth "github.com/tigera/operator/pkg/render/common/authentication"
@@ -365,7 +366,7 @@ func (r *ReconcileMonitor) Reconcile(ctx context.Context, request reconcile.Requ
 	certificateManager.AddToStatusManager(r.status, common.TigeraPrometheusNamespace)
 
 	// Fetch the Authentication spec. If present, we use to configure user authentication.
-	authenticationCR, err := utils.GetAuthentication(ctx, r.client)
+	authenticationCR, err := eutils.GetAuthentication(ctx, r.client)
 	if err != nil && !errors.IsNotFound(err) {
 		r.status.SetDegraded(operatorv1.ResourceReadError, "Error querying Authentication", err, reqLogger)
 		return reconcile.Result{}, err
